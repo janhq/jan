aa44d4fb3b756631e37d233af70aad0f2eb91c66
Hien To <>
2023-08-29 14:19:09 +0700

    add github action deploy docusaurus to github page

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,12 +6,12 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Jan Docs',
+  title: 'Jan',
   tagline: 'Self-hosted AI inference platform',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://docs.jan.ai',
+  url: 'https://jan.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
