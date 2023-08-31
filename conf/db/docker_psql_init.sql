c5397184ee86da49af46b067ebd8f7bbac432a0a
namvuong <22463238+vuonghoainam@users.noreply.github.com>
2023-08-31 14:23:06 +0700

    fix(compose): Use 1 psql db instead of 2 (#23)
    * fix(compose): Use 1 psql db instead of 2
    
    * chore: fix kc db config
    
    * chore: clean env
    
    ---------
    
    Co-authored-by: Louis <louis@jan.ai>

---
+++ conf/db/docker_psql_init.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE "jan-keycloak"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+CREATE DATABASE "jan-hasura-data"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+CREATE DATABASE "jan-hasura-metadata"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
