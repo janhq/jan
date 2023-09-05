25a096da28de8ba32a0b1b782811d5a2d142892c
Louis <133622055+louis-jan@users.noreply.github.com>
2023-09-05 22:00:01 +0700

    Resolves #35, resolves #59, resolves #53 (#60)
    * fix: [#35] - jan containers wont restart
    
    * chore: remove keycloak health check https://github.com/keycloak/keycloak/issues/17273
    
    * fix: #53 hasura seeds should not yield error on conflict
    
    * fix: #59 hasura image generation action timeout
    
    * chore: jan web - fixes error logs of empty href url

--- app-backend/hasura/seeds/jandb/1692710265351_collectionsSeed.sql
+++ app-backend/hasura/seeds/jandb/1692710265351_collectionsSeed.sql
@@ -1,4 +1,5 @@
 SET check_function_bodies = false;
 INSERT INTO public.collections ("slug", "name", "description") VALUES
 ('conversational', 'Conversational', 'Chatbot alternatives to ChatGPT. Converse with these models and get answers.'),
-('text-to-image', 'Text to image', 'Converse with these models to generate images.');
+('text-to-image', 'Text to image', 'Converse with these models to generate images.')
+ON CONFLICT (slug) DO NOTHING;
