f790aae05c84140dee0c07bdc02787ced107082d
Louis <133622055+louis-jan@users.noreply.github.com>
2023-09-06 15:51:15 +0700

    fix: #39 remove SD model data seed (#68)

--- app-backend/hasura/seeds/jandb/1692710265351_collectionsSeed.sql
+++ app-backend/hasura/seeds/jandb/1692710265351_collectionsSeed.sql
@@ -1,5 +1,4 @@
 SET check_function_bodies = false;
 INSERT INTO public.collections ("slug", "name", "description") VALUES
-('conversational', 'Conversational', 'Chatbot alternatives to ChatGPT. Converse with these models and get answers.'),
-('text-to-image', 'Text to image', 'Converse with these models to generate images.')
+('conversational', 'Conversational', 'Chatbot alternatives to ChatGPT. Converse with these models and get answers.')
 ON CONFLICT (slug) DO NOTHING;
