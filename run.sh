510d680e2e434f9959c88640593ff4fba5747056
Louis <louis@jan.ai>
2023-09-07 11:18:06 +0700

    chore: auto increment steps

--- run.sh
+++ run.sh
@@ -44,55 +44,56 @@ progress() {
     echo -ne "\\r\\033[1A- [$3/$MAX_STEPS] [ ✔ ] $2 $CLEAR_LINE\\n"
 }
 # macOS
+step=1
 if [[ "$OSTYPE" == "darwin"* ]]; then
     if [[ -x "$(command -v brew)" ]]; then
-        progress 'brew update' "Updating Homebrew" 1
+        progress 'brew update' "Updating Homebrew" $((step++))
     else
         progress '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"' "Installing Homebrew" 1
     fi
 
     xcode-select -p &>/dev/null
     if [ $? -ne 0 ]; then
-        progress 'xcode-select --install' "Installing Xcode Command Line Tools" 2
+        progress 'xcode-select --install' "Installing Xcode Command Line Tools" $((step++))
     else
-        progress '' "Installing Xcode Command Line Tools" 2
+        progress '' "Installing Xcode Command Line Tools" $((step++))
     fi
 
     if [[ ! -x "$(command -v git)" ]]; then
-        progress 'brew install git' "Installing Git" 3
+        progress 'brew install git' "Installing Git" $((step++))
     else
-        progress '' "Installing Git" 3
+        progress '' "Installing Git" $((step++))
     fi
 
     if [[ ! -x "$(command -v docker)" ]]; then
-        progress 'brew install --cask docker' "Installing Docker" 4
+        progress 'brew install --cask docker' "Installing Docker" $((step++))
     else
-        progress '' "Installing Docker" 4
+        progress '' "Installing Docker" $((step++))
     fi
 
     if [ -x "$(command -v docker compose)" ] || [ -x "$(command -v docker-compose)" ]; then
-        progress '' "Installing Docker Compose" 5
+        progress '' "Installing Docker Compose" $((step++))
     elif [[ ! -x "$(command -v docker-compose)" ]]; then
-        progress 'brew install docker-compose' "Installing Docker Compose" 5
+        progress 'brew install docker-compose' "Installing Docker Compose" $((step++))
     fi
 fi
 
 if [ -d "jan" ]; then
     cd jan
-    progress 'git fetch; git pull 2>/dev/null' "Git pull" 6
+    progress 'git pull 2>/dev/null' "Git pull" $((step++))
 else
-    progress 'git clone --quiet https://github.com/janhq/jan' "Git clone" 6
+    progress 'git clone --quiet https://github.com/janhq/jan' "Git clone" $((step++))
     cd jan
 fi
 
 cp -f sample.env .env
 
-progress 'git submodule update --init --recursive' "Pull submodule" 7
+progress 'git submodule update --init --recursive' "Pull submodule" $((step++))
 
 if [ -f "jan-inference/llm/models/llama-2-7b-chat.ggmlv3.q4_1.bin" ]; then
-    progress '' "Download Llama model" 8
+    progress '' "Download Llama model" $((step++))
 else
-    progress 'wget https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/resolve/main/llama-2-7b-chat.ggmlv3.q4_1.bin -P jan-inference/llm/models' "Download Llama model" 8
+    progress 'wget https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/resolve/main/llama-2-7b-chat.ggmlv3.q4_1.bin -P jan-inference/llm/models' "Download Llama model" $((step++))
 fi
 
 progress $'
@@ -101,12 +102,12 @@ progress $'
         while (! docker stats --no-stream 2>/dev/null ); do
             sleep 0.3 
         done 
-    fi' "Waiting for docker to launch" 9
+    fi' "Waiting for docker to launch" $((step++))
 if [[ -x "$(command -v docker compose)" ]]; then
-    progress 'docker compose up -d --quiet-pull --remove-orphans 2>/dev/null' "Docker compose up" 10
+    progress 'docker compose up -d --quiet-pull --remove-orphans 2>/dev/null' "Docker compose up" $((step++))
 fi
 if [[ -x "$(command -v docker-compose)" ]]; then
-progress 'docker-compose up -d --quiet-pull --remove-orphans 2>/dev/null' "Docker compose up" 10
+progress 'docker-compose up -d --quiet-pull --remove-orphans 2>/dev/null' "Docker compose up" $((step++))
 fi
 
 progress $'
@@ -115,6 +116,6 @@ progress $'
         break
     fi
 done
-' "Waiting for service ready" 11
+' "Waiting for service ready" $((step++))
 
 echo -ne "\\r You can now view Jan app in the browser: http://localhost:3000 \\n"
