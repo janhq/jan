ed91a010ff9b8814c7465cce81266b0f01aecd55
Louis <louis@jan.ai>
2023-09-07 11:50:40 +0700

    chore: shorten linux setup

--- run.sh
+++ run.sh
@@ -105,20 +105,7 @@ if [[ "$OSTYPE" == "linux"* ]]; then
         progress '' "Installing Wget" $((step++))
     fi
 
-    progress $'
-    sudo apt-get install ca-certificates curl gnupg
-    sudo install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-    echo \
-    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update >/dev/null
-    ' "Setting up Docker's Apt repository" $((step++))
-
-    progress 'sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin' "Installing Docker" $((step++))
+    progress '/bin/bash -c "$(curl -fsSL https://get.docker.com/)"' "Installing Docker" $((step++))
 fi
 ### Debian setup
 
