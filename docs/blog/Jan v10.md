a0d7c94119d2697e3c4888177233a23538e84332
Its-Alamin-H <alaminhosssain97@gmail.com>
2023-09-21 20:57:43 +0600

    [added changelog and posthog]

---
+++ docs/blog/Jan v10.md
@@ -0,0 +1,33 @@
+---
+title: JanAI v1.0
+description: First changelog
+slug: jan v1.0
+authors: [hiento09, Louis, Daniel, automaticcat, namvuong, Nicole]
+hide_table_of_contents: false
+---
+
+Stay Updated with JanAI: Explore Our Latest Changelog.
+
+<!--truncate-->
+
+### 🎉 We're thrilled to introduce the first version of Jan Web!
+
+What is Jan Web, you ask? It’s pretty much a web version of our Jan Mobile, that lets you generate amazing art via Open Source AI Models as well as engaging with the most popular AI Chatbots in the market. The best part of this? It’s totally free!
+
+**This version included:**
+
+**🤖 AI Models:**
+
+- Access 5 LLMs and over 20 Image Generators
+- This includes: SDXL, OpenJourney, ChatGPT, Guanaco and many more!
+
+**💬 Chat (Prompt):**
+
+- Google Login for session saving
+- Download or Regenerate images with Image Models
+- Manage sessions: Create or Delete
+- Model details panel: Learn model info, try prompt samples
+
+Got feedback? Let us know via Discord:
+
+[Join our Discord Server](https://discord.com/invite/FTk2MvZwJH)
--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,69 +1,76 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Jan Documentation',
-  tagline: 'Self-hosted AI inference platform',
-  favicon: 'img/favicon.ico',
+  title: "Jan Documentation",
+  tagline: "Self-hosted AI inference platform",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://docs.jan.ai',
+  url: "https://docs.jan.ai",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'janhq', // Usually your GitHub org/user name.
-  projectName: 'jan', // Usually your repo name.
+  organizationName: "janhq", // Usually your GitHub org/user name.
+  projectName: "jan", // Usually your repo name.
 
-  onBrokenLinks: 'ignore',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "ignore",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   // Plugins we added
+
   // Only for react live
-  themes: ['@docusaurus/theme-live-codeblock'],
+  themes: ["@docusaurus/theme-live-codeblock"],
 
   // The classic preset will relay each option entry to the respective sub plugin/theme.
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         // Will be passed to @docusaurus/plugin-content-docs (false to disable)
         docs: {
-          routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/janhq/jan/tree/main/docs',
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/janhq/jan/tree/main/docs",
           sidebarCollapsed: false,
           showLastUpdateAuthor: true,
           showLastUpdateTime: true,
         },
         // Will be passed to @docusaurus/plugin-content-sitemap (false to disable)
         sitemap: {
-          changefreq: 'weekly',
+          changefreq: "weekly",
           priority: 0.5,
-          ignorePatterns: ['/tags/**'],
-          filename: 'sitemap.xml',
+          ignorePatterns: ["/tags/**"],
+          filename: "sitemap.xml",
         },
         // Will be passed to @docusaurus/plugin-content-blog (false to disable)
-        blog: false,
+        blog: {
+          routeBasePath: "/blog",
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/janhq/jan/tree/main/docs",
+        },
+
         // Will be passed to @docusaurus/theme-classic.
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
         // Will be passed to @docusaurus/plugin-content-pages (false to disable)
         // pages: {},
@@ -71,142 +78,149 @@ const config = {
     ],
     // Redoc preset
     [
-      'redocusaurus',
+      "redocusaurus",
       {
         specs: [
           {
-            spec: 'openapi/OpenAPISpec.json', // can be local file, url, or parsed json object
-            route: '/reference/',
+            spec: "openapi/OpenAPISpec.json", // can be local file, url, or parsed json object
+            route: "/reference/",
           },
         ],
         theme: {
-          primaryColor: '#1a73e8',
-          primaryColorDark: '#1a73e8',
+          primaryColor: "#1a73e8",
+          primaryColorDark: "#1a73e8",
           // redocOptions: { hideDownloadButton: false },
         },
-      }
-    ]
+      },
+    ],
   ],
 
   // Docs: https://docusaurus.io/docs/api/themes/configuration
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      image: 'img/jan-social-card.png',
+      image: "img/jan-social-card.png",
       // Only for react live
       liveCodeBlock: {
-        playgroundPosition: 'bottom',
+        playgroundPosition: "bottom",
       },
       navbar: {
-        title: 'Jan Docs',
+        title: "Jan Docs",
         logo: {
-          alt: 'Jan Logo',
-          src: 'img/logo.svg',
+          alt: "Jan Logo",
+          src: "img/logo.svg",
         },
         items: [
           // Navbar left
           {
-            type: 'docSidebar',
-            sidebarId: 'quickstartSidebar',
-            position: 'left',
-            label: 'Getting Started',
+            type: "docSidebar",
+            sidebarId: "quickstartSidebar",
+            position: "left",
+            label: "Getting Started",
           },
           {
-            type: 'docSidebar',
-            sidebarId: 'guidesSidebar',
-            position: 'left',
-            label: 'Guides',
+            type: "docSidebar",
+            sidebarId: "guidesSidebar",
+            position: "left",
+            label: "Guides",
           },
           {
-            type: 'docSidebar',
-            sidebarId: 'hardwareSidebar',
-            position: 'left',
-            label: 'Hardware',
+            type: "docSidebar",
+            sidebarId: "hardwareSidebar",
+            position: "left",
+            label: "Hardware",
           },
           {
-            type: 'docSidebar',
-            sidebarId: 'integrationsSidebar',
-            position: 'left',
-            label: 'Integrations',
+            type: "docSidebar",
+            sidebarId: "integrationsSidebar",
+            position: "left",
+            label: "Integrations",
           },
           {
-            position: 'left',
-            label: 'Reference',
-            to: '/reference',
+            position: "left",
+            label: "Reference",
+            to: "/reference",
           },
+          {
+            sidebarId: "integrationsSidebar",
+            position: "left",
+            label: "Changelog",
+            to: "/blog",
+          },
+
           // Navbar right
           {
-            type: 'docSidebar',
-            sidebarId: 'aboutSidebar',
-            position: 'right',
-            label: 'About',
+            type: "docSidebar",
+            sidebarId: "aboutSidebar",
+            position: "right",
+            label: "About",
           },
           {
-            href: 'https://github.com/janhq/jan',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/janhq/jan",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Documentation',
+            title: "Documentation",
             items: [
               {
-                label: 'Getting Started',
-                to: '/',
+                label: "Getting Started",
+                to: "/",
               },
               {
-                label: 'Guides',
-                to: '/guides',
+                label: "Guides",
+                to: "/guides",
               },
               {
-                label: 'Developer',
-                to: '/developer',
+                label: "Developer",
+                to: "/developer",
               },
               {
-                label: 'Reference',
-                to: '/reference',
+                label: "Reference",
+                to: "/reference",
               },
               {
-                label: 'Changelog',
-                to: '/changelog',
+                label: "Changelog",
+                to: "/blog",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Discord',
-                href: 'https://discord.gg/FTk2MvZwJH',
+                label: "Discord",
+                href: "https://discord.gg/FTk2MvZwJH",
               },
               {
-                label: 'Telegram',
-                href: 'https://t.me/+cSSlxwEM-0piMDQ1',
+                label: "Telegram",
+                href: "https://t.me/+cSSlxwEM-0piMDQ1",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/askjanai',
+                label: "Twitter",
+                href: "https://twitter.com/askjanai",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'About',
-                to: '/about',
+                label: "About",
+                to: "/about",
               },
               {
-                label: 'Blog',
-                href: 'https://blog.jan.ai',
+                label: "Blog",
+                href: "https://blog.jan.ai",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/janhq/jan',
+                label: "GitHub",
+                href: "https://github.com/janhq/jan",
               },
             ],
           },
@@ -216,9 +230,23 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['python'],
+        additionalLanguages: ["python"],
       },
     }),
 };
+
+// Integrating PostHog product analytics with Docusaurus
+
+config.plugins = [
+  [
+    "posthog-docusaurus",
+    {
+      apiKey: process.env.POSTHOG_API_KEY || "DEV",
+      appUrl: "https://app.posthog.com", // optional
+      enableInDevelopment: true, // optional
+      // other options are passed to posthog-js init as is
+    },
+  ],
+];
 
 module.exports = config;
