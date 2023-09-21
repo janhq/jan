f6ce072ec7bcac2ec25147744947584485c2e6f5
Louis <louis@jan.ai>
2023-09-21 15:17:43 +0700

    chore: add empty state for conversation list

--- web-client/app/_components/HistoryEmpty/index.tsx
+++ web-client/app/_components/HistoryEmpty/index.tsx
@@ -1,10 +1,25 @@
+import useCreateConversation from "@/_hooks/useCreateConversation";
+import { executeSerial } from "@/_services/pluginService";
 import Image from "next/image";
-import Link from "next/link";
 import React from "react";
+import { ModelManagementService } from "../../../shared/coreService";
 
 const HistoryEmpty: React.FC = () => {
+  const { requestCreateConvo } = useCreateConversation();
+  const startChat = async () => {
+    const downloadedModels = await executeSerial(
+      ModelManagementService.GET_DOWNLOADED_MODELS
+    );
+    if (!downloadedModels || downloadedModels.length === 0) {
+      alert(
+        "Seems like there is no model downloaded yet. Please download a model first."
+      );
+    } else {
+      requestCreateConvo(1);
+    }
+  };
   return (
-    <div className="flex flex-col w-full h-full items-center justify-center gap-4">
+    <div className="mt-5 flex flex-col w-full h-full items-center justify-center gap-4">
       <Image
         src={"icons/chats-circle-light.svg"}
         width={50}
@@ -12,14 +27,14 @@ const HistoryEmpty: React.FC = () => {
         alt=""
       />
       <p className="text-sm leading-5 text-center text-[#9CA3AF]">
-        Jan allows you to use 100s of AIs on your mobile phone
+        Its empty here
       </p>
-      <Link
-        href="/ai"
+      <button
+        onClick={startChat}
         className="bg-[#1F2A37] py-[10px] px-5 gap-2 rounded-[8px] text-[14px] font-medium leading-[21px] text-white"
       >
-        Explore AIs
-      </Link>
+        Let&apos;s chat
+      </button>
     </div>
   );
 };
--- web-client/app/_components/HistoryList/index.tsx
+++ web-client/app/_components/HistoryList/index.tsx
@@ -4,6 +4,7 @@ import ExpandableHeader from "../ExpandableHeader";
 import { useAtomValue } from "jotai";
 import { searchAtom, userConversationsAtom } from "@/_helpers/JotaiWrapper";
 import useGetUserConversations from "@/_hooks/useGetUserConversations";
+import HistoryEmpty from "../HistoryEmpty";
 
 const HistoryList: React.FC = () => {
   const conversations = useAtomValue(userConversationsAtom);
@@ -25,21 +26,25 @@ const HistoryList: React.FC = () => {
       <div
         className={`flex flex-col gap-1 mt-1 ${!expand ? "hidden " : "block"}`}
       >
-        {conversations
-          .filter(
-            (e) =>
-              searchText.trim() === "" ||
-              e.name?.toLowerCase().includes(searchText.toLowerCase().trim())
-          )
-          .map((convo) => (
-            <HistoryItem
-              key={convo.id}
-              conversation={convo}
-              avatarUrl={convo.image}
-              name={convo.name || "Jan"}
-              updatedAt={convo.updated_at ?? ""}
-            />
-          ))}
+        {conversations.length > 0 ? (
+          conversations
+            .filter(
+              (e) =>
+                searchText.trim() === "" ||
+                e.name?.toLowerCase().includes(searchText.toLowerCase().trim())
+            )
+            .map((convo) => (
+              <HistoryItem
+                key={convo.id}
+                conversation={convo}
+                avatarUrl={convo.image}
+                name={convo.name || "Jan"}
+                updatedAt={convo.updated_at ?? ""}
+              />
+            ))
+        ) : (
+          <HistoryEmpty/>
+        )}
       </div>
     </div>
   );
