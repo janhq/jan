43d2dc4997dc9cb31db831a48bbeddae1f6b59c7
Louis <louis@jan.ai>
2023-09-21 15:05:10 +0700

    chore: search conversation history

--- web-client/app/_components/HistoryList/index.tsx
+++ web-client/app/_components/HistoryList/index.tsx
@@ -2,11 +2,12 @@ import HistoryItem from "../HistoryItem";
 import { useEffect, useState } from "react";
 import ExpandableHeader from "../ExpandableHeader";
 import { useAtomValue } from "jotai";
-import { userConversationsAtom } from "@/_helpers/JotaiWrapper";
+import { searchAtom, userConversationsAtom } from "@/_helpers/JotaiWrapper";
 import useGetUserConversations from "@/_hooks/useGetUserConversations";
 
 const HistoryList: React.FC = () => {
   const conversations = useAtomValue(userConversationsAtom);
+  const searchText = useAtomValue(searchAtom);
   const [expand, setExpand] = useState<boolean>(true);
   const { getUserConversations } = useGetUserConversations();
 
@@ -24,15 +25,21 @@ const HistoryList: React.FC = () => {
       <div
         className={`flex flex-col gap-1 mt-1 ${!expand ? "hidden " : "block"}`}
       >
-        {conversations.map((convo) => (
-          <HistoryItem
-            key={convo.id}
-            conversation={convo}
-            avatarUrl={convo.image}
-            name={convo.name || "Jan"}
-            updatedAt={convo.updated_at ?? ""}
-          />
-        ))}
+        {conversations
+          .filter(
+            (e) =>
+              searchText.trim() === "" ||
+              e.name?.toLowerCase().includes(searchText.toLowerCase().trim())
+          )
+          .map((convo) => (
+            <HistoryItem
+              key={convo.id}
+              conversation={convo}
+              avatarUrl={convo.image}
+              name={convo.name || "Jan"}
+              updatedAt={convo.updated_at ?? ""}
+            />
+          ))}
       </div>
     </div>
   );
