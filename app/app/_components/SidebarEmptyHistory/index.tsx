a47a766dc8665707ab365a0a6b850c0f9fd8f5d5
Louis <louis@jan.ai>
2023-09-22 14:33:01 +0700

    chore: add new chat function

--- app/app/_components/SidebarEmptyHistory/index.tsx
+++ app/app/_components/SidebarEmptyHistory/index.tsx
@@ -1,7 +1,23 @@
 import Image from "next/image";
 import { SidebarButton } from "../SidebarButton";
+import { executeSerial } from "../../../electron/core/plugin-manager/execution/extension-manager";
+import { ModelManagementService } from "../../../shared/coreService";
+import useCreateConversation from "@/_hooks/useCreateConversation";
 
 const SidebarEmptyHistory: React.FC = () => {
+  const { requestCreateConvo } = useCreateConversation();
+  const startChat = async () => {
+    const downloadedModels = await executeSerial(
+      ModelManagementService.GET_DOWNLOADED_MODELS
+    );
+    if (!downloadedModels || downloadedModels.length === 0) {
+      alert(
+        "Seems like there is no model downloaded yet. Please download a model first."
+      );
+    } else {
+      requestCreateConvo(1);
+    }
+  };
   return (
     <div className="flex flex-col items-center py-10 gap-3">
       <Image
@@ -20,6 +36,7 @@ const SidebarEmptyHistory: React.FC = () => {
           </div>
         </div>
         <SidebarButton
+          callback={startChat}
           className="flex items-center border bg-blue-600 rounded-lg py-[9px] pl-[15px] pr-[17px] gap-2 text-white font-medium text-sm"
           height={14}
           icon="icons/Icon_plus.svg"
