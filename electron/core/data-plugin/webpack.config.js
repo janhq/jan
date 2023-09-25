fc20caee6656054eca0d100eb5d808a6d610f164
Louis <louis@jan.ai>
2023-09-25 20:15:54 +0700

    chore: pluggin in ts

--- electron/core/data-plugin/webpack.config.js
+++ electron/core/data-plugin/webpack.config.js
@@ -2,14 +2,24 @@ const path = require("path");
 
 module.exports = {
   experiments: { outputModule: true },
-  entry: "./index.js", // Adjust the entry point to match your project's main file
+  entry: "./index.ts", // Adjust the entry point to match your project's main file
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
   output: {
-    filename: "bundle.js", // Adjust the output file name as needed
+    filename: "index.js", // Adjust the output file name as needed
     path: path.resolve(__dirname, "dist"),
     library: { type: "module" }, // Specify ESM output format
   },
   resolve: {
-    extensions: [".js"],
+    extensions: [".ts", ".js"],
   },
   // Add loaders and other configuration as needed for your project
 };
