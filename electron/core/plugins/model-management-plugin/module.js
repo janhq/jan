a0afdda16ee3240fdeabfbeb6fca04d89406e6d2
Louis <louis@jan.ai>
2023-09-29 15:01:50 +0700

    chore: update model description

--- electron/core/plugins/model-management-plugin/module.js
+++ electron/core/plugins/model-management-plugin/module.js
@@ -32,7 +32,8 @@ const ALL_MODELS = [
     id: "llama-2-13b-chat.Q4_K_M.gguf",
     slug: "llama-2-13b-chat.Q4_K_M.gguf",
     name: "Llama 2 13B Chat - GGUF",
-    description: "medium, balanced quality - recommended",
+    description:
+      "medium, balanced quality - not recommended for RAM 16GB and below",
     avatarUrl:
       "https://aeiljuispo.cloudimg.io/v7/https://cdn-uploads.huggingface.co/production/uploads/6426d3f3a7723d62b53c259b/tvPikpAzKTKGN5wrpadOJ.jpeg?w=200&h=200&f=face",
     longDescription:
