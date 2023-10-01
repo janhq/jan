6500d0dca086496f12f7d96f258b54eed1cd22a1
Louis <louis@jan.ai>
2023-10-01 12:29:17 +0700

    chore: add linter

---
+++ electron/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  env: {
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+  ],
+  rules: {
+    "@typescript-eslint/no-non-null-assertion": "off",
+    quotes: ["error", "single"],
+    "react/prop-types": "off", // In favor of strong typing - no need to dedupe
+  },
+  settings: {
+    react: {
+      createClass: "createReactClass", // Regex for Component Factory to use,
+      // default to "createReactClass"
+      pragma: "React", // Pragma to use, default to "React"
+      version: "detect", // React version. "detect" automatically picks the version you have installed.
+      // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+      // default to latest and warns if missing
+      // It will default to "detect" in the future
+    },
+    linkComponents: [
+      // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
+      "Hyperlink",
+      { name: "Link", linkAttribute: "to" },
+    ],
+  },
+  ignorePatterns: ["renderer/*", "node_modules/*", "core/plugins"],
+};
