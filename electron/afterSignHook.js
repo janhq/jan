32a02426857d5d2074357c4b56f0ec4ff39b9e4b
Louis <louis@jan.ai>
2023-10-05 14:47:46 +0700

    chore: notarize Jan app for macos distribution

---
+++ electron/afterSignHook.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
+var electron_notarize = require("electron-notarize");
+
+module.exports = async function (params) {
+  if (process.platform !== "darwin") {
+    return;
+  }
+
+  console.log("afterSign hook triggered", params);
+
+  let appId = "jan.ai.app";
+
+  let appPath = path.join(
+    params.appOutDir,
+    `${params.packager.appInfo.productFilename}.app`
+  );
+  if (!fs.existsSync(appPath)) {
+    console.log("skip");
+    return;
+  }
+
+  console.log(`Notarizing ${appId} found at ${appPath}`);
+
+  try {
+    await electron_notarize.notarize({
+      appBundleId: appId,
+      appPath: appPath,
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_ID_PASSWORD,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
+  console.log(`Done notarizing ${appId}`);
+};
