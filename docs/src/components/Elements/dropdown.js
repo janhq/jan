6ec63bdb75f680dab83d368670223acd15ff5ffa
0xSage <n@pragmatic.vc>
2023-10-06 12:02:53 +0800

    feat: download btn detects os

--- docs/src/components/Elements/dropdown.js
+++ docs/src/components/Elements/dropdown.js
@@ -3,7 +3,7 @@ import { Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
-const items = [
+const systems = [
   {
     name: "Download for Mac (M1/M2)",
     href: "https://github.com/janhq/jan/releases/download/v0.1.2/Jan-0.1.2-arm64.dmg",
@@ -31,19 +31,32 @@ function classNames(...classes) {
 }
 
 export default function Dropdown() {
+  const uAgent = window.navigator.userAgent;
+  let defaultSystem;
+
+  if (uAgent.indexOf("Win") !== -1) {
+    defaultSystem = systems[2];
+  } else if (uAgent.indexOf("Mac") !== -1) {
+    // Note: There's no way to detect ARM architecture from browser. Hardcoding to M1/M2 for now.
+    defaultSystem = systems[0];
+  } else if (uAgent.indexOf("Linux") !== -1) {
+    defaultSystem = systems[3];
+  } else {
+    defaultSystem = systems[0];
+  }
+
   return (
     <div className="inline-flex align-items-stretch">
-      {/* TODO dynamically detect users OS through browser */}
       <a
         className="cursor-pointer relative inline-flex items-center rounded-l-md border-0 px-3.5 py-2.5 text-base font-semibold text-white bg-indigo-600 dark:bg-indigo-500 hover:bg-indigo-500 dark:hover:bg-indigo-400 hover:text-white"
-        href={items[0].href}
+        href={defaultSystem.href}
       >
         <img
           src={require("@site/static/img/apple-logo-white.png").default}
           alt="Logo"
           className="h-5 mr-3 -mt-1"
         />
-        Download for Mac (Silicon)
+        {defaultSystem.name}
       </a>
       <Menu as="div" className="relative -ml-px block">
         <Menu.Button className="cursor-pointer relative inline-flex items-center rounded-r-md border-0 border-l border-gray-300 active:border-l active:border-white h-full text-white bg-indigo-600 dark:bg-indigo-500 hover:bg-indigo-500 dark:hover:bg-indigo-400">
@@ -61,11 +74,11 @@ export default function Dropdown() {
         >
           <Menu.Items className="absolute right-0 z-10 mt-2 w-72 text-left origin-top-right rounded-md bg-indigo-600 dark:bg-indigo-500 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
             <div className="py-1">
-              {items.map((item) => (
-                <Menu.Item key={item.name}>
+              {systems.map((system) => (
+                <Menu.Item key={system.name}>
                   {({ active }) => (
                     <a
-                      href={item.href}
+                      href={system.href}
                       className={classNames(
                         active
                           ? "bg-indigo-500 dark:hover:bg-indigo-400 hover:text-white"
@@ -74,11 +87,11 @@ export default function Dropdown() {
                       )}
                     >
                       <img
-                        src={item.logo}
+                        src={system.logo}
                         alt="Logo"
                         className="w-3 mr-3 -mt-1"
                       />
-                      {item.name}
+                      {system.name}
                     </a>
                   )}
                 </Menu.Item>
