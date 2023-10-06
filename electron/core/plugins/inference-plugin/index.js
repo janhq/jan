3d9b6663d2877c97e22436503c5ff8ae692df502
namvuong <22463238+vuonghoainam@users.noreply.github.com>
2023-10-06 16:22:52 +0700

    Chore/update nitro d09bae4 (#282)
    * fix(nitro): Update config for new nitro
    
    * fix(nitro): Update new name mapping
    
    * feat(nitro): Update new version d09bae4
    
    * chore(nitro): Update windows to new version
    
    * fix: Update path to none
    
    Co-authored-by: Louis <133622055+louis-jan@users.noreply.github.com>
    
    * fix: Update inline conditional for binary selection re: platform
    
    Co-authored-by: Louis <133622055+louis-jan@users.noreply.github.com>
    
    * fix(nitro): Update port for nitro webserver
    
    ---------
    
    Co-authored-by: Louis <133622055+louis-jan@users.noreply.github.com>

--- electron/core/plugins/inference-plugin/index.js
+++ electron/core/plugins/inference-plugin/index.js
@@ -17,7 +17,7 @@ const dispose = async () =>
         .then((res) => resolve(res));
     }
   });
-const inferenceUrl = () => "http://localhost:8080/llama/chat_completion";
+const inferenceUrl = () => "http://localhost:3928/llama/chat_completion";
 
 // Register all the above functions and objects with the relevant extension points
 export function init({ register }) {
