7815a334065945dd227d0cdcf92ab8be2c576665
0xSage <n@pragmatic.vc>
2023-10-10 14:55:03 +0800

    fix: remove crap not related to pr

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -217,22 +217,4 @@ const sidebars = {
   ],
 };
 
-module.exports = {
-  docs: [
-    {
-      type: "category",
-      label: "Overview",
-      link: {
-        type: "generated-index",
-        title: "Hardware Guides",
-        description: "Learn about the most important Docusaurus concepts!",
-        slug: "/category/docusaurus-guides",
-        keywords: ["guides"],
-        image: "/img/docusaurus.png",
-      },
-      items: ["pages", "docs", "blog", "search"],
-    },
-  ],
-};
-
 module.exports = sidebars;
