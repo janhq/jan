a6a4948aa4cf05267b0c3d5dcabea1c48680eb02
Louis <louis@jan.ai>
2023-10-16 09:10:52 +0700

    chore: remove dom component from core

--- plugin-core/preferences.ts
+++ plugin-core/preferences.ts
@@ -55,9 +55,11 @@ function clear(pluginName: string): Promise<void> {
  * @param defaultValue The default value of the preference.
  */
 function registerPreferences<T>(register: Function, pluginName: string, preferenceName: string, defaultValue: T) {
-  register("PluginPreferences", `${pluginName}.${preferenceName}`, () =>
-    experimentComponent(pluginName, preferenceName, defaultValue)
-  );
+  register("PluginPreferences", `${pluginName}.${preferenceName}`, () => ({
+    pluginName,
+    preferenceName,
+    defaultValue,
+  }));
 }
 
 /**
@@ -68,50 +70,4 @@ export const preferences = {
   set,
   clear,
   registerPreferences,
-};
-
-// TODO: Better CSS styling
-const experimentComponent = async (pluginName: string, preferenceName: string, defaultValue: any) => {
-  var parent = document.createElement("div");
-  parent.style.marginTop = "16px";
-  const label = document.createElement("p");
-  label.style.marginTop = "5px";
-  label.style.fontSize = "16px";
-  label.innerText = `Setting ${preferenceName}:`;
-  parent.appendChild(label);
-
-  const form = document.createElement("form");
-  form.id = "Set key value";
-  form.addEventListener("submit", (e: any) => {
-    e.preventDefault();
-    const value = new FormData(e.target).get("value");
-    set(pluginName, preferenceName, value);
-  });
-  const input = document.createElement("input");
-  input.name = "value";
-  input.defaultValue = (await preferences.get(pluginName, preferenceName)) || defaultValue;
-  input.style.marginTop = "3px";
-  input.style.padding = "5px";
-  input.style.paddingLeft = "12px";
-  input.style.borderRadius = "5px";
-  input.style.borderColor = "#CBD5E0";
-  input.style.borderWidth = "1px";
-  input.style.marginRight = "10px";
-  input.style.width = "40%";
-  input.style.fontSize = "14px";
-  input.style.color = "#9ca3af";
-
-  form.appendChild(input);
-  const button = document.createElement("button");
-  button.type = "submit";
-  button.innerText = "Save";
-  button.style.backgroundColor = "#3b82f6";
-  button.style.color = "white";
-  button.style.padding = "5px 10px";
-  button.style.border = "none";
-  button.style.borderRadius = "5px";
-  form.appendChild(button);
-
-  parent.appendChild(form);
-  return parent;
 };
