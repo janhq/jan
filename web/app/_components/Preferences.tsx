84509247a70a109dc339bdcbff16ebe26df3c4c6
Louis <louis@jan.ai>
2023-10-17 14:39:34 +0700

    chore: display origin plugin name

--- web/app/_components/Preferences.tsx
+++ web/app/_components/Preferences.tsx
@@ -227,8 +227,8 @@ export const Preferences = () => {
                       <img className="h-14 w-14 rounded-md" src={e.icon ?? "icons/app_icon.svg"} alt="" />
                     </span>
                     <div className="flex flex-col">
-                      <p className="text-xl font-bold tracking-tight text-gray-900 dark:text-white capitalize">
-                        {e.name.replaceAll("-", " ")}
+                      <p className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        {e.name}
                       </p>
                       <p className="font-normal text-gray-700 dark:text-gray-400">Version: {e.version}</p>
                     </div>
