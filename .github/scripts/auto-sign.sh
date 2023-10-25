b92e6bf2d1b7922244dc56b53e6bb19f047b1624
Service Account <service@jan.ai>
2023-10-25 12:07:07 +0700

    Temporary disable electron build code sign from p12 file

--- .github/scripts/auto-sign.sh
+++ .github/scripts/auto-sign.sh
@@ -2,4 +2,4 @@
 
 APP_PATH=${APP_PATH}
 DEVELOPER_ID=${DEVELOPER_ID}
-find $APP_PATH -type f \( -perm +111 -o -perm +644 \) -exec codesign -s "$DEVELOPER_ID" --options=runtime {} \;
+find $APP_PATH \( -type f -perm +111 -o -name "*.node" \) -exec codesign -s "$DEVELOPER_ID" --options=runtime {} \;
