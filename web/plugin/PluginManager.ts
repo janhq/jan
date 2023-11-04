0bc64e1c71d7f8bda3ec6afa5a9b0c191e842100
Louis <louis@jan.ai>
2023-11-04 08:00:20 +0700

    chore: add facades

--- web/plugin/PluginManager.ts
+++ web/plugin/PluginManager.ts
@@ -5,6 +5,7 @@ import Plugin from './Plugin'
  * Manages the registration and retrieval of plugins.
  */
 export class PluginManager {
+  // MARK: - Plugin Manager
   private plugins = new Map<PluginType, JanPlugin>()
 
   /**
@@ -51,7 +52,28 @@ export class PluginManager {
   }
 
   /**
+   * Register a plugin with its class.
+   * @param {Plugin} plugin plugin object as provided by the main process.
+   * @returns {void}
+   */
+  async activatePlugin(plugin: Plugin) {
+    if (plugin.url)
+      // Import class
+      await import(/* webpackIgnore: true */ plugin.url).then((pluginClass) => {
+        // Register class if it has a default export
+        if (
+          typeof pluginClass.default === 'function' &&
+          pluginClass.default.prototype
+        ) {
+          this.register(new pluginClass.default())
+        }
+      })
+  }
+
+  // MARK: - Plugin Facades
+  /**
    * Registers all active plugins.
+   * @returns {void}
    */
   async registerActive() {
     // Get active plugins
@@ -68,20 +90,48 @@ export class PluginManager {
           plugin.icon
         )
     )
-    // Loop over and find import url
-    plugins.forEach((plugin: Plugin) => {
-      if (plugin.url)
-        // Import class
-        import(/* webpackIgnore: true */ plugin.url).then((pluginClass) => {
-          // Register class if it has a default export
-          if (
-            typeof pluginClass.default === 'function' &&
-            pluginClass.default.prototype
-          ) {
-            this.register(new pluginClass.default())
-          }
-        })
+    // Activate all
+    await Promise.all(
+      plugins.map((plugin: Plugin) => this.activatePlugin(plugin))
+    )
+  }
+
+  /**
+   * Install a new plugin.
+   * @param {Array.<installOptions | string>} plugins A list of NPM specifiers, or installation configuration objects.
+   * @returns {Promise.<Array.<Plugin> | false>} plugin as defined by the main process. Has property cancelled set to true if installation was cancelled in the main process.
+   * @alias plugins.install
+   */
+  async install(plugins: any[]) {
+    if (typeof window === 'undefined') {
+      return
+    }
+    const plgList = await window.pluggableElectronIpc?.install(plugins)
+    if (plgList.cancelled) return false
+    return plgList.map(async (plg: any) => {
+      const plugin = new Plugin(
+        plg.name,
+        plg.url,
+        plg.activationPoints,
+        plg.active
+      )
+      await this.activatePlugin(plugin)
+      return plugin
     })
+  }
+
+  /**
+   * Uninstall provided plugins
+   * @param {Array.<string>} plugins List of names of plugins to uninstall.
+   * @param {boolean} reload Whether to reload all renderers after updating the plugins.
+   * @returns {Promise.<boolean>} Whether uninstalling the plugins was successful.
+   * @alias plugins.uninstall
+   */
+  uninstall(plugins: string[], reload = true) {
+    if (typeof window === 'undefined') {
+      return
+    }
+    return window.pluggableElectronIpc?.uninstall(plugins, reload)
   }
 }
 
