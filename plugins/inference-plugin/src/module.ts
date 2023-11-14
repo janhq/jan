7c2c1a2b3d266c90952f225082e74fc9982831ef
Louis <louis@jan.ai>
2023-11-14 16:58:52 +0700

    chore: validate model status

--- plugins/inference-plugin/src/module.ts
+++ plugins/inference-plugin/src/module.ts
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const kill = require("kill-port");
 const path = require("path");
-const { app } = require("electron");
 const { spawn } = require("child_process");
 const tcpPortUsed = require("tcp-port-used");
+const fetchRetry = require("fetch-retry")(global.fetch);
 
 // The PORT to use for the Nitro subprocess
 const PORT = 3928;
@@ -11,9 +11,11 @@ const LOCAL_HOST = "127.0.0.1";
 const NITRO_HTTP_SERVER_URL = `http://${LOCAL_HOST}:${PORT}`;
 const NITRO_HTTP_LOAD_MODEL_URL = `${NITRO_HTTP_SERVER_URL}/inferences/llamacpp/loadmodel`;
 const NITRO_HTTP_UNLOAD_MODEL_URL = `${NITRO_HTTP_SERVER_URL}/inferences/llamacpp/unloadModel`;
+const NITRO_HTTP_VALIDATE_MODEL_URL = `${NITRO_HTTP_SERVER_URL}/inferences/llamacpp/modelstatus`;
 
 // The subprocess instance for Nitro
 let subprocess = null;
+let currentModelFile = null;
 
 /**
  * The response from the initModel function.
@@ -25,75 +27,85 @@ interface InitModelResponse {
 
 /**
  * Initializes a Nitro subprocess to load a machine learning model.
- * @param fileName - The name of the machine learning model file.
+ * @param modelFile - The name of the machine learning model file.
  * @returns A Promise that resolves when the model is loaded successfully, or rejects with an error message if the model is not found or fails to load.
  * TODO: Should pass absolute of the model file instead of just the name - So we can modurize the module.ts to npm package
  * TODO: Should it be startModel instead?
  */
-function initModel(fileName: string): Promise<InitModelResponse> {
+function initModel(modelFile: string): Promise<InitModelResponse> {
   // 1. Check if the model file exists
+  currentModelFile = modelFile;
+
   return (
-    checkModelFileExist(fileName)
-      // 2. Check if the port is used, if used, attempt to unload model / kill nitro process
-      .then(checkAndUnloadNitro)
-      // 3. Spawn the Nitro subprocess
+    // 1. Check if the port is used, if used, attempt to unload model / kill nitro process
+    checkAndUnloadNitro()
+      // 2. Spawn the Nitro subprocess
       .then(spawnNitroProcess)
-      // 4. Wait until the port is used (Nitro http server is up)
+      // 3. Wait until the port is used (Nitro http server is up)
       .then(() => tcpPortUsed.waitUntilUsed(PORT, 300, 30000))
-      // 5. Load the model into the Nitro subprocess (HTTP POST request)
-      .then(() => loadLLMModel(fileName))
-      // 6. Check if the model is loaded successfully
-      .then(async (res) => {
-        if (res.ok) {
-          // Success - Model loaded
-          return {};
-        }
-        const json = await res.json();
-        throw new Error(`${json?.message ?? "Model loading failed."}`);
-      })
-      .catch((err) => {
-        return { error: err };
-      })
+      // 4. Load the model into the Nitro subprocess (HTTP POST request)
+      .then(loadLLMModel)
+      // 5. Check if the model is loaded successfully
+      .then(validateModelStatus)
   );
 }
 
 /**
  * Loads a LLM model into the Nitro subprocess by sending a HTTP POST request.
- * @param fileName - The name of the model file.
  * @returns A Promise that resolves when the model is loaded successfully, or rejects with an error message if the model is not found or fails to load.
  */
-function loadLLMModel(fileName: string): Promise<Response> {
-  const llama_model_path = path.join(appPath(), fileName);
-
+function loadLLMModel(): Promise<Response> {
   const config = {
-    llama_model_path,
+    llama_model_path: currentModelFile,
     ctx_len: 2048,
     ngl: 100,
     embedding: false, // Always enable embedding mode on
   };
 
   // Load model config
-  return fetch(NITRO_HTTP_LOAD_MODEL_URL, {
+  return fetchRetry(NITRO_HTTP_LOAD_MODEL_URL, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(config),
+    retries: 3,
+    retryDelay: 500,
   });
 }
 
 /**
- * Checks if the model file exists.
- * @param fileName - The name of the model file.
- * @returns A Promise that resolves when the model file exists, or rejects with an error message if the model file does not exist.
+ * Validates the status of a model.
+ * @returns {Promise<InitModelResponse>} A promise that resolves to an object.
+ * If the model is loaded successfully, the object is empty.
+ * If the model is not loaded successfully, the object contains an error message.
  */
-function checkModelFileExist(fileName: string): Promise<string> {
-  return new Promise<string>(async (resolve, reject) => {
-    if (!fileName) {
-      reject("Model not found, please download again.");
-    }
-    resolve(fileName);
-  });
+async function validateModelStatus(): Promise<InitModelResponse> {
+  // Send a GET request to the validation URL.
+  // Retry the request up to 3 times if it fails, with a delay of 500 milliseconds between retries.
+  return fetchRetry(NITRO_HTTP_VALIDATE_MODEL_URL, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    retries: 5,
+    retryDelay: 500,
+  })
+    .then(async (res: Response) => {
+      // If the response is OK, check model_loaded status.
+      if (res.ok) {
+        const body = await res.json();
+        // If the model is loaded, return an empty object.
+        // Otherwise, return an object with an error message.
+        if (body.model_loaded) {
+          return { error: undefined };
+        }
+      }
+      return { error: "Model is not loaded successfully" };
+    })
+    .catch((err) => {
+      return { error: `Model is not loaded successfully. ${err.message}` };
+    });
 }
 
 /**
@@ -108,14 +120,6 @@ function killSubprocess(): Promise<void> {
   } else {
     return kill(PORT, "tcp").then(console.log).catch(console.log);
   }
-}
-
-/**
- * Returns the path to the user data directory.
- * @returns The path to the user data directory.
- */
-function appPath() {
-  return app.getPath("userData");
 }
 
 /**
