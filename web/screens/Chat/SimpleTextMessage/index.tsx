40ad0fe72bb2c9193083d8a4275c7027625fcdee
Louis <louis@jan.ai>
2023-11-24 15:05:11 +0700

    feat: message actions tooltip

--- web/screens/Chat/SimpleTextMessage/index.tsx
+++ web/screens/Chat/SimpleTextMessage/index.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React from 'react'
+import React, { useContext } from 'react'
 
-import { ChatCompletionRole, ThreadMessage } from '@janhq/core'
+import { ChatCompletionRole, MessageStatus, ThreadMessage } from '@janhq/core'
+
 import hljs from 'highlight.js'
 
+import { MoreVertical } from 'lucide-react'
 import { Marked } from 'marked'
 
 import { markedHighlight } from 'marked-highlight'
@@ -14,7 +16,11 @@ import LogoMark from '@/containers/Brand/Logo/Mark'
 
 import BubbleLoader from '@/containers/Loader/Bubble'
 
+import { FeatureToggleContext } from '@/context/FeatureToggle'
+
 import { displayDate } from '@/utils/datetime'
+
+import MessageToolbar from '../MessageToolbar'
 
 const marked = new Marked(
   markedHighlight({
@@ -42,12 +48,13 @@ const marked = new Marked(
 )
 
 const SimpleTextMessage: React.FC<ThreadMessage> = (props) => {
+  const { experimentalFeatureEnabed } = useContext(FeatureToggleContext)
   const parsedText = marked.parse(props.content ?? '')
   const isUser = props.role === ChatCompletionRole.User
   const isSystem = props.role === ChatCompletionRole.System
 
   return (
-    <div className="mx-auto rounded-xl px-4 lg:w-3/4">
+    <div className="group mx-auto rounded-xl px-4 lg:w-3/4">
       <div
         className={twMerge(
           'mb-1 flex items-center justify-start gap-2',
@@ -57,10 +64,17 @@ const SimpleTextMessage: React.FC<ThreadMessage> = (props) => {
         {!isUser && !isSystem && <LogoMark width={20} />}
         <div className="text-sm font-extrabold capitalize">{props.role}</div>
         <p className="text-xs font-medium">{displayDate(props.createdAt)}</p>
+
+        {experimentalFeatureEnabed && (
+          <div className="hidden cursor-pointer group-hover:flex">
+            <MessageToolbar message={props} />
+          </div>
+        )}
       </div>
 
       <div className={twMerge('w-full')}>
-        {!props.content || props.content === '' ? (
+        {props.status === MessageStatus.Pending &&
+        (!props.content || props.content === '') ? (
           <BubbleLoader />
         ) : (
           <>
