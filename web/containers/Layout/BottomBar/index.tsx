b8359d8358692d027183e1ed055beacbf5bbce17
Louis <louis@jan.ai>
2023-11-24 11:55:09 +0700

    fix: incorrect update progress bar visibility check

--- web/containers/Layout/BottomBar/index.tsx
+++ web/containers/Layout/BottomBar/index.tsx
@@ -31,7 +31,7 @@ const BottomBar = () => {
     <div className="fixed bottom-0 left-16 z-20 flex h-12 w-[calc(100%-64px)] items-center justify-between border-t border-border bg-background/50 px-3">
       <div className="flex flex-shrink-0 items-center gap-x-2">
         <div className="flex items-center space-x-2">
-          {!progress && progress === 0 ? (
+          {progress && progress > 0 ? (
             <ProgressBar total={100} used={progress} />
           ) : null}
         </div>
