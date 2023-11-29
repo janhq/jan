877498c331bbd03c108bfa07838a2e818f2191f5
Faisal Amir <urmauur@gmail.com>
2023-11-29 23:32:19 +0700

    Blank state when user have not any model

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="bg-background/50 font-sans text-sm antialiased">
+      <body className="bg-background/50 font-sans antialiased">
         <div className="title-bar" />
         <Providers>{children}</Providers>
       </body>
