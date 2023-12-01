a5a71d792e5786a31cdc499988060bfee0b06c30
hieu-jan <150573299+hieu-jan@users.noreply.github.com>
2023-12-01 22:50:03 +0900

    improve SEO

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -166,7 +166,7 @@ const config = {
         },
         innerHTML: JSON.stringify({
           '@context': 'https://schema.org/',
-          '@type': 'LLMInference',
+          '@type': 'localAI',
           name: 'Jan',
           description: "Jan is a ChatGPT-alternative that runs on your own computer, with a local API server.",
           keywords: "Jan, ChatGPT alternative, on-premises AI, local API server, local AI, llm, conversational AI, no-subscription fee",
