09fe295c8523f50be00f33ad7875d5259eb7b819
hiento09 <136591877+hiento09@users.noreply.github.com>
2023-12-04 16:50:03 +0700

    Hotfix jan windows download nitro failed (#838)
    Co-authored-by: Hien To <tominhhien97@gmail.com>

--- extensions/inference-extension/download.bat
+++ extensions/inference-extension/download.bat
@@ -1,4 +1,3 @@
 @echo off
 set /p NITRO_VERSION=<./nitro/version.txt
-.\node_modules\.bin\download https://github.com/janhq/nitro/releases/download/v%NITRO_VERSION%/nitro-%NITRO_VERSION%-win-amd64-cuda.tar.gz -e --strip 1 -o ./nitro/win-cuda
-.\node_modules\.bin\download https://github.com/janhq/nitro/releases/download/v%NITRO_VERSION%/nitro-%NITRO_VERSION%-win-amd64.tar.gz -e --strip 1 -o ./nitro/win-cpu
+.\node_modules\.bin\download https://github.com/janhq/nitro/releases/download/v%NITRO_VERSION%/nitro-%NITRO_VERSION%-win-amd64-cuda.tar.gz -e --strip 1 -o ./nitro/win-cuda && .\node_modules\.bin\download https://github.com/janhq/nitro/releases/download/v%NITRO_VERSION%/nitro-%NITRO_VERSION%-win-amd64.tar.gz -e --strip 1 -o ./nitro/win-cpu
