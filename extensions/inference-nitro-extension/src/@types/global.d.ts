b970e978693fae6a6b461a19c21afae962e1d594
hiro <vuonghoainam.work@gmail.com>
2023-12-05 00:29:48 +0700

    chore: Move interface definition location

--- extensions/inference-nitro-extension/src/@types/global.d.ts
+++ extensions/inference-nitro-extension/src/@types/global.d.ts
@@ -1,2 +1,9 @@
 declare const MODULE: string;
 declare const INFERENCE_URL: string;
+
+interface EngineSettings {
+    ctx_len: number;
+    ngl: number;
+    cont_batching: boolean;
+    embedding: boolean;
+}
