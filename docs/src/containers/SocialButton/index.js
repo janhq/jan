0de94a5b365649848fa33a2135d08f6cef2355cd
Faisal Amir <urmauur@gmail.com>
2023-12-06 00:20:26 +0700

    Revamp landing page

--- docs/src/containers/SocialButton/index.js
+++ docs/src/containers/SocialButton/index.js
@@ -7,7 +7,7 @@ export default function SocialButton() {
       <a
         href="https://discord.gg/FTk2MvZwJH"
         target="_blank"
-        className="inline-flex px-4 py-3 rounded-lg text-lg font-semibold cursor-pointer justify-center items-center space-x-2 border border-gray-400 dark:border-gray-700 text-gray-600 dark:text-white"
+        className="text-white bg-blue-600 hover:bg-blue-700 hover:text-white inline-flex px-4 py-3 rounded-lg text-lg font-semibold cursor-pointer justify-center items-center space-x-2"
       >
         <span>
           <FaDiscord className="text-xl" />
@@ -17,7 +17,7 @@ export default function SocialButton() {
       <a
         href="https://github.com/janhq/jan"
         target="_blank"
-        className="text-white bg-blue-600 hover:bg-blue-700 hover:text-white inline-flex px-4 py-3 rounded-lg text-lg font-semibold cursor-pointer justify-center items-center space-x-2"
+        className="inline-flex px-4 py-3 rounded-lg text-lg font-semibold cursor-pointer justify-center items-center space-x-2 border border-gray-400 dark:border-gray-700 text-white bg-black hover:text-white"
       >
         <span>
           <FaGithub className="text-lg" />
