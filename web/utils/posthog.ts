f04cf0c4ed69528f13d36b92ac5f423af5fce108
Louis <louis@jan.ai>
2023-12-13 19:49:26 +0700

    chore: add desktop app analytics

---
+++ web/utils/posthog.ts
@@ -0,0 +1,15 @@
+import posthog, { Properties } from 'posthog-js'
+
+posthog.init(ANALYTICS_ID, {
+  api_host: ANALYTICS_HOST,
+})
+
+export const instance = posthog
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const trackEvent = (name: string, properties?: Properties) => {
+  posthog.capture(name, properties)
+}
+
+export enum AnalyticsEvent {
+}
