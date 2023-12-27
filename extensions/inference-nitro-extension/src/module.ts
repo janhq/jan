0ad04fe0997b2b275f0cd70aa773d2d1f5c2d2b9
Hien To <tominhhien97@gmail.com>
2023-12-27 13:39:28 +0700

    Add js function to generate gpu and cuda detection

--- extensions/inference-nitro-extension/src/module.ts
+++ extensions/inference-nitro-extension/src/module.ts
@@ -1,9 +1,11 @@
 const fs = require("fs");
+const fsPromises = fs.promises;
 const path = require("path");
-const { spawn } = require("child_process");
+const { exec, spawn } = require("child_process");
 const tcpPortUsed = require("tcp-port-used");
 const fetchRetry = require("fetch-retry")(global.fetch);
 const si = require("systeminformation");
+const { readFileSync, writeFileSync, existsSync  } = require('fs');
 
 // The PORT to use for the Nitro subprocess
 const PORT = 3928;
@@ -14,6 +16,7 @@ const NITRO_HTTP_UNLOAD_MODEL_URL = `${NITRO_HTTP_SERVER_URL}/inferences/llamacp
 const NITRO_HTTP_VALIDATE_MODEL_URL = `${NITRO_HTTP_SERVER_URL}/inferences/llamacpp/modelstatus`;
 const NITRO_HTTP_KILL_URL = `${NITRO_HTTP_SERVER_URL}/processmanager/destroy`;
 const SUPPORTED_MODEL_FORMAT = ".gguf";
+const NVIDIA_INFO_FILE = path.join(__dirname, "bin", "nvidia.json");
 
 // The subprocess instance for Nitro
 let subprocess = undefined;
@@ -27,6 +30,79 @@ let currentSettings = undefined;
  */
 function stopModel(): Promise<void> {
   return killSubprocess();
+}
+
+/**
+ * Validate nvidia and cuda for linux and windows
+ */
+function updateNvidiaDriverInfo() {
+  exec('nvidia-smi --query-gpu=driver_version --format=csv,noheader', (error, stdout) => {
+      let data = JSON.parse(readFileSync(NVIDIA_INFO_FILE, 'utf8'));
+
+      if (!error) {
+          const firstLine = stdout.split('\n')[0].trim();
+          data['nvidia-driver'].exist = true;
+          data['nvidia-driver'].version = firstLine;
+      } else {
+          data['nvidia-driver'].exist = false;
+      }
+
+      writeFileSync(NVIDIA_INFO_FILE, JSON.stringify(data, null, 2));
+  });
+}
+
+
+function checkFileExistenceInPaths(file: string, paths: string[]): boolean {
+  return paths.some(p => existsSync(path.join(p, file)));
+}
+
+function updateCudaExistence() {
+  let files: string[];
+  let paths: string[];
+
+  if (process.platform === 'win32') {
+      files = ['cublas64_12.dll', 'cudart64_12.dll', 'cublasLt64_12.dll'];
+      paths = process.env.PATH ? process.env.PATH.split(path.delimiter) : [];
+      const nitro_cuda_path = path.join(__dirname, "bin", "win-cuda");
+      paths.push(nitro_cuda_path);
+  } else {
+      files = ['libcudart.so.12', 'libcublas.so.12', 'libcublasLt.so.12'];
+      paths = process.env.LD_LIBRARY_PATH ? process.env.LD_LIBRARY_PATH.split(path.delimiter) : [];
+      const nitro_cuda_path = path.join(__dirname, "bin", "linux-cuda");
+      paths.push(nitro_cuda_path);
+  }
+
+  let cudaExists = files.every(file => existsSync(file) || checkFileExistenceInPaths(file, paths));
+
+  let data = JSON.parse(readFileSync(NVIDIA_INFO_FILE, 'utf8'));
+  data['cuda'].exist = cudaExists;
+  writeFileSync(NVIDIA_INFO_FILE, JSON.stringify(data, null, 2));
+}
+
+function updateGpuInfo() {
+  exec('nvidia-smi --query-gpu=index,memory.total --format=csv,noheader,nounits', (error, stdout) => {
+      let data = JSON.parse(readFileSync(NVIDIA_INFO_FILE, 'utf8'));
+
+      if (!error) {
+          let gpus = stdout.trim().split('\n').map(line => {
+              let [id, vram] = line.split(', ');
+              vram = vram.replace(/\r/g, '');
+              return { id, vram };
+          });
+
+          data['gpus'] = gpus;
+      } else {
+          data['gpus'] = [];
+      }
+
+      writeFileSync(NVIDIA_INFO_FILE, JSON.stringify(data, null, 2));
+  });
+}
+
+function updateNvidiaInfo() {
+  updateNvidiaDriverInfo();
+  updateCudaExistence();
+  updateGpuInfo();
 }
 
 /**
@@ -223,6 +299,7 @@ async function killSubprocess(): Promise<void> {
  * Should run exactly platform specified Nitro binary version
  */
 function spawnNitroProcess(nitroResourceProbe: any): Promise<any> {
+  updateNvidiaInfo();
   console.debug("Starting Nitro subprocess...");
   return new Promise(async (resolve, reject) => {
     let binaryFolder = path.join(__dirname, "bin"); // Current directory by default
@@ -296,4 +373,5 @@ module.exports = {
   stopModel,
   killSubprocess,
   dispose,
+  updateNvidiaInfo,
 };
