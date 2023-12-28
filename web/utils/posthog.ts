84e1c6d361cd59403899a6078eb35d85a6a8b5fa
Ho Duc Hieu <150573299+hieu-jan@users.noreply.github.com>
2023-12-28 15:25:29 +0700

    feat: filtered posthog properties

--- web/utils/posthog.ts
+++ web/utils/posthog.ts
@@ -9,9 +9,40 @@ posthog.init(ANALYTICS_ID, {
 
 export const instance = posthog
 
+// Function to remove unnecessary properties
+const filterProperties = (properties?: Properties): Properties | undefined => {
+  if (!properties) {
+    return undefined;
+  }
+
+  // Create a shallow copy of the properties object
+  const filteredProperties = { ...properties };
+
+  // Remove unwanted properties
+  delete filteredProperties['$ip'];
+  delete filteredProperties['$referrer'];
+  delete filteredProperties['$initial_referrer'];
+  delete filteredProperties['$referring_domain'];
+  delete filteredProperties['$initial_referring_domain'];
+  delete filteredProperties['$pathname'];
+  delete filteredProperties['$initial_pathname'];
+  delete filteredProperties['$browser_language'];
+  delete filteredProperties['$current_url'];
+  delete filteredProperties['$initial_current_url'];
+  delete filteredProperties['$browser'];
+  delete filteredProperties['$initial_browser'];
+  delete filteredProperties['$browser_version'];
+  delete filteredProperties['$initial_browser_version'];
+
+  return filteredProperties;
+}
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const trackEvent = (name: string, properties?: Properties) => {
-  posthog.capture(name, properties)
+  // Filter out unwanted properties before sending the event
+  const filteredProperties = filterProperties(properties);
+
+  posthog.capture(name, filteredProperties);
 }
 
 export enum AnalyticsEvent {}
