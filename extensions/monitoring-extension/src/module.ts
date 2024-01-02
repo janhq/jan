a15f8c6868fbd1ae7e9777344071ef2794c0e1d1
Service Account <service@jan.ai>
2024-01-03 01:21:59 +0700

    Fix memory on mac included cached and swap

--- extensions/monitoring-extension/src/module.ts
+++ extensions/monitoring-extension/src/module.ts
@@ -1,25 +1,25 @@
 const os = require("os");
-const osUtils = require("os-utils");
+const nodeOsUtils = require("node-os-utils");
 
 const getResourcesInfo = () =>
   new Promise((resolve) => {
-    const totalMemory = os.totalmem();
-    const freeMemory = os.freemem();
-    const usedMemory = totalMemory - freeMemory;
-
-    const response = {
-      mem: {
-        totalMemory,
-        usedMemory,
-      },
-    };
-    resolve(response);
+    nodeOsUtils.mem.used()
+      .then(ramUsedInfo => {
+        const totalMemory = ramUsedInfo.totalMemMb * 1024 * 1024;
+        const usedMemory = ramUsedInfo.usedMemMb * 1024 * 1024;
+        const response = {
+          mem: {
+            totalMemory,
+            usedMemory,
+          },
+        };
+        resolve(response);
+      })
   });
 
 const getCurrentLoad = () =>
   new Promise((resolve) => {
-    osUtils.cpuUsage(function(v){
-      const cpuPercentage = v * 100;
+    nodeOsUtils.cpu.usage().then(cpuPercentage =>{
       const response = {
         cpu: {
           usage: cpuPercentage,
