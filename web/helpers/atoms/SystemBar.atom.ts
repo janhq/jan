32b95a7966d160dbd196efdcea5c7c7d0efd52ca
hiro <vuonghoainam.work@gmail.com>
2024-01-06 15:58:59 +0700

    feat" Add systemBar atom

--- web/helpers/atoms/SystemBar.atom.ts
+++ web/helpers/atoms/SystemBar.atom.ts
@@ -4,3 +4,6 @@ export const totalRamAtom = atom<number>(0)
 export const usedRamAtom = atom<number>(0)
 
 export const cpuUsageAtom = atom<number>(0)
+
+export const nvidiaGpuUsageAtom = atom<number>(0)
+export const nvidiaVramUsageAtom = atom<number>(0)
