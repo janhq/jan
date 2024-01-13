66fd0829db4b2013b29fa5a1ddd323e1bcccc27e
Faisal Amir <urmauur@gmail.com>
2024-01-13 20:40:30 +0700

    feat: added API server dashboard

---
+++ web/screens/LocalServer/index.tsx
@@ -0,0 +1,11 @@
+const LocalServerScreen = () => {
+  return (
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum labore,
+      neque, voluptatum necessitatibus est commodi perferendis, quo ea alias
+      rerum facilis! Optio commodi quae vero eius sint iusto illum est?
+    </p>
+  )
+}
+
+export default LocalServerScreen
