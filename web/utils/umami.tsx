7de325456bb2938fb843d98e98276a151e515786
Hieu Ho <150573299+hieu-jan@users.noreply.github.com>
2024-01-15 21:17:54 +0700

    feat: format umami.tsx

--- web/utils/umami.tsx
+++ web/utils/umami.tsx
@@ -1,8 +1,8 @@
-import Script from 'next/script'
-import { useEffect } from 'react'
+import Script from 'next/script';
+import { useEffect } from 'react';
 
 const Umami = () => {
-  const appVersion = VERSION
+  const appVersion = VERSION;
 
   useEffect(() => {
     const ping = () => {
@@ -10,33 +10,46 @@ const Umami = () => {
       if (typeof umami !== 'undefined') {
         umami.track(appVersion, {
           version: appVersion,
-        })
+        });
       }
-    }
+    };
 
     // Wait for umami to be defined before ping
     if (typeof umami !== 'undefined') {
-      ping()
+      ping();
     } else {
       // Listen for umami script load event
-      document.addEventListener('umami:loaded', ping)
+      document.addEventListener('umami:loaded', ping);
     }
 
     // Cleanup function to remove event listener if the component unmounts
     return () => {
-      document.removeEventListener('umami:loaded', ping)
-    }
-  }, [appVersion])
+      document.removeEventListener('umami:loaded', ping);
+    };
+  }, [appVersion]);
 
   return (
-    <Script
-      src="https://us.umami.is/script.js"
-      data-website-id="0d6d8b40-a7e8-4f1c-9d81-a28cfcbc28f4"
-      data-cache="true"
-      defer
-      onLoad={() => document.dispatchEvent(new Event('umami:loaded'))}
-    />
-  )
-}
+    <>
+      {/* Assuming umami script is loaded synchronously */}
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+            if (typeof umami === 'undefined') {
+              var umamiLoaded = new Event('umami:loaded');
+              document.dispatchEvent(umamiLoaded);
+            }
+          `,
+        }}
+      />
+      <Script
+        src="https://us.umami.is/script.js"
+        data-website-id="0d6d8b40-a7e8-4f1c-9d81-a28cfcbc28f4"
+        data-cache="true"
+        defer
+        onLoad={() => document.dispatchEvent(new Event('umami:loaded'))}
+      />
+    </>
+  );
+};
 
-export default Umami
+export default Umami;
