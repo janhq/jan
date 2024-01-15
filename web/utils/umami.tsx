6a4cfa99dbbea303e936e4a684af44320474e786
Hieu Ho <150573299+hieu-jan@users.noreply.github.com>
2024-01-15 21:31:46 +0700

    feat: clean code

--- web/utils/umami.tsx
+++ web/utils/umami.tsx
@@ -1,46 +1,19 @@
-import Script from 'next/script';
-import { useEffect } from 'react';
+import Script from 'next/script'
+import { useEffect } from 'react'
 
 const Umami = () => {
-  const appVersion = VERSION;
+  const appVersion = VERSION
 
   useEffect(() => {
     const ping = () => {
-      // Check if umami is defined before ping
-      if (typeof umami !== 'undefined') {
-        umami.track(appVersion, {
-          version: appVersion,
-        });
-      }
-    };
-
-    // Wait for umami to be defined before ping
-    if (typeof umami !== 'undefined') {
-      ping();
-    } else {
-      // Listen for umami script load event
-      document.addEventListener('umami:loaded', ping);
+      umami.track(appVersion, {
+        version: appVersion,
+      })
     }
-
-    // Cleanup function to remove event listener if the component unmounts
-    return () => {
-      document.removeEventListener('umami:loaded', ping);
-    };
-  }, [appVersion]);
+  }, [appVersion])
 
   return (
     <>
-      {/* Assuming umami script is loaded synchronously */}
-      <Script
-        dangerouslySetInnerHTML={{
-          __html: `
-            if (typeof umami === 'undefined') {
-              var umamiLoaded = new Event('umami:loaded');
-              document.dispatchEvent(umamiLoaded);
-            }
-          `,
-        }}
-      />
       <Script
         src="https://us.umami.is/script.js"
         data-website-id="0d6d8b40-a7e8-4f1c-9d81-a28cfcbc28f4"
@@ -49,7 +22,7 @@ const Umami = () => {
         onLoad={() => document.dispatchEvent(new Event('umami:loaded'))}
       />
     </>
-  );
-};
+  )
+}
 
-export default Umami;
+export default Umami
