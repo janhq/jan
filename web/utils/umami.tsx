98f2845e6b62aeade87429fb92b4ba389a36d68e
Hieu Ho <150573299+hieu-jan@users.noreply.github.com>
2024-01-15 21:06:18 +0700

    feat: integrate data-cache

--- web/utils/umami.tsx
+++ web/utils/umami.tsx
@@ -32,6 +32,7 @@ const Umami = () => {
     <Script
       src="https://us.umami.is/script.js"
       data-website-id="0d6d8b40-a7e8-4f1c-9d81-a28cfcbc28f4"
+      data-cache="true"
       defer
       onLoad={() => document.dispatchEvent(new Event('umami:loaded'))}
     />
