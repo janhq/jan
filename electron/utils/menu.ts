4326ee99f1e2e6e0e75c5e35d7334e611f0727cb
Louis <louis@jan.ai>
2024-01-16 16:45:32 +0700

    fix: enable check for update on all supported platforms

--- electron/utils/menu.ts
+++ electron/utils/menu.ts
@@ -1,110 +1,104 @@
 // @ts-nocheck
-import { app, Menu, dialog, shell } from "electron";
-const isMac = process.platform === "darwin";
-const { autoUpdater } = require("electron-updater");
-import { compareSemanticVersions } from "./versionDiff";
+import { app, Menu, dialog, shell } from 'electron'
+const isMac = process.platform === 'darwin'
+const { autoUpdater } = require('electron-updater')
+import { compareSemanticVersions } from './versionDiff'
 
 const template: (Electron.MenuItemConstructorOptions | Electron.MenuItem)[] = [
-  ...(isMac
-    ? [
-        {
-          label: app.name,
-          submenu: [
-            { role: "about" },
-            {
-              label: "Check for Updates...",
-              click: () =>
-                autoUpdater.checkForUpdatesAndNotify().then((e) => {
-                  if (
-                    !e ||
-                    compareSemanticVersions(
-                      app.getVersion(),
-                      e.updateInfo.version
-                    ) >= 0
-                  )
-                    dialog.showMessageBox({
-                      message: `There are currently no updates available.`,
-                    });
-                }),
-            },
-            { type: "separator" },
-            { role: "services" },
-            { type: "separator" },
-            { role: "hide" },
-            { role: "hideOthers" },
-            { role: "unhide" },
-            { type: "separator" },
-            { role: "quit" },
-          ],
-        },
-      ]
-    : []),
   {
-    label: "Edit",
+    label: app.name,
     submenu: [
-      { role: "undo" },
-      { role: "redo" },
-      { type: "separator" },
-      { role: "cut" },
-      { role: "copy" },
-      { role: "paste" },
+      { role: 'about' },
+      {
+        label: 'Check for Updates...',
+        click: () =>
+          autoUpdater.checkForUpdatesAndNotify().then((e) => {
+            if (
+              !e ||
+              compareSemanticVersions(app.getVersion(), e.updateInfo.version) >=
+                0
+            )
+              dialog.showMessageBox({
+                message: `There are currently no updates available.`,
+              })
+          }),
+      },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideOthers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' },
+    ],
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
       ...(isMac
         ? [
-            { role: "pasteAndMatchStyle" },
-            { role: "delete" },
-            { role: "selectAll" },
-            { type: "separator" },
+            { role: 'pasteAndMatchStyle' },
+            { role: 'delete' },
+            { role: 'selectAll' },
+            { type: 'separator' },
             {
-              label: "Speech",
-              submenu: [{ role: "startSpeaking" }, { role: "stopSpeaking" }],
+              label: 'Speech',
+              submenu: [{ role: 'startSpeaking' }, { role: 'stopSpeaking' }],
             },
           ]
-        : [{ role: "delete" }, { type: "separator" }, { role: "selectAll" }]),
+        : [{ role: 'delete' }, { type: 'separator' }, { role: 'selectAll' }]),
     ],
   },
   {
-    label: "View",
+    label: 'View',
     submenu: [
-      { role: "reload" },
-      { role: "forceReload" },
-      { role: "toggleDevTools" },
-      { type: "separator" },
-      { role: "resetZoom" },
-      { role: "zoomIn" },
-      { role: "zoomOut" },
-      { type: "separator" },
-      { role: "togglefullscreen" },
+      { role: 'reload' },
+      { role: 'forceReload' },
+      { role: 'toggleDevTools' },
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' },
     ],
   },
   {
-    label: "Window",
+    label: 'Window',
     submenu: [
-      { role: "minimize" },
-      { role: "zoom" },
+      { role: 'minimize' },
+      { role: 'zoom' },
       ...(isMac
         ? [
-            { type: "separator" },
-            { role: "front" },
-            { type: "separator" },
-            { role: "window" },
+            { type: 'separator' },
+            { role: 'front' },
+            { type: 'separator' },
+            { role: 'window' },
           ]
-        : [{ role: "close" }]),
+        : [{ role: 'close' }]),
     ],
   },
   {
-    role: "help",
+    role: 'help',
     submenu: [
       {
-        label: "Learn More",
+        label: 'Learn More',
         click: async () => {
-          await shell.openExternal("https://jan.ai/");
+          await shell.openExternal('https://jan.ai/')
         },
       },
     ],
   },
-];
+]
 
 export const setupMenu = () => {
-  const menu = Menu.buildFromTemplate(template);
-  Menu.setApplicationMenu(menu);
-};
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+}
