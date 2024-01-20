285d253c26c0400a7f2e8efafb05de78c077d0f2
hiro <vuonghoainam.work@gmail.com>
2024-01-20 23:25:19 +0700

    chore: reformat

--- core/src/types/model/modelInterface.ts
+++ core/src/types/model/modelInterface.ts
@@ -1,4 +1,4 @@
-import { Model } from './modelEntity'
+import { Model } from "./modelEntity";
 
 /**
  * Model extension for managing models.
@@ -10,38 +10,38 @@ export interface ModelInterface {
    * @param network - Optional object to specify proxy/whether to ignore SSL certificates.
    * @returns A Promise that resolves when the model has been downloaded.
    */
-  downloadModel(model: Model, network?: { ignoreSSL?: boolean, proxy?: string }): Promise<void>
+  downloadModel(model: Model, network?: { ignoreSSL?: boolean; proxy?: string }): Promise<void>;
 
   /**
    * Cancels the download of a specific model.
    * @param {string} modelId - The ID of the model to cancel the download for.
    * @returns {Promise<void>} A promise that resolves when the download has been cancelled.
    */
-  cancelModelDownload(modelId: string): Promise<void>
+  cancelModelDownload(modelId: string): Promise<void>;
 
   /**
    * Deletes a model.
    * @param modelId - The ID of the model to delete.
    * @returns A Promise that resolves when the model has been deleted.
    */
-  deleteModel(modelId: string): Promise<void>
+  deleteModel(modelId: string): Promise<void>;
 
   /**
    * Saves a model.
    * @param model - The model to save.
    * @returns A Promise that resolves when the model has been saved.
    */
-  saveModel(model: Model): Promise<void>
+  saveModel(model: Model): Promise<void>;
 
   /**
    * Gets a list of downloaded models.
    * @returns A Promise that resolves with an array of downloaded models.
    */
-  getDownloadedModels(): Promise<Model[]>
+  getDownloadedModels(): Promise<Model[]>;
 
   /**
    * Gets a list of configured models.
    * @returns A Promise that resolves with an array of configured models.
    */
-  getConfiguredModels(): Promise<Model[]>
+  getConfiguredModels(): Promise<Model[]>;
 }
