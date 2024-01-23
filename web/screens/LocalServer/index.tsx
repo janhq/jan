5c8c6f479517ca4681b3caf43738a337c29689df
hiro <vuonghoainam.work@gmail.com>
2024-01-23 17:55:23 +0700

    fix: Change to fixed `localhost` instead of using host variable

--- web/screens/LocalServer/index.tsx
+++ web/screens/LocalServer/index.tsx
@@ -129,7 +129,7 @@ const LocalServerScreen = () => {
             </Button>
             {serverEnabled && (
               <Button block themes="secondaryBlue" asChild>
-                <a href={`http://${host}:${port}`} target="_blank">
+                <a href={`http://localhost:${port}`} target="_blank">
                   API Reference <ExternalLinkIcon size={20} className="ml-2" />
                 </a>
               </Button>
