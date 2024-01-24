387d24748b819d62e11dd95ba869e32f92c6a4ac
nam <namnh0122@gmail.com>
2024-01-25 00:25:08 +0700

    feat(FactoryReset): add factory reset feature
    Signed-off-by: nam <namnh0122@gmail.com>

--- web/screens/Settings/Advanced/index.tsx
+++ web/screens/Settings/Advanced/index.tsx
@@ -237,6 +237,30 @@ const Advanced = () => {
           Clear
         </Button>
       </div>
+
+      {/* Factory Reset */}
+      <div className="flex w-full items-start justify-between border-b border-border py-4 first:pt-0 last:border-none">
+        <div className="w-4/5 flex-shrink-0 space-y-1.5">
+          <div className="flex gap-x-2">
+            <h6 className="text-sm font-semibold capitalize">
+              Reset to Factory Default
+            </h6>
+          </div>
+          <p className="whitespace-pre-wrap leading-relaxed">
+            Reset the application to its original state, deleting all your usage
+            data, including model customizations and conversation history. This
+            action is irreversible and recommended only if the application is in
+            a corrupted state.
+          </p>
+        </div>
+        <Button
+          size="sm"
+          themes="secondaryDanger"
+          onClick={onFactoryResetClick}
+        >
+          Reset
+        </Button>
+      </div>
     </div>
   )
 }
