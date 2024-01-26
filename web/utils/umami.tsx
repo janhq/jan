1e4aa176036ac90bc218c439d1dcee87fdecd7e7
hieu-jan <150573299+hieu-jan@users.noreply.github.com>
2024-01-26 19:44:00 +0900

    feat: integrate umami

---
+++ web/utils/umami.tsx
@@ -0,0 +1,52 @@
+import Script from 'next/script'
+import { useEffect } from 'react'
+
+declare global {
+  interface Window {
+    umami: any
+  }
+}
+
+const Umami = () => {
+  const appVersion = VERSION
+  const analyticsHost = ANALYTICS_HOST
+  const analyticsId = ANALYTICS_ID
+
+  useEffect(() => {
+    const ping = () => {
+      // Check if umami is defined before ping
+      if (typeof window.umami !== 'undefined') {
+        window.umami.track(appVersion, {
+          version: appVersion,
+        })
+      }
+    }
+
+    // Wait for umami to be defined before ping
+    if (typeof window.umami !== 'undefined') {
+      ping()
+    } else {
+      // Listen for umami script load event
+      document.addEventListener('umami:loaded', ping)
+    }
+
+    // Cleanup function to remove event listener if the component unmounts
+    return () => {
+      document.removeEventListener('umami:loaded', ping)
+    }
+  }, [appVersion])
+
+  return (
+    <>
+      <Script
+        src={analyticsHost}
+        data-website-id={analyticsId}
+        data-cache="true"
+        defer
+        onLoad={() => document.dispatchEvent(new Event('umami:loaded'))}
+      />
+    </>
+  )
+}
+
+export default Umami
