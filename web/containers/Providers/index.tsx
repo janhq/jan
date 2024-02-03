6ea7d8f6cf985b4811d7f27d3b92dad9db3da350
Louis <louis@jan.ai>
2024-02-03 16:45:34 +0700

    fix: migration loading indicator (#1913)

--- web/containers/Providers/index.tsx
+++ web/containers/Providers/index.tsx
@@ -21,6 +21,8 @@ import {
 
 import Umami from '@/utils/umami'
 
+import Loader from '../Loader'
+
 import KeyListener from './KeyListener'
 
 import { extensionManager } from '@/extension'
@@ -30,6 +32,7 @@ const Providers = (props: PropsWithChildren) => {
 
   const [setupCore, setSetupCore] = useState(false)
   const [activated, setActivated] = useState(false)
+  const [settingUp, setSettingUp] = useState(false)
 
   async function setupExtensions() {
     // Register all active extensions
@@ -37,11 +40,13 @@ const Providers = (props: PropsWithChildren) => {
 
     setTimeout(async () => {
       if (!isCoreExtensionInstalled()) {
-        setupBaseExtensions()
+        setSettingUp(true)
+        await setupBaseExtensions()
         return
       }
 
       extensionManager.load()
+      setSettingUp(false)
       setActivated(true)
     }, 500)
   }
@@ -71,6 +76,7 @@ const Providers = (props: PropsWithChildren) => {
     <JotaiWrapper>
       <ThemeWrapper>
         <Umami />
+        {settingUp && <Loader description="Preparing Update..." />}
         {setupCore && activated && (
           <KeyListener>
             <FeatureToggleWrapper>
