14b792cc89c34d07e2c2a9063548511fb40669ae
InNoobWeTrust <vu.truong.16193@gmail.com>
2024-02-06 05:25:49 +0700

    fix(web/typo): Fix a typo in setting screen

--- web/screens/Settings/CoreExtensions/index.tsx
+++ web/screens/Settings/CoreExtensions/index.tsx
@@ -100,7 +100,7 @@ const ExtensionCatalog = () => {
             </h6>
           </div>
           <p className="whitespace-pre-wrap leading-relaxed ">
-            Select a extension file to install (.tgz)
+            Select an extension file to install (.tgz)
           </p>
         </div>
         <div>
