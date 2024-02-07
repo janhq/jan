6baff5d58d6b4d79214b2326a3542f9d6301144e
vanpham <ptvan0311@gmail.com>
2024-02-07 23:12:57 +0700

    feat: video recorder on failures

--- electron/tests/pages/basePage.ts
+++ electron/tests/pages/basePage.ts
@@ -1,20 +1,24 @@
 import {
-  expect,
-  test as base,
   _electron as electron,
+  BrowserContext,
   ElectronApplication,
+  expect,
   Page,
+  test as base,
 } from '@playwright/test'
 import {
+  ElectronAppInfo,
   findLatestBuild,
   parseElectronApp,
   stubDialog,
 } from 'electron-playwright-helpers'
-
-export const TIMEOUT: number = parseInt(process.env.TEST_TIMEOUT || '300000')
+import * as fs from 'fs'
+import { Constants } from '../config/constants'
 
 export let electronApp: ElectronApplication
 export let page: Page
+export let appInfo: ElectronAppInfo
+export const TIMEOUT = parseInt(process.env.TEST_TIMEOUT || Constants.TIMEOUT)
 
 export async function setupElectron() {
   process.env.CI = 'e2e'
@@ -23,20 +27,19 @@ export async function setupElectron() {
   expect(latestBuild).toBeTruthy()
 
   // parse the packaged Electron app and find paths and other info
-  const appInfo = parseElectronApp(latestBuild)
+  appInfo = parseElectronApp(latestBuild)
   expect(appInfo).toBeTruthy()
 
   electronApp = await electron.launch({
     args: [appInfo.main], // main file from package.json
     executablePath: appInfo.executable, // path to the Electron executable
+    recordVideo: { dir: Constants.VIDEO_DIR }, // Specify the directory for video recordings
   })
   await stubDialog(electronApp, 'showMessageBox', { response: 1 })
 
   page = await electronApp.firstWindow({
     timeout: TIMEOUT,
   })
-  // Return appInfo for future use
-  return appInfo
 }
 
 export async function teardownElectron() {
@@ -44,9 +47,43 @@ export async function teardownElectron() {
   await electronApp.close()
 }
 
-export const test = base.extend<{
-  attachScreenshotsToReport: void
-}>({
+/**
+ * this fixture is needed to record and attach videos / screenshot on failed tests when
+ * tests are run in serial mode (i.e. browser is not closed between tests)
+ */
+export const test = base.extend<
+  {
+    attachVideoPage: Page
+    attachScreenshotsToReport: void
+  },
+  { createVideoContext: BrowserContext }
+>({
+  createVideoContext: [
+    async ({ browser }, use) => {
+      // const context = browser.newContext()
+      const context = electronApp.context()
+      await use(context)
+
+      fs.rmSync(Constants.VIDEO_DIR, { recursive: true })
+    },
+    { scope: 'worker' },
+  ],
+
+  attachVideoPage: [
+    async ({ createVideoContext }, use, testInfo) => {
+      await use(page)
+
+      if (testInfo.status !== testInfo.expectedStatus) {
+        const path = await createVideoContext.pages()[0].video()?.path()
+        await createVideoContext.close()
+        await testInfo.attach('video', {
+          path: path,
+        })
+      }
+    },
+    { scope: 'test', auto: true },
+  ],
+
   attachScreenshotsToReport: [
     async ({ request }, use, testInfo) => {
       await use()
@@ -65,3 +102,13 @@ export const test = base.extend<{
 })
 
 test.setTimeout(TIMEOUT)
+
+test.beforeAll(async () => {
+  console.log('before tests')
+  await setupElectron()
+})
+
+test.afterAll(async () => {
+  console.log('after tests')
+  await teardownElectron()
+})
