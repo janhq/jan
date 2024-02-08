727a35095c0ce50371efa68b7c4faf410deb728f
Louis <louis@jan.ai>
2024-02-08 17:38:23 +0700

    chore: trimming langchainjs

--- extensions/assistant-extension/rollup.config.ts
+++ extensions/assistant-extension/rollup.config.ts
@@ -48,9 +48,7 @@ export default [
     // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
     external: [
       "@janhq/core/node",
-      "@langchain/community",
-      "langchain",
-      "langsmith",
+      "pdf-parse",
       "path",
       "hnswlib-node",
     ],
