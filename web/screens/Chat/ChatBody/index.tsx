48b20954ab2f2599d4e765a29e3bd7c8659630c7
Faisal Amir <urmauur@gmail.com>
2024-02-09 13:56:55 +0700

    resolve inconsistent message hidden

--- web/screens/Chat/ChatBody/index.tsx
+++ web/screens/Chat/ChatBody/index.tsx
@@ -81,7 +81,8 @@ const ChatBody: React.FC = () => {
         <ScrollToBottom className="flex h-full w-full flex-col">
           {messages.map((message, index) => (
             <div key={message.id}>
-              {(message.status !== MessageStatus.Pending ||
+              {((message.status !== MessageStatus.Error &&
+                message.status !== MessageStatus.Pending) ||
                 message.content.length > 0) && (
                 <ChatItem {...message} key={message.id} />
               )}
