c9f2feefdfc18bbf3bb8bf56f5163fae7434f42a
vanpham <ptvan0311@gmail.com>
2024-02-13 19:41:21 +0700

    feat: fixture for sample page class

--- electron/tests/e2e/settings.e2e.spec.ts
+++ electron/tests/e2e/settings.e2e.spec.ts
@@ -1,18 +1,9 @@
 import { expect } from '@playwright/test'
 
-import { test, page, TIMEOUT } from '../pages/basePage'
-
-test.beforeAll(async () => {
-  console.log('before settings')
-})
-
-test.afterAll(async () => {
-  console.log('after settings')
-})
+import { test, page, TIMEOUT } from '../config/fixtures'
 
 test('shows settings', async () => {
-  await page.getByTestId('Settings123').first().click()
-  expect(true).toEqual(false)
+  await page.getByTestId('Settings').first().click()
   const settingDescription = page.getByTestId('testid-setting-description')
   await expect(settingDescription).toBeVisible({ timeout: TIMEOUT })
 })
