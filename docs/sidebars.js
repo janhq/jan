7ce0a13e9007c2da679e36a3bb06e3be9d8717be
hieu-jan <150573299+hieu-jan@users.noreply.github.com>
2024-02-22 22:17:14 +0900

    revert content & remove sidebar only

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -19,6 +19,7 @@ const sidebars = {
       label: "What is Jan?",
       link: { type: "doc", id: "about/about" },
       items: [
+        //"about/roadmap",
         "community/community",
       ],
     },
@@ -59,7 +60,7 @@ const sidebars = {
       items: [
         "platforms/desktop",
         "server-suite/home-server",
-        //"server-suite/enterprise",
+        // "server-suite/enterprise",
         // "platforms/mobile",
         // "platforms/hub",
       ],
