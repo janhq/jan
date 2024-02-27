d7070d8c4a8e4088ee49a94dbb36e0b05b361ed7
NamH <NamNh0122@gmail.com>
2024-02-27 23:59:37 +0700

    fix: some bugs for import model (#2181)
    * fix: some bugs for import model
    
    Signed-off-by: James <james@jan.ai>
    
    Signed-off-by: James <james@jan.ai>
    Co-authored-by: James <james@jan.ai>

--- web/screens/Settings/ImportingModelModal/ImportingModelItem.tsx
+++ web/screens/Settings/ImportingModelModal/ImportingModelItem.tsx
@@ -1,5 +1,9 @@
+import { useCallback, useMemo } from 'react'
+
 import { ImportingModel } from '@janhq/core/.'
 import { useSetAtom } from 'jotai'
+
+import { AlertCircle } from 'lucide-react'
 
 import { setImportModelStageAtom } from '@/hooks/useImportModel'
 
@@ -16,28 +20,39 @@ type Props = {
 const ImportingModelItem: React.FC<Props> = ({ model }) => {
   const setImportModelStage = useSetAtom(setImportModelStageAtom)
   const setEditingModelId = useSetAtom(editingModelIdAtom)
-  const sizeInGb = toGibibytes(model.size)
 
-  const onEditModelInfoClick = () => {
+  const onEditModelInfoClick = useCallback(() => {
     setEditingModelId(model.importId)
     setImportModelStage('EDIT_MODEL_INFO')
-  }
+  }, [setImportModelStage, setEditingModelId, model.importId])
 
-  const onDeleteModelClick = () => {}
+  const onDeleteModelClick = useCallback(() => {}, [])
+
+  const displayStatus = useMemo(() => {
+    if (model.status === 'FAILED') {
+      return 'Failed'
+    } else {
+      return toGibibytes(model.size)
+    }
+  }, [model.status, model.size])
 
   return (
     <div className="flex w-full flex-row items-center space-x-3 rounded-lg border px-4 py-3">
-      <p className="line-clamp-1 flex-1">{model.name}</p>
-      <p>{sizeInGb}</p>
+      <p className="line-clamp-1 flex-1 font-semibold text-[#09090B]">
+        {model.name}
+      </p>
+      <p className="text-[#71717A]">{displayStatus}</p>
 
-      {model.status === 'IMPORTED' || model.status === 'FAILED' ? (
+      {model.status === 'IMPORTED' && (
         <ImportSuccessIcon onEditModelClick={onEditModelInfoClick} />
-      ) : (
+      )}
+      {(model.status === 'IMPORTING' || model.status === 'PREPARING') && (
         <ImportInProgressIcon
           percentage={model.percentage ?? 0}
           onDeleteModelClick={onDeleteModelClick}
         />
       )}
+      {model.status === 'FAILED' && <AlertCircle size={24} color="#F00" />}
     </div>
   )
 }
