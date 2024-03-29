bd93c4233d03230e34a42bcd91ad4d0818aa083d
James <james@jan.ai>
2024-03-30 01:03:27 +0700

    update
    Signed-off-by: James <james@jan.ai>

--- web/app/search/layout.tsx
+++ web/app/search/layout.tsx
@@ -1,13 +1,21 @@
 'use client'
 
+import { useEffect } from 'react'
+
 import ClipboardListener from '@/containers/Providers/ClipboardListener'
 
 import JotaiWrapper from '@/containers/Providers/Jotai'
 import ThemeWrapper from '@/containers/Providers/Theme'
 
+import { setupCoreServices } from '@/services/coreService'
+
 import Search from './page'
 
 export default function RootLayout() {
+  useEffect(() => {
+    setupCoreServices()
+  }, [])
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="bg-white font-sans text-sm antialiased dark:bg-background">
