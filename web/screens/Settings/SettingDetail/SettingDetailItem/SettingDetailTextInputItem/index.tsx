b609d2ac4d696b0215d624f45dd05bcdc1313f26
James <james@jan.ai>
2024-03-29 12:25:39 +0700

    support markdown for extension description
    Signed-off-by: James <james@jan.ai>

--- web/screens/Settings/SettingDetail/SettingDetailItem/SettingDetailTextInputItem/index.tsx
+++ web/screens/Settings/SettingDetail/SettingDetailItem/SettingDetailTextInputItem/index.tsx
@@ -1,10 +1,21 @@
 import { InputComponentProps, SettingComponentProps } from '@janhq/core'
 import { Input } from '@janhq/uikit'
+import { Marked, Renderer } from 'marked'
 
 type Props = {
   settingProps: SettingComponentProps
   onValueChanged?: (e: string) => void
 }
+
+const marked: Marked = new Marked({
+  renderer: {
+    link: (href, title, text) => {
+      return Renderer.prototype.link
+        ?.apply(this, [href, title, text])
+        .replace('<a', "<a class='text-blue-500' target='_blank'")
+    },
+  },
+})
 
 const SettingDetailTextInputItem: React.FC<Props> = ({
   settingProps,
@@ -13,13 +24,21 @@ const SettingDetailTextInputItem: React.FC<Props> = ({
   const { value, type, placeholder } =
     settingProps.controllerProps as InputComponentProps
 
+  const description = marked.parse(settingProps.description ?? '', {
+    async: false,
+  })
+
   return (
     <div className="flex w-full justify-between py-6">
       <div className="flex flex-1 flex-col space-y-1">
         <h1 className="text-base font-bold">{settingProps.title}</h1>
-        <span className="text-sm font-normal text-muted-foreground">
-          {settingProps.description}
-        </span>
+        {
+          <div
+            // eslint-disable-next-line @typescript-eslint/naming-convention
+            dangerouslySetInnerHTML={{ __html: description }}
+            className="text-sm font-normal text-muted-foreground"
+          />
+        }
       </div>
       <Input
         placeholder={placeholder}
