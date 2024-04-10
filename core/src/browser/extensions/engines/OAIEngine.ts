8917be5ef308393078beddc5a44df6f46634d434
NamH <NamNh0122@gmail.com>
2024-04-10 18:15:20 +0700

    fix: add fallback as default endpoint for inference engine (#2669)
    Co-authored-by: James <james@jan.ai>

--- core/src/browser/extensions/engines/OAIEngine.ts
+++ core/src/browser/extensions/engines/OAIEngine.ts
@@ -102,7 +102,7 @@ export abstract class OAIEngine extends AIEngine {
         events.emit(MessageEvent.OnMessageUpdate, message)
       },
       error: async (err: any) => {
-        console.error(`Inference error:`, err)
+        console.error(`Inference error:`, JSON.stringify(err))
         if (this.isCancelled || message.content.length) {
           message.status = MessageStatus.Stopped
           events.emit(MessageEvent.OnMessageUpdate, message)
