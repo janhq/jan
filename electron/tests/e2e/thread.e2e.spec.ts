cdab762a6acd81230803030e1d6b706eeaa2c3b6
Van-QA <van@jan.ai>
2024-04-10 16:27:22 +0700

    feat: add new test for the Use model button from Hub

--- electron/tests/e2e/thread.e2e.spec.ts
+++ electron/tests/e2e/thread.e2e.spec.ts
@@ -1,10 +1,20 @@
 import { expect } from '@playwright/test'
 import { page, test, TIMEOUT } from '../config/fixtures'
 
-test('create thread button', async () => {
-  const settingsBtn = await page
+test('Select GPT model for chat via Use model button from Jan Hub', async ({ hubPage }) => {
+  await hubPage.navigateByMenu()
+  await hubPage.verifyContainerVisible()
+
+  // Select the first GPT model
+  await page
+    .locator('[data-testid^="use-model-btn"][data-testid*="gpt"]')
+    .first().click()
+
+  // Attempt to create thread and chat in Thread page
+  await page
     .getByTestId('btn-create-thread')
     .click()
+
   await page
     .getByTestId('txt-input-chat')
     .fill('dummy value')
@@ -13,13 +23,8 @@ test('create thread button', async () => {
     .getByTestId('btn-send-chat')
     .click()
 
-  expect([settingsBtn].filter((e) => !e).length).toBe(0)
-  // Chat section should be there
-  await page.getByTestId('Local API Server').first().click({
-    timeout: TIMEOUT,
-  })
-  const localServer = page.getByTestId('local-server-testid').first()
-  await expect(localServer).toBeVisible({
+  const APIKeyError = page.getByTestId('invalid-API-key-error')
+  await expect(APIKeyError).toBeVisible({
     timeout: TIMEOUT,
   })
 })
