ddb73d8131befcde98a43ed0d451369d741e6e6e
NamH <NamNh0122@gmail.com>
2024-04-11 10:56:47 +0700

    fix: can't read the setting at first time (#2677)
    Signed-off-by: James <james@jan.ai>
    Co-authored-by: James <james@jan.ai>

--- extensions/inference-openai-extension/src/index.ts
+++ extensions/inference-openai-extension/src/index.ts
@@ -37,6 +37,13 @@ export default class JanInferenceOpenAIExtension extends RemoteOAIEngine {
       Settings.chatCompletionsEndPoint,
       ''
     )
+    if (this.inferenceUrl.length === 0) {
+      SETTINGS.forEach((setting) => {
+        if (setting.key === Settings.chatCompletionsEndPoint) {
+          this.inferenceUrl = setting.controllerProps.value as string
+        }
+      })
+    }
   }
 
   onSettingUpdate<T>(key: string, value: T): void {
