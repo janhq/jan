29ddfc3cf2e29a3c2c0ff84dea0f919278633a4d
Van-QA <van@jan.ai>
2024-05-06 14:54:11 +0700

    feat: test mapper

--- docs/tests/conftest.py
+++ docs/tests/conftest.py
@@ -1,6 +1,20 @@
+import json
+
+
 def pytest_collection_modifyitems(items):
+    # load the JSON file
+    with open("endpoints_mapping.json", "r") as json_file:
+        endpoints_file_mapping = json.load(json_file)
+
+    # create a dictionary to map filenames to endpoints
+    filename_to_endpoint = {}
+    for endpoint, files in endpoints_file_mapping.items():
+        for filename in files:
+            filename_to_endpoint[filename] = endpoint
+
+    # add the markers based on the JSON file
     for item in items:
-        # add the name of the file (without extension) as a marker
-        filename = item.nodeid.split("::")[0].split("/")[-1].replace(".py", "")
-        marker = pytest.mark.file(filename)
-        item.add_marker(marker)
+        # map the name of the file to endpoint, else use default value
+        filename = item.fspath.basename
+        marker = filename_to_endpoint.get(filename, filename)
+        item.add_marker(pytest.mark.endpoint(marker, filename=filename))
