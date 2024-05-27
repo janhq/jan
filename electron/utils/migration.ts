e552c1f80fa7e84abf3f1c0422a8d6089b0f8550
Louis <louis@jan.ai>
2024-05-27 14:50:50 +0700

    chore: migrate themes

--- electron/utils/migration.ts
+++ electron/utils/migration.ts
@@ -1,29 +1,33 @@
 import { app } from 'electron'
 
-import { rmdir } from 'fs'
+import { join } from 'path'
+import { rmdirSync, cpSync } from 'fs'
 import Store from 'electron-store'
-import { getJanExtensionsPath } from '@janhq/core/node'
+import {
+  getJanExtensionsPath,
+  getJanDataFolderPath,
+  appResourcePath,
+} from '@janhq/core/node'
 
 /**
- * Migrates the extensions by deleting the `extensions` directory in the user data path.
+ * Migrates the extensions & themes.
  * If the `migrated_version` key in the `Store` object does not match the current app version,
  * the function deletes the `extensions` directory and sets the `migrated_version` key to the current app version.
  * @returns A Promise that resolves when the migration is complete.
  */
-export function migrateExtensions() {
-  return new Promise((resolve) => {
-    const store = new Store()
-    if (store.get('migrated_version') !== app.getVersion()) {
-      console.debug('start migration:', store.get('migrated_version'))
+export async function migrate() {
+  const store = new Store()
+  if (store.get('migrated_version') !== app.getVersion()) {
+    console.debug('start migration:', store.get('migrated_version'))
 
-      rmdir(getJanExtensionsPath(), { recursive: true }, function (err) {
-        if (err) console.error(err)
-        store.set('migrated_version', app.getVersion())
-        console.debug('migrate extensions done')
-        resolve(undefined)
-      })
-    } else {
-      resolve(undefined)
-    }
-  })
+    rmdirSync(getJanExtensionsPath(), { recursive: true })
+    rmdirSync(join(getJanDataFolderPath()), { recursive: true })
+    cpSync(
+      join(await appResourcePath(), 'themes'),
+      join(getJanDataFolderPath(), 'themes')
+    )
+
+    store.set('migrated_version', app.getVersion())
+    console.debug('migrate extensions done')
+  }
 }
