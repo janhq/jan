e1fab6e580663e19bfa221682b85785fbdf1aa4a
Faisal Amir <urmauur@gmail.com>
2024-07-11 15:15:44 +0700

    chore: integrate script migration

--- web/containers/Providers/ModalMigrations.tsx
+++ web/containers/Providers/ModalMigrations.tsx
@@ -1,51 +1,125 @@
-import React, { Fragment, useCallback } from 'react'
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { Button, Checkbox, Modal, Badge } from '@janhq/joi'
+import React, { Fragment, useCallback, useEffect } from 'react'
 
+import { Button, Modal, Badge } from '@janhq/joi'
+
+import { useAtom, useAtomValue } from 'jotai'
 import { AlertTriangleIcon } from 'lucide-react'
+
+import { twMerge } from 'tailwind-merge'
 
 import useMigratingData from '@/hooks/useMigratingData'
 
+import {
+  didShowMigrationWarningAtom,
+  modelsMigrationSuccessAtom,
+  threadsMessagesMigrationSuccessAtom,
+  skipMigrationAtom,
+} from '@/helpers/atoms/AppConfig.atom'
+
+const Loader = () => {
+  return (
+    <div role="status">
+      <svg
+        aria-hidden="true"
+        className="inline h-4 w-4 animate-spin fill-[hsla(var(--primary-bg))] text-[hsla(var(--text-quaternary))]"
+        viewBox="0 0 100 101"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+          fill="currentColor"
+        />
+        <path
+          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+          fill="currentFill"
+        />
+      </svg>
+      <span className="sr-only">Loading...</span>
+    </div>
+  )
+}
+
 const ModalMigrations = ({ children }: React.PropsWithChildren) => {
-  const [open, setOpen] = React.useState(false)
+  const [didShowMigrationWarning, setDidShowMigrationWarning] = useAtom(
+    didShowMigrationWarningAtom
+  )
+  const [skipMigration, setSkipMigration] = useAtom(skipMigrationAtom)
+  const [modelsMigrationSuccess, setModelsMigrationSuccess] = useAtom(
+    modelsMigrationSuccessAtom
+  )
+  const [threadsMessagesMigrationSuccess, setThreadsMessagesMigrationSuccess] =
+    useAtom(threadsMessagesMigrationSuccessAtom)
+
   const [step, setStep] = React.useState(1)
+  const [loaderThreads, setLoaderThreads] = React.useState(false)
+  const [loaderModels, setLoaderModels] = React.useState(false)
+  const [threadsAndMessages, setThreadsAndMessages] = React.useState({})
 
   const getStepTitle = () => {
     switch (step) {
       case 1:
         return 'Important Update: Data Migration Needed'
 
-      case 3:
-        return 'Migration Completed'
-
       default:
-        return 'Migration In Progressed'
+        return loaderThreads || loaderModels
+          ? 'Migration In Progressed'
+          : 'Migration Completed'
     }
   }
 
-  const handleStartMigration = () => {
+  const handleStartMigration = async () => {
     setStep(2)
+    await handleStartMigrationModels()
+    await handleStartMigrationThreads()
   }
 
-  const { getJanThreadsAndMessages } = useMigratingData()
+  const handleStartMigrationThreads = async () => {
+    setLoaderThreads(true)
+    await migrateThreadsAndMessages()
+    setTimeout(() => {
+      setLoaderThreads(false)
+    }, 1200)
+  }
 
-  const test = async () => await getJanThreadsAndMessages()
+  const handleStartMigrationModels = async () => {
+    setLoaderModels(true)
+    await migrateModels()
+    setTimeout(() => {
+      setLoaderModels(false)
+    }, 1200)
+  }
 
-  // console.log(test())
+  const { getJanThreadsAndMessages, migrateModels, migrateThreadsAndMessages } =
+    useMigratingData()
 
-  const tos = useCallback(async (): Promise<{
-    messages: any[]
-    threads: any[]
-  }> => {
-    return window?.electronAPI?.getAllMessagesAndThreads()
-  }, [])
+  const getMigrationNotif = useCallback(async () => {
+    try {
+      const result = await getJanThreadsAndMessages()
+      setThreadsAndMessages(result)
+    } catch (error) {
+      setThreadsAndMessages({ threads: [], messages: [] })
+      setDidShowMigrationWarning(false)
+      console.error(error)
+    }
+    return threadsAndMessages
+  }, [getJanThreadsAndMessages, setDidShowMigrationWarning, threadsAndMessages])
 
-  console.log(tos())
+  useEffect(() => {
+    getMigrationNotif()
+
+    if (skipMigration) {
+      setDidShowMigrationWarning(false)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [skipMigration, setSkipMigration, setDidShowMigrationWarning])
 
   return (
     <>
       <Modal
-        open={open}
+        open={didShowMigrationWarning}
         hideClose
         title={getStepTitle()}
         content={
@@ -86,6 +160,13 @@ const ModalMigrations = ({ children }: React.PropsWithChildren) => {
                 <div className="flex justify-end">
                   <Button
                     className="mt-4"
+                    theme="ghost"
+                    onClick={() => setSkipMigration(true)}
+                  >
+                    Skip
+                  </Button>
+                  <Button
+                    className="ml-2 mt-4"
                     onClick={() => handleStartMigration()}
                   >
                     Migrate Now
@@ -95,70 +176,75 @@ const ModalMigrations = ({ children }: React.PropsWithChildren) => {
             )}
             {step === 2 && (
               <Fragment>
-                <div className="mb-2 mt-4 flex justify-between rounded-lg border border-[hsla(var(--app-border))] p-3">
-                  <p className="font-bold">Threads</p>
-                  <div role="status">
-                    <svg
-                      aria-hidden="true"
-                      className="inline h-4 w-4 animate-spin fill-[hsla(var(--primary-bg))] text-[hsla(var(--text-quaternary))]"
-                      viewBox="0 0 100 101"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                        fill="currentColor"
-                      />
-                      <path
-                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                        fill="currentFill"
-                      />
-                    </svg>
-                    <span className="sr-only">Loading...</span>
+                <div
+                  className={twMerge(
+                    'mb-2 mt-4 flex justify-between rounded-lg border border-[hsla(var(--app-border))] p-3',
+                    threadsMessagesMigrationSuccess
+                      ? 'bg-[hsla(var(--success-bg-soft))]'
+                      : 'bg-[hsla(var(--destructive-bg-soft))]',
+                    loaderThreads && 'bg-trasparent'
+                  )}
+                >
+                  <div className="flex items-center gap-x-1.5">
+                    {!loaderThreads && (
+                      <>
+                        {threadsMessagesMigrationSuccess ? (
+                          <Badge theme="success">Success</Badge>
+                        ) : (
+                          <Badge theme="destructive">Failed</Badge>
+                        )}
+                      </>
+                    )}
+                    <p className="font-bold">Threads</p>
                   </div>
+                  {loaderThreads ? (
+                    <Loader />
+                  ) : (
+                    !threadsMessagesMigrationSuccess && (
+                      <Button size="small" theme="ghost">
+                        Retry
+                      </Button>
+                    )
+                  )}
                 </div>
-                <div className="my-2 flex justify-between rounded-lg border border-[hsla(var(--app-border))] p-3">
-                  <p className="font-bold">Models</p>
-                  <div role="status">
-                    <svg
-                      aria-hidden="true"
-                      className="inline h-4 w-4 animate-spin fill-[hsla(var(--primary-bg))] text-[hsla(var(--text-quaternary))]"
-                      viewBox="0 0 100 101"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                        fill="currentColor"
-                      />
-                      <path
-                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                        fill="currentFill"
-                      />
-                    </svg>
-                    <span className="sr-only">Loading...</span>
-                  </div>
-                </div>
-              </Fragment>
-            )}
-            {step === 3 && (
-              <Fragment>
-                <div className="mb-2 mt-4 flex justify-between rounded-lg border border-[hsla(var(--app-border))] p-3">
-                  <p className="font-bold">Threads</p>
-                  <Checkbox checked />
-                </div>
-                <div className="my-2 flex justify-between rounded-lg border border-[hsla(var(--app-border))] bg-[hsla(var(--destructive-bg-soft))] p-3">
-                  <div className="flex items-center gap-x-1">
-                    <Badge theme="destructive">Failed</Badge>
+                <div
+                  className={twMerge(
+                    'my-2 flex justify-between rounded-lg border border-[hsla(var(--app-border))] p-3',
+                    modelsMigrationSuccess
+                      ? 'bg-[hsla(var(--success-bg-soft))]'
+                      : 'bg-[hsla(var(--destructive-bg-soft))]',
+                    loaderModels && 'bg-trasparent'
+                  )}
+                >
+                  <div className="flex items-center gap-x-1.5">
+                    {!loaderModels && (
+                      <>
+                        {modelsMigrationSuccess ? (
+                          <Badge theme="success">Success</Badge>
+                        ) : (
+                          <Badge theme="destructive">Failed</Badge>
+                        )}
+                      </>
+                    )}
                     <p className="font-bold">Models</p>
                   </div>
-                  <Button size="small" theme="ghost">
-                    Retry
-                  </Button>
+                  {loaderModels ? (
+                    <Loader />
+                  ) : (
+                    !modelsMigrationSuccess && (
+                      <Button size="small" theme="ghost">
+                        Retry
+                      </Button>
+                    )
+                  )}
                 </div>
-
                 <div className="flex justify-end">
-                  <Button className="mt-2" onClick={() => setOpen(false)}>
+                  <Button
+                    className="mt-2"
+                    onClick={() => {
+                      setDidShowMigrationWarning(false)
+                    }}
+                  >
                     Done
                   </Button>
                 </div>
--- web/hooks/useMigratingData.ts
+++ web/hooks/useMigratingData.ts
@@ -2,14 +2,23 @@
 import { useCallback } from 'react'
 
 import { MessageCreateParams } from '@janhq/core'
-import { useAtomValue } from 'jotai'
+import { useAtom, useAtomValue } from 'jotai'
 
 import useCortex from './useCortex'
 import useThreads from './useThreads'
 
+import {
+  threadsMessagesMigrationSuccessAtom,
+  modelsMigrationSuccessAtom,
+} from '@/helpers/atoms/AppConfig.atom'
 import { assistantsAtom } from '@/helpers/atoms/Assistant.atom'
 
 const useMigratingData = () => {
+  const [threadsMessagesMigrationSuccess, setThreadsMessagesMigrationSuccess] =
+    useAtom(threadsMessagesMigrationSuccessAtom)
+  const [modelsMigrationSuccess, setModelsMigrationSuccess] = useAtom(
+    modelsMigrationSuccessAtom
+  )
   const { createThread } = useThreads()
   const { updateThread, createMessage } = useCortex()
   const assistants = useAtomValue(assistantsAtom)
@@ -22,8 +31,16 @@ const useMigratingData = () => {
   }, [])
 
   const migrateModels = useCallback(async () => {
-    await window?.electronAPI?.syncModelFileToCortex()
-  }, [])
+    try {
+      if (!modelsMigrationSuccess) {
+        await window?.electronAPI?.syncModelFileToCortex()
+        setModelsMigrationSuccess(true)
+      }
+    } catch (err) {
+      console.log(err)
+      setModelsMigrationSuccess(false)
+    }
+  }, [modelsMigrationSuccess, setModelsMigrationSuccess])
 
   const migrateThreadsAndMessages = useCallback(async () => {
     if (!assistants || assistants.length === 0) {
@@ -33,60 +50,66 @@ const useMigratingData = () => {
     try {
       const threadsAndMessages = await getJanThreadsAndMessages()
       const janThreads = threadsAndMessages.threads
-      for (const thread of janThreads) {
-        const modelId: string | undefined = thread.assistants[0]?.model?.id
-        if (!modelId || modelId.trim().length === 0 || modelId === '*') {
-          console.error(
-            `Ignore thread ${thread.id} because modelId is not found`
-          )
-          continue
-        }
-        const threadTitle: string = thread.title ?? 'New Thread'
-        const instruction: string = thread.assistants[0]?.instruction ?? ''
-
-        // currently, we don't have api support for creating thread with messages
-        const cortexThread = await createThread(modelId, assistants[0])
-        console.log('createThread', cortexThread)
-        // update instruction
-        cortexThread.assistants[0].instructions = instruction
-        cortexThread.title = threadTitle
-
-        // update thread name
-        await updateThread(cortexThread)
-        console.log('updateThread', cortexThread)
-
-        // we finished with thread, now continue with messages
-        const janMessages = threadsAndMessages.messages.filter(
-          (m) => m.thread_id === thread.id
-        )
-        console.log(janMessages)
-        for (let j = 0; j < janMessages.length; ++j) {
-          const janMessage = janMessages[j]
-          // filter out the system message if any
-          if (janMessage.role === 'system') continue
-          try {
-            const messageContent: string =
-              janMessage.content[0]?.text.value ?? ''
-            const createMessageParam: MessageCreateParams = {
-              content: messageContent,
-              role: janMessage.role,
-            }
-            // can speed up here with Promise.allSettled
-            await createMessage(cortexThread.id, createMessageParam)
-            console.log('createMesage', cortexThread.id, createMessageParam)
-          } catch (err) {
-            console.error(err)
+      if (!threadsMessagesMigrationSuccess) {
+        for (const thread of janThreads) {
+          const modelId: string | undefined = thread.assistants[0]?.model?.id
+          if (!modelId || modelId.trim().length === 0 || modelId === '*') {
+            console.error(
+              `Ignore thread ${thread.id} because modelId is not found`
+            )
+            continue
           }
+          const threadTitle: string = thread.title ?? 'New Thread'
+          const instruction: string = thread.assistants[0]?.instruction ?? ''
+
+          // currently, we don't have api support for creating thread with messages
+          const cortexThread = await createThread(modelId, assistants[0])
+          console.log('createThread', cortexThread)
+          // update instruction
+          cortexThread.assistants[0].instructions = instruction
+          cortexThread.title = threadTitle
+
+          // update thread name
+          await updateThread(cortexThread)
+          console.log('updateThread', cortexThread)
+
+          // we finished with thread, now continue with messages
+          const janMessages = threadsAndMessages.messages.filter(
+            (m) => m.thread_id === thread.id
+          )
+          console.log(janMessages)
+          for (let j = 0; j < janMessages.length; ++j) {
+            const janMessage = janMessages[j]
+            // filter out the system message if any
+            if (janMessage.role === 'system') continue
+            try {
+              const messageContent: string =
+                janMessage.content[0]?.text.value ?? ''
+              const createMessageParam: MessageCreateParams = {
+                content: messageContent,
+                role: janMessage.role,
+              }
+              // can speed up here with Promise.allSettled
+              await createMessage(cortexThread.id, createMessageParam)
+              console.log('createMesage', cortexThread.id, createMessageParam)
+            } catch (err) {
+              console.error(err)
+            }
+          }
+          setThreadsMessagesMigrationSuccess(true)
         }
       }
     } catch (err) {
       console.log(err)
+      setThreadsMessagesMigrationSuccess(false)
     }
   }, [
     assistants,
     getJanThreadsAndMessages,
-    updateThread,
+    threadsMessagesMigrationSuccess,
     createThread,
+    updateThread,
+    setThreadsMessagesMigrationSuccess,
     createMessage,
   ])
 
