d78626108730338d403dffe217b9b79bcc9d898d
marknguyen1302 <nguyenvu1302.work@gmail.com>
2024-07-11 15:06:27 +0700

    fix test

--- core/tests/node/path.test.ts
+++ core/tests/node/path.test.ts
@@ -1,5 +1,9 @@
 import { normalizeFilePath } from "../../src/node/helper/path";
 
+jest.mock('os', () => ({
+  homedir: jest.fn().mockReturnValue('/mock/home/dir')
+}));
+
 describe("Test file normalize", () => {
   test("returns no file protocol prefix on Unix", async () => {
     expect(normalizeFilePath("file://test.txt")).toBe("test.txt");
