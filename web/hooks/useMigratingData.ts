0c36e6ef7fb9cb632227ce3fc617cc33447b0fc3
Faisal Amir <urmauur@gmail.com>
2024-07-11 15:24:15 +0700

    chore: update check thread message success migrate

--- web/hooks/useMigratingData.ts
+++ web/hooks/useMigratingData.ts
@@ -48,56 +48,55 @@ const useMigratingData = () => {
       return
     }
     try {
+      if (threadsMessagesMigrationSuccess) return
       const threadsAndMessages = await getJanThreadsAndMessages()
       const janThreads = threadsAndMessages.threads
-      if (!threadsMessagesMigrationSuccess) {
-        for (const thread of janThreads) {
-          const modelId: string | undefined = thread.assistants[0]?.model?.id
-          if (!modelId || modelId.trim().length === 0 || modelId === '*') {
-            console.error(
-              `Ignore thread ${thread.id} because modelId is not found`
-            )
-            continue
-          }
-          const threadTitle: string = thread.title ?? 'New Thread'
-          const instruction: string = thread.assistants[0]?.instruction ?? ''
-
-          // currently, we don't have api support for creating thread with messages
-          const cortexThread = await createThread(modelId, assistants[0])
-          console.log('createThread', cortexThread)
-          // update instruction
-          cortexThread.assistants[0].instructions = instruction
-          cortexThread.title = threadTitle
-
-          // update thread name
-          await updateThread(cortexThread)
-          console.log('updateThread', cortexThread)
-
-          // we finished with thread, now continue with messages
-          const janMessages = threadsAndMessages.messages.filter(
-            (m) => m.thread_id === thread.id
+      for (const thread of janThreads) {
+        const modelId: string | undefined = thread.assistants[0]?.model?.id
+        if (!modelId || modelId.trim().length === 0 || modelId === '*') {
+          console.error(
+            `Ignore thread ${thread.id} because modelId is not found`
           )
-          console.log(janMessages)
-          for (let j = 0; j < janMessages.length; ++j) {
-            const janMessage = janMessages[j]
-            // filter out the system message if any
-            if (janMessage.role === 'system') continue
-            try {
-              const messageContent: string =
-                janMessage.content[0]?.text.value ?? ''
-              const createMessageParam: MessageCreateParams = {
-                content: messageContent,
-                role: janMessage.role,
-              }
-              // can speed up here with Promise.allSettled
-              await createMessage(cortexThread.id, createMessageParam)
-              console.log('createMesage', cortexThread.id, createMessageParam)
-            } catch (err) {
-              console.error(err)
-            }
-          }
-          setThreadsMessagesMigrationSuccess(true)
+          continue
         }
+        const threadTitle: string = thread.title ?? 'New Thread'
+        const instruction: string = thread.assistants[0]?.instruction ?? ''
+
+        // currently, we don't have api support for creating thread with messages
+        const cortexThread = await createThread(modelId, assistants[0])
+        console.log('createThread', cortexThread)
+        // update instruction
+        cortexThread.assistants[0].instructions = instruction
+        cortexThread.title = threadTitle
+
+        // update thread name
+        await updateThread(cortexThread)
+        console.log('updateThread', cortexThread)
+
+        // we finished with thread, now continue with messages
+        const janMessages = threadsAndMessages.messages.filter(
+          (m) => m.thread_id === thread.id
+        )
+        console.log(janMessages)
+        for (let j = 0; j < janMessages.length; ++j) {
+          const janMessage = janMessages[j]
+          // filter out the system message if any
+          if (janMessage.role === 'system') continue
+          try {
+            const messageContent: string =
+              janMessage.content[0]?.text.value ?? ''
+            const createMessageParam: MessageCreateParams = {
+              content: messageContent,
+              role: janMessage.role,
+            }
+            // can speed up here with Promise.allSettled
+            await createMessage(cortexThread.id, createMessageParam)
+            console.log('createMesage', cortexThread.id, createMessageParam)
+          } catch (err) {
+            console.error(err)
+          }
+        }
+        setThreadsMessagesMigrationSuccess(true)
       }
     } catch (err) {
       console.log(err)
