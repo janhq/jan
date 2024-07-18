d5b89d9675cefc8cdae0d6ee9d2515494376f261
James <namnh0122@gmail.com>
2024-07-18 15:36:21 +0700

    fix system monitor rounded only on the left
    Signed-off-by: James <namnh0122@gmail.com>

--- web/containers/Layout/BottomPanel/index.tsx
+++ web/containers/Layout/BottomPanel/index.tsx
@@ -43,7 +43,7 @@ const BottomPanel = () => {
           'border-t border-[hsla(var(--app-border))] bg-[hsla(var(--bottom-panel-bg))]'
       )}
     >
-      <div className="flex flex-shrink-0 items-center gap-x-2">
+      <div className="flex flex-1 items-center gap-x-2">
         <div className="flex items-center space-x-2">
           {progress && progress > 0 ? (
             <UpdateApp total={100} used={progress} />
@@ -54,7 +54,7 @@ const BottomPanel = () => {
         <UpdatedFailedModal />
         <InstallingExtension />
       </div>
-      <div className="flex items-center gap-x-1">
+      <div className="flex flex-shrink-0 items-center gap-x-1">
         <SystemMonitor />
         <span className="font-medium text-[hsla(var(--text-secondary))]">
           Jan v{VERSION ?? ''}
@@ -62,26 +62,25 @@ const BottomPanel = () => {
         <div className="ml-2 flex items-center">
           {menuLinks
             .filter((link) => !!link)
-            .map((link, i) => (
-              <div className="relative" key={i}>
-                <Tooltip
-                  withArrow={false}
-                  side="top"
-                  trigger={
-                    <Button theme="icon">
-                      <a
-                        href={link.link}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="relative flex w-full flex-shrink-0 items-center justify-center no-underline"
-                      >
-                        {link.icon}
-                      </a>
-                    </Button>
-                  }
-                  content={link.name}
-                />
-              </div>
+            .map((link) => (
+              <Tooltip
+                key={link.name}
+                withArrow={false}
+                side="top"
+                trigger={
+                  <Button theme="icon">
+                    <a
+                      href={link.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="relative flex w-full flex-shrink-0 items-center justify-center no-underline"
+                    >
+                      {link.icon}
+                    </a>
+                  </Button>
+                }
+                content={link.name}
+              />
             ))}
         </div>
       </div>
