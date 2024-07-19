3ea2ce034719bcd1d0011d6161d34b7f61669097
James <namnh0122@gmail.com>
2024-07-19 16:53:29 +0700

    chore: put engine to experimental feature
    Signed-off-by: James <namnh0122@gmail.com>

--- web/screens/Settings/SettingLeftPanel/index.tsx
+++ web/screens/Settings/SettingLeftPanel/index.tsx
@@ -1,29 +1,43 @@
 import React from 'react'
 
+import { useAtomValue } from 'jotai'
+
 import LeftPanelContainer from '@/containers/LeftPanelContainer'
 
-import { SettingScreenList } from '..'
+import { SettingScreen, SettingScreenList } from '..'
 
 import SettingItem from './SettingItem'
 
-const SettingLeftPanel: React.FC = () => (
-  <LeftPanelContainer>
-    <div className="flex-shrink-0 px-2 py-3">
-      <div className="mb-1 px-2">
-        <label className="text-xs font-medium text-[hsla(var(--text-secondary))]">
-          General
-        </label>
-      </div>
+import { experimentalFeatureEnabledAtom } from '@/helpers/atoms/AppConfig.atom'
 
-      {SettingScreenList.map((settingScreen) => (
-        <SettingItem
-          key={settingScreen}
-          name={settingScreen}
-          setting={settingScreen}
-        />
-      ))}
-    </div>
-  </LeftPanelContainer>
-)
+const SettingLeftPanel: React.FC = () => {
+  const experimentalEnabled = useAtomValue(experimentalFeatureEnabledAtom)
+
+  const screenList: SettingScreen[] = Array.isArray(SettingScreenList)
+    ? experimentalEnabled
+      ? SettingScreenList
+      : SettingScreenList.filter((screen) => screen !== 'Engines')
+    : []
+
+  return (
+    <LeftPanelContainer>
+      <div className="flex-shrink-0 px-2 py-3">
+        <div className="mb-1 px-2">
+          <label className="text-xs font-medium text-[hsla(var(--text-secondary))]">
+            General
+          </label>
+        </div>
+
+        {screenList.map((settingScreen) => (
+          <SettingItem
+            key={settingScreen}
+            name={settingScreen}
+            setting={settingScreen}
+          />
+        ))}
+      </div>
+    </LeftPanelContainer>
+  )
+}
 
 export default React.memo(SettingLeftPanel)
