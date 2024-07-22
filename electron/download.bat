a73d1bf32b24279508ce7ece619e74f02354ff25
James <namnh0122@gmail.com>
2024-07-22 18:19:59 +0700

    feat: integrate cortex single executable
    Signed-off-by: James <namnh0122@gmail.com>

--- electron/download.bat
+++ electron/download.bat
@@ -1,3 +1,3 @@
 @echo off
 set /p CORTEX_VERSION=<./resources/version.txt
-.\node_modules\.bin\download https://github.com/janhq/cortex/releases/download/v%CORTEX_VERSION%/cortex-%CORTEX_VERSION%-amd64-windows.tar.gz -e -o ./resources/win
+.\node_modules\.bin\download https://github.com/janhq/cortex/releases/download/v%CORTEX_VERSION%/cortex-%CORTEX_VERSION%-amd64-windows.tar.gz -e -s 1 -o ./resources/win
