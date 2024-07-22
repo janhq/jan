ccf450a1c47ab75d654ed1ea13159686a6958852
Faisal Amir <urmauur@gmail.com>
2024-07-22 08:39:22 +0700

    chore: make on-device tab showing 6 items instead of 4

--- web/screens/HubScreen2/components/BuiltInModelGroup.tsx
+++ web/screens/HubScreen2/components/BuiltInModelGroup.tsx
@@ -22,7 +22,7 @@ const BuiltInModelGroup: React.FC<Props> = ({ onSeeAllClick }) => {
 
   const models: HfModelEntry[] = (
     data.modelCategories.get('BuiltInModels') ?? []
-  ).slice(0, 4)
+  ).slice(0, 6)
   if (models.length === 0) return null
 
   return (
