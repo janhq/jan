b1047894d0a3df3af7e6a6b79b843429928e6e31
Faisal Amir <urmauur@gmail.com>
2024-07-24 15:24:56 +0700

    chore: fix linter

--- web/screens/Settings/Advanced/FactoryReset/index.tsx
+++ web/screens/Settings/Advanced/FactoryReset/index.tsx
@@ -12,12 +12,14 @@ const FactoryReset = () => {
     <div className="flex w-full flex-col items-start justify-between gap-4 border-b border-[hsla(var(--app-border))] py-4 first:pt-0 last:border-none sm:flex-row">
       <div className="space-y-1">
         <div className="flex gap-x-2">
-          <h6 className="font-semibold capitalize">Reset to Factory Settings</h6>
+          <h6 className="font-semibold capitalize">
+            Reset to Factory Settings
+          </h6>
         </div>
         <p className="whitespace-pre-wrap font-medium leading-relaxed text-[hsla(var(--text-secondary))]">
           Reset the application to its initial state, deleting all your usage
-          data, including conversation history. This action is irreversible
-          and recommended only if the application is in a corrupted state.
+          data, including conversation history. This action is irreversible and
+          recommended only if the application is in a corrupted state.
         </p>
       </div>
       <Button theme="destructive" onClick={() => setModalValidation(true)}>
