1c474581cdefddf7b06ebf270b606d8866883e1d
James <namnh0122@gmail.com>
2024-07-25 17:01:52 +0700

    chore: temporary hide gpu config

--- web/screens/Settings/Advanced/index.tsx
+++ web/screens/Settings/Advanced/index.tsx
@@ -198,184 +198,184 @@ const Advanced = () => {
         </div>
 
         {/* CPU / GPU switching */}
-        {!isMac && (
-          <div className="flex w-full flex-col items-start justify-between border-b border-[hsla(var(--app-border))] py-4 first:pt-0 last:border-none">
-            <div className="flex w-full items-start justify-between">
-              <div className="space-y-1">
-                <div className="flex gap-x-2">
-                  <h6 className="font-semibold capitalize">GPU Acceleration</h6>
-                </div>
-                <p className="pr-8 leading-relaxed">
-                  Enable to enhance model performance by utilizing your GPU
-                  devices for acceleration. Read{' '}
-                  <span>
-                    {' '}
-                    <span
-                      className="cursor-pointer text-[var(--app-link)]"
-                      // onClick={() =>
-                      //   openExternalUrl(
-                      //     'https://jan.ai/guides/troubleshooting/gpu-not-used/'
-                      //   )
-                      // }
-                    >
-                      troubleshooting guide
-                    </span>{' '}
-                  </span>{' '}
-                  for further assistance.
-                </p>
-              </div>
-
-              <div className="flex items-center">
-                {gpuList.length > 0 && !gpuEnabled && (
-                  <Tooltip
-                    trigger={
-                      <AlertCircleIcon
-                        size={16}
-                        className="mr-2 text-[hsla(var(--warning-bg))]"
-                      />
-                    }
-                    content="Disabling NVIDIA GPU Acceleration may result in reduced
-                    performance. It is recommended to keep this enabled for
-                    optimal user experience."
-                  />
-                )}
-                <Tooltip
-                  trigger={
-                    <Switch
-                      disabled={gpuList.length === 0 || vulkanEnabled}
-                      checked={gpuEnabled}
-                      onChange={(e) => {
-                        if (e.target.checked === true) {
-                          saveSettings({ runMode: 'gpu' })
-                          setGpuEnabled(true)
-                          snackbar({
-                            description:
-                              'Successfully turned on GPU Acceleration',
-                            type: 'success',
-                          })
-                        } else {
-                          saveSettings({ runMode: 'cpu' })
-                          setGpuEnabled(false)
-                          snackbar({
-                            description:
-                              'Successfully turned off GPU Acceleration',
-                            type: 'success',
-                          })
-                        }
-                        // Stop any running model to apply the changes
-                        if (e.target.checked !== gpuEnabled) {
-                          for (const activeModel of activeModels) {
-                            stopModel(activeModel.model)
-                          }
-                        }
-                      }}
-                    />
-                  }
-                  content="Your current device does not have a compatible GPU for
-                  monitoring. To enable GPU monitoring, please ensure your
-                  device has a supported Nvidia or AMD GPU with updated
-                  drivers."
-                  disabled={gpuList.length > 0}
-                />
-              </div>
-            </div>
-            <div className="mt-2 flex w-full flex-col rounded-lg px-2 py-4">
-              <label className="mb-2 mr-2 inline-block font-medium">
-                Choose device(s)
-              </label>
-              <div className="relative flex w-full md:w-1/2" ref={setToggle}>
-                <Input
-                  value={selectedGpu.join() || ''}
-                  className="w-full cursor-pointer"
-                  readOnly
-                  placeholder={gpuSelectionPlaceHolder}
-                  suffixIcon={
-                    <ChevronDownIcon
-                      size={14}
-                      className={twMerge(open && 'rotate-180')}
-                    />
-                  }
-                  onClick={() => setOpen(!open)}
-                />
-                <div
-                  className={twMerge(
-                    'max-h-80 shadow-sm absolute right-0 z-20 mt-10 w-full overflow-hidden rounded-lg border border-[hsla(var(--app-border))] bg-[hsla(var(--app-bg))]',
-                    open ? 'flex' : 'hidden'
-                  )}
-                  ref={setDropdownOptions}
-                >
-                  <div className="w-full p-4">
-                    <p>{vulkanEnabled ? 'Vulkan Supported GPUs' : 'Nvidia'}</p>
-                    <div className="py-2">
-                      <div className="rounded-lg">
-                        {gpuList
-                          .filter((gpu) =>
-                            vulkanEnabled
-                              ? gpu.name
-                              : gpu.name?.toLowerCase().includes('nvidia')
-                          )
-                          .map((gpu) => (
-                            <div
-                              key={gpu.id}
-                              className="mt-2 flex items-center space-x-2"
-                            >
-                              <Checkbox
-                                id={`gpu-${gpu.id}`}
-                                name="gpu-nvidia"
-                                value={gpu.id}
-                                checked={gpusInUse.includes(gpu.id)}
-                                onChange={() => handleGPUChange(gpu.id)}
-                                label={
-                                  <span>
-                                    <span>{gpu.name}</span>
-                                    {!vulkanEnabled && (
-                                      <span>{gpu.vram}MB VRAM</span>
-                                    )}
-                                  </span>
-                                }
-                              />
-                            </div>
-                          ))}
-                      </div>
-                      {gpuEnabled && gpusInUse.length > 1 && (
-                        <div className="mt-2 flex items-start space-x-2 text-[hsla(var(--warning-bg))]">
-                          <AlertTriangleIcon
-                            size={16}
-                            className="flex-shrink-0"
-                          />
-                          <p className="text-xs leading-relaxed">
-                            If multi-GPU is enabled with different GPU models or
-                            without NVLink, it could impact token speed.
-                          </p>
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
+        {/* {!isMac && ( */}
+        {/*   <div className="flex w-full flex-col items-start justify-between border-b border-[hsla(var(--app-border))] py-4 first:pt-0 last:border-none"> */}
+        {/*     <div className="flex w-full items-start justify-between"> */}
+        {/*       <div className="space-y-1"> */}
+        {/*         <div className="flex gap-x-2"> */}
+        {/*           <h6 className="font-semibold capitalize">GPU Acceleration</h6> */}
+        {/*         </div> */}
+        {/*         <p className="pr-8 leading-relaxed"> */}
+        {/*           Enable to enhance model performance by utilizing your GPU */}
+        {/*           devices for acceleration. Read{' '} */}
+        {/*           <span> */}
+        {/*             {' '} */}
+        {/*             <span */}
+        {/*               className="cursor-pointer text-[var(--app-link)]" */}
+        {/*               // onClick={() => */}
+        {/*               //   openExternalUrl( */}
+        {/*               //     'https://jan.ai/guides/troubleshooting/gpu-not-used/' */}
+        {/*               //   ) */}
+        {/*               // } */}
+        {/*             > */}
+        {/*               troubleshooting guide */}
+        {/*             </span>{' '} */}
+        {/*           </span>{' '} */}
+        {/*           for further assistance. */}
+        {/*         </p> */}
+        {/*       </div> */}
+        {/**/}
+        {/*       <div className="flex items-center"> */}
+        {/*         {gpuList.length > 0 && !gpuEnabled && ( */}
+        {/*           <Tooltip */}
+        {/*             trigger={ */}
+        {/*               <AlertCircleIcon */}
+        {/*                 size={16} */}
+        {/*                 className="mr-2 text-[hsla(var(--warning-bg))]" */}
+        {/*               /> */}
+        {/*             } */}
+        {/*             content="Disabling NVIDIA GPU Acceleration may result in reduced */}
+        {/*             performance. It is recommended to keep this enabled for */}
+        {/*             optimal user experience." */}
+        {/*           /> */}
+        {/*         )} */}
+        {/*         <Tooltip */}
+        {/*           trigger={ */}
+        {/*             <Switch */}
+        {/*               disabled={gpuList.length === 0 || vulkanEnabled} */}
+        {/*               checked={gpuEnabled} */}
+        {/*               onChange={(e) => { */}
+        {/*                 if (e.target.checked === true) { */}
+        {/*                   saveSettings({ runMode: 'gpu' }) */}
+        {/*                   setGpuEnabled(true) */}
+        {/*                   snackbar({ */}
+        {/*                     description: */}
+        {/*                       'Successfully turned on GPU Acceleration', */}
+        {/*                     type: 'success', */}
+        {/*                   }) */}
+        {/*                 } else { */}
+        {/*                   saveSettings({ runMode: 'cpu' }) */}
+        {/*                   setGpuEnabled(false) */}
+        {/*                   snackbar({ */}
+        {/*                     description: */}
+        {/*                       'Successfully turned off GPU Acceleration', */}
+        {/*                     type: 'success', */}
+        {/*                   }) */}
+        {/*                 } */}
+        {/*                 // Stop any running model to apply the changes */}
+        {/*                 if (e.target.checked !== gpuEnabled) { */}
+        {/*                   for (const activeModel of activeModels) { */}
+        {/*                     stopModel(activeModel.model) */}
+        {/*                   } */}
+        {/*                 } */}
+        {/*               }} */}
+        {/*             /> */}
+        {/*           } */}
+        {/*           content="Your current device does not have a compatible GPU for */}
+        {/*           monitoring. To enable GPU monitoring, please ensure your */}
+        {/*           device has a supported Nvidia or AMD GPU with updated */}
+        {/*           drivers." */}
+        {/*           disabled={gpuList.length > 0} */}
+        {/*         /> */}
+        {/*       </div> */}
+        {/*     </div> */}
+        {/*     <div className="mt-2 flex w-full flex-col rounded-lg px-2 py-4"> */}
+        {/*       <label className="mb-2 mr-2 inline-block font-medium"> */}
+        {/*         Choose device(s) */}
+        {/*       </label> */}
+        {/*       <div className="relative flex w-full md:w-1/2" ref={setToggle}> */}
+        {/*         <Input */}
+        {/*           value={selectedGpu.join() || ''} */}
+        {/*           className="w-full cursor-pointer" */}
+        {/*           readOnly */}
+        {/*           placeholder={gpuSelectionPlaceHolder} */}
+        {/*           suffixIcon={ */}
+        {/*             <ChevronDownIcon */}
+        {/*               size={14} */}
+        {/*               className={twMerge(open && 'rotate-180')} */}
+        {/*             /> */}
+        {/*           } */}
+        {/*           onClick={() => setOpen(!open)} */}
+        {/*         /> */}
+        {/*         <div */}
+        {/*           className={twMerge( */}
+        {/*             'max-h-80 shadow-sm absolute right-0 z-20 mt-10 w-full overflow-hidden rounded-lg border border-[hsla(var(--app-border))] bg-[hsla(var(--app-bg))]', */}
+        {/*             open ? 'flex' : 'hidden' */}
+        {/*           )} */}
+        {/*           ref={setDropdownOptions} */}
+        {/*         > */}
+        {/*           <div className="w-full p-4"> */}
+        {/*             <p>{vulkanEnabled ? 'Vulkan Supported GPUs' : 'Nvidia'}</p> */}
+        {/*             <div className="py-2"> */}
+        {/*               <div className="rounded-lg"> */}
+        {/*                 {gpuList */}
+        {/*                   .filter((gpu) => */}
+        {/*                     vulkanEnabled */}
+        {/*                       ? gpu.name */}
+        {/*                       : gpu.name?.toLowerCase().includes('nvidia') */}
+        {/*                   ) */}
+        {/*                   .map((gpu) => ( */}
+        {/*                     <div */}
+        {/*                       key={gpu.id} */}
+        {/*                       className="mt-2 flex items-center space-x-2" */}
+        {/*                     > */}
+        {/*                       <Checkbox */}
+        {/*                         id={`gpu-${gpu.id}`} */}
+        {/*                         name="gpu-nvidia" */}
+        {/*                         value={gpu.id} */}
+        {/*                         checked={gpusInUse.includes(gpu.id)} */}
+        {/*                         onChange={() => handleGPUChange(gpu.id)} */}
+        {/*                         label={ */}
+        {/*                           <span> */}
+        {/*                             <span>{gpu.name}</span> */}
+        {/*                             {!vulkanEnabled && ( */}
+        {/*                               <span>{gpu.vram}MB VRAM</span> */}
+        {/*                             )} */}
+        {/*                           </span> */}
+        {/*                         } */}
+        {/*                       /> */}
+        {/*                     </div> */}
+        {/*                   ))} */}
+        {/*               </div> */}
+        {/*               {gpuEnabled && gpusInUse.length > 1 && ( */}
+        {/*                 <div className="mt-2 flex items-start space-x-2 text-[hsla(var(--warning-bg))]"> */}
+        {/*                   <AlertTriangleIcon */}
+        {/*                     size={16} */}
+        {/*                     className="flex-shrink-0" */}
+        {/*                   /> */}
+        {/*                   <p className="text-xs leading-relaxed"> */}
+        {/*                     If multi-GPU is enabled with different GPU models or */}
+        {/*                     without NVLink, it could impact token speed. */}
+        {/*                   </p> */}
+        {/*                 </div> */}
+        {/*               )} */}
+        {/*             </div> */}
+        {/*           </div> */}
+        {/*         </div> */}
+        {/*       </div> */}
+        {/*     </div> */}
+        {/*   </div> */}
+        {/* )} */}
 
         {/* Vulkan for AMD GPU/ APU and Intel Arc GPU */}
-        {!isMac && experimentalEnabled && (
-          <div className="flex w-full flex-col items-start justify-between gap-4 border-b border-[hsla(var(--app-border))] py-4 first:pt-0 last:border-none sm:flex-row">
-            <div className="flex-shrink-0 space-y-1">
-              <div className="flex gap-x-2">
-                <h6 className="font-semibold capitalize">Vulkan Support</h6>
-              </div>
-              <p className="font-medium leading-relaxed text-[hsla(var(--text-secondary))]">
-                Enable Vulkan with AMD GPU/APU and Intel Arc GPU for better
-                model performance (reload needed).
-              </p>
-            </div>
-
-            <Switch
-              checked={vulkanEnabled}
-              onChange={(e) => updateVulkanEnabled(e.target.checked)}
-            />
-          </div>
-        )}
+        {/* {!isMac && experimentalEnabled && ( */}
+        {/*   <div className="flex w-full flex-col items-start justify-between gap-4 border-b border-[hsla(var(--app-border))] py-4 first:pt-0 last:border-none sm:flex-row"> */}
+        {/*     <div className="flex-shrink-0 space-y-1"> */}
+        {/*       <div className="flex gap-x-2"> */}
+        {/*         <h6 className="font-semibold capitalize">Vulkan Support</h6> */}
+        {/*       </div> */}
+        {/*       <p className="font-medium leading-relaxed text-[hsla(var(--text-secondary))]"> */}
+        {/*         Enable Vulkan with AMD GPU/APU and Intel Arc GPU for better */}
+        {/*         model performance (reload needed). */}
+        {/*       </p> */}
+        {/*     </div> */}
+        {/**/}
+        {/*     <Switch */}
+        {/*       checked={vulkanEnabled} */}
+        {/*       onChange={(e) => updateVulkanEnabled(e.target.checked)} */}
+        {/*     /> */}
+        {/*   </div> */}
+        {/* )} */}
 
         {/* <DataFolder /> */}
 
