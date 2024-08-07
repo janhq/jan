946258a4ebf50bad89f27180961f9c6d3212733d
Faisal Amir <urmauur@gmail.com>
2024-08-07 14:46:31 +0700

    chore: disabled option when engine isn't ready

--- web/containers/ModelDropdown/ModelSection.tsx
+++ web/containers/ModelDropdown/ModelSection.tsx
@@ -2,12 +2,15 @@ import { useCallback, useEffect, useState } from 'react'
 
 import Image from 'next/image'
 
-import { LlmEngine, Model, RemoteEngine } from '@janhq/core'
+import { EngineStatus, LlmEngine, Model, RemoteEngine } from '@janhq/core'
 
 import { Button } from '@janhq/joi'
 import { useSetAtom } from 'jotai'
 import { SettingsIcon } from 'lucide-react'
 
+import { twMerge } from 'tailwind-merge'
+
+import useEngineQuery from '@/hooks/useEngineQuery'
 import useGetModelsByEngine from '@/hooks/useGetModelsByEngine'
 
 import { getTitleByCategory } from '@/utils/model-engine'
@@ -30,6 +33,7 @@ const ModelSection: React.FC<Props> = ({
   const [models, setModels] = useState<Model[]>([])
   const { getModelsByEngine } = useGetModelsByEngine()
   const setUpRemoteModelStage = useSetAtom(setUpRemoteModelStageAtom)
+  const { data: engineData } = useEngineQuery()
 
   const engineLogo: string | undefined = models.find(
     (entry) => entry?.metadata?.logo != null
@@ -79,18 +83,32 @@ const ModelSection: React.FC<Props> = ({
         </Button>
       </div>
       <ul className="pb-2">
-        {models.map((model) => (
-          <li
-            key={model.model}
-            className="flex cursor-pointer items-center gap-2 px-3 py-2 hover:bg-[hsla(var(--dropdown-menu-hover-bg))]"
-            onClick={() => onModelSelected(model)}
-          >
-            <div className="flex w-full items-center justify-between">
-              <p className="line-clamp-1">{model.name ?? model.model}</p>
-            </div>
-            <ModelLabel metadata={model.metadata} compact />
-          </li>
-        ))}
+        {models.map((model) => {
+          const isEngineReady =
+            engineData?.find((e) => e.name === model.engine)?.status ===
+            EngineStatus.Ready
+          return (
+            <li
+              key={model.model}
+              className={twMerge(
+                'flex cursor-pointer items-center gap-2 px-3 py-2 hover:bg-[hsla(var(--dropdown-menu-hover-bg))]',
+                isEngineReady
+                  ? 'text-[hsla(var(--text-primary))]'
+                  : 'cursor-not-allowed text-[hsla(var(--text-tertiary))]'
+              )}
+              onClick={() => {
+                if (isEngineReady) {
+                  onModelSelected(model)
+                }
+              }}
+            >
+              <div className="flex w-full items-center justify-between">
+                <p className="line-clamp-1">{model.name ?? model.model}</p>
+              </div>
+              <ModelLabel metadata={model.metadata} compact />
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
