ad6fbea22df6deaba31e146dddb456e4a5d5dd75
Faisal Amir <urmauur@gmail.com>
2024-08-13 20:17:47 +0700

    chore: add engine logo from local instead of metadata logo (#3363)

--- web/utils/model-engine.ts
+++ web/utils/model-engine.ts
@@ -1,4 +1,4 @@
-import { LocalEngine } from '@janhq/core'
+import { LocalEngine, RemoteEngine } from '@janhq/core'
 
 import { ModelHubCategory } from '@/hooks/useModelHub'
 
@@ -52,6 +52,32 @@ export const getLogoByLocalEngine = (engine: LocalEngine) => {
   switch (engine) {
     case 'cortex.llamacpp':
       return 'icons/llamacpp.svg'
+
+    default:
+      return undefined
+  }
+}
+
+export const getLogoByRemoteEngine = (engine: RemoteEngine) => {
+  switch (engine) {
+    case 'anthropic':
+      return 'icons/anthropic.svg'
+    case 'mistral':
+      return 'icons/mistral.svg'
+    case 'martian':
+      return 'icons/martian.svg'
+    case 'openrouter':
+      return 'icons/openrouter.svg'
+    case 'openai':
+      return 'icons/openai.svg'
+    case 'groq':
+      return 'icons/groq.svg'
+    case 'triton_trtllm':
+      return 'icons/triton_trtllm.svg'
+    case 'cohere':
+      return 'icons/cohere.svg'
+    case 'nvidia':
+      return 'icons/nvidia.svg'
 
     default:
       return undefined
