1cdf095e5ec054527112f50c9399dd2356fd55ac
Faisal Amir <urmauur@gmail.com>
2024-08-13 13:30:02 +0700

    feat: added model dropdown for assistand tab

--- web/screens/Thread/ThreadRightPanel/AssistantSettingContainer/index.tsx
+++ web/screens/Thread/ThreadRightPanel/AssistantSettingContainer/index.tsx
@@ -1,9 +1,12 @@
 import React, { useCallback, useEffect, useState } from 'react'
 
-import { TextArea } from '@janhq/joi'
+import { Button, TextArea } from '@janhq/joi'
 
-import { useAtomValue } from 'jotai'
+import { useAtomValue, useSetAtom } from 'jotai'
+import { Settings2Icon } from 'lucide-react'
 import { useDebouncedCallback } from 'use-debounce'
+
+import ModelDropdown from '@/containers/ModelDropdown'
 
 import useUpdateInstruction from '@/hooks/useUpdateInstruction'
 
@@ -12,6 +15,7 @@ import CopyOverInstruction from './components/CopyOverInstruction'
 import { experimentalFeatureEnabledAtom } from '@/helpers/atoms/AppConfig.atom'
 
 import { activeThreadAtom } from '@/helpers/atoms/Thread.atom'
+import { activeTabThreadRightPanelAtom } from '@/helpers/atoms/ThreadRightPanel.atom'
 
 const AssistantSettingContainer: React.FC = () => {
   const experimentalEnabled = useAtomValue(experimentalFeatureEnabledAtom)
@@ -19,6 +23,7 @@ const AssistantSettingContainer: React.FC = () => {
   const [instructions, setInstructions] = useState(
     activeThread?.assistants[0]?.instructions || ''
   )
+  const setActiveTabThreadRightPanel = useSetAtom(activeTabThreadRightPanelAtom)
   const { updateInstruction } = useUpdateInstruction()
 
   useEffect(() => {
@@ -55,6 +60,32 @@ const AssistantSettingContainer: React.FC = () => {
         onChange={onInstructionChanged}
       />
       {experimentalEnabled && <CopyOverInstruction />}
+      <div className="mt-2">
+        <label
+          id="assistant-instructions"
+          className="mb-2 inline-block font-bold"
+        >
+          Model
+        </label>
+        <div className="flex gap-2">
+          <div className="w-full">
+            <ModelDropdown />
+          </div>
+          <Button
+            theme="icon"
+            variant="outline"
+            className="!h-8 !w-8 flex-shrink-0"
+            onClick={() => {
+              setActiveTabThreadRightPanel('model')
+            }}
+          >
+            <Settings2Icon
+              size={16}
+              className="flex-shrink-0 cursor-pointer text-[hsla(var(--text-secondary))]"
+            />
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
