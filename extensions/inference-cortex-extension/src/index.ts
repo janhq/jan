0996ecaa1dc2cd94571689254378b670f233a536
Louis <louis@jan.ai>
2024-08-19 10:49:27 +0700

    feat: initialize extension boilerplate

---
+++ extensions/inference-cortex-extension/src/index.ts
@@ -0,0 +1,46 @@
+/**
+ * @file This file exports a class that implements the InferenceExtension interface from the @janhq/core package.
+ * The class provides methods for initializing and stopping a model, and for making inference requests.
+ * It also subscribes to events emitted by the @janhq/core package and handles new message requests.
+ * @version 1.0.0
+ * @module inference-extension/src/index
+ */
+
+import { Model, LocalOAIEngine } from '@janhq/core'
+
+/**
+ * A class that implements the InferenceExtension interface from the @janhq/core package.
+ * The class provides methods for initializing and stopping a model, and for making inference requests.
+ * It also subscribes to events emitted by the @janhq/core package and handles new message requests.
+ */
+export default class JanInferenceNitroExtension extends LocalOAIEngine {
+  nodeModule: string = NODE
+  provider: string = 'cortex'
+  /**
+   * The URL for making inference requests.
+   */
+  inferenceUrl = ''
+
+  /**
+   * Subscribes to events emitted by the @janhq/core package.
+   */
+  async onLoad() {
+    this.inferenceUrl = INFERENCE_URL
+
+    const models = MODELS as unknown as Model[]
+    this.registerModels(models)
+    super.onLoad()
+  }
+
+  override loadModel(model: Model): Promise<void> {
+    if (model.engine !== this.provider) return Promise.resolve()
+
+    return super.loadModel(model)
+  }
+
+  override async unloadModel(model?: Model): Promise<void> {
+    if (model?.engine && model.engine !== this.provider) return
+
+    return super.unloadModel(model)
+  }
+}
