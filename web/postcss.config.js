7a6e37bfffccc38a62fe19f19dd9369f4924aa2f
Faisal Amir <urmauur@gmail.com>
2024-08-20 10:17:29 +0700

    chore: add postcss-url

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,6 +1,19 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    'tailwindcss': {},
+    'autoprefixer': {},
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    'postcss-url': {
+      url: (asset) => {
+        if (asset.url.startsWith('./_next/static/media/')) {
+          return asset.url.replace(
+            './_next/static/media/',
+            '/_next/static/media/'
+          )
+        }
+        // Leave other assets as they are
+        return asset.url
+      },
+    },
   },
 }
