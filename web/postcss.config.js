b740414ef5cd50b08fc0b2ab0b8867c95ccb6ca7
Faisal Amir <urmauur@gmail.com>
2024-08-22 00:49:14 +0700

    fix: font katex

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,19 +1,6 @@
 module.exports = {
   plugins: {
-    'tailwindcss': {},
-    'autoprefixer': {},
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    'postcss-url': {
-      url: (asset) => {
-        if (asset.url.startsWith('./_next/static/media/')) {
-          return asset.url.replace(
-            './_next/static/media/',
-            '/_next/static/media/'
-          )
-        }
-        // Leave other assets as they are
-        return asset.url
-      },
-    },
+    tailwindcss: {},
+    autoprefixer: {},
   },
 }
