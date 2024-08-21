a609c1bb9bd32b727c43579bfbe60e0fae9951ba
marknguyen1302 <nguyenvu1302.work@gmail.com>
2024-08-22 03:41:54 +0700

    add downloading indicator

--- web/helpers/atoms/Extension.atom.ts
+++ web/helpers/atoms/Extension.atom.ts
@@ -9,6 +9,12 @@ export type InstallingExtensionState = {
   localPath?: string
 }
 
+export type InstallingExtensionPackage = {
+  extensionId: ExtensionId
+  packageName: string
+  percentage: number
+}
+
 export const installingExtensionAtom = atom<InstallingExtensionState[]>([])
 
 export const setInstallingExtensionAtom = atom(
@@ -31,12 +37,51 @@ export const setInstallingExtensionAtom = atom(
   }
 )
 
+export const installingPackageAtom = atom<InstallingExtensionPackage[]>([])
+
+export const setInstallingPackageAtom = atom(
+  null,
+  (get, set, extensionId: string, state: InstallingExtensionPackage) => {
+    const current = get(installingPackageAtom)
+
+    const isExists = current.some(
+      (e) =>
+        e.extensionId === extensionId && e.packageName === state.packageName
+    )
+    if (isExists) {
+      const newCurrent = current.map((e) => {
+        if (
+          e.extensionId === extensionId &&
+          e.packageName === state.packageName
+        ) {
+          return state
+        }
+        return e
+      })
+      set(installingPackageAtom, newCurrent)
+    } else {
+      set(installingPackageAtom, [...current, state])
+    }
+  }
+)
+
 export const removeInstallingExtensionAtom = atom(
   null,
   (get, set, extensionId: string) => {
     const current = get(installingExtensionAtom)
     const newCurrent = current.filter((e) => e.extensionId !== extensionId)
     set(installingExtensionAtom, newCurrent)
+  }
+)
+
+export const removeInstallingPackageAtom = atom(
+  null,
+  (get, set, extensionId: string, packageName: string) => {
+    const current = get(installingPackageAtom)
+    const newCurrent = current.filter(
+      (e) => e.extensionId !== extensionId && e.packageName !== packageName
+    )
+    set(installingPackageAtom, newCurrent)
   }
 )
 
