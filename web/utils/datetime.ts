cee1536941d902f00196d0f92dc007e4b3d55e06
Faisal Amir <urmauur@gmail.com>
2024-08-22 12:03:43 +0700

    fix: date format for chat item

--- web/utils/datetime.ts
+++ web/utils/datetime.ts
@@ -1,17 +1,32 @@
 export const isToday = (timestamp: number) => {
   const today = new Date()
-  return today.setHours(0, 0, 0, 0) == new Date(timestamp).setHours(0, 0, 0, 0)
+  return today.setHours(0, 0, 0, 0) === new Date(timestamp).setHours(0, 0, 0, 0)
 }
 
 export const displayDate = (timestamp?: string | number | Date) => {
   if (!timestamp) return 'N/A'
 
-  let displayDate = new Date(timestamp).toLocaleString()
+  const date = new Date(timestamp)
+
+  let displayDate = `${date.toLocaleDateString(undefined, {
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric',
+  })}, ${date.toLocaleTimeString(undefined, {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true,
+  })}`
+
   if (typeof timestamp === 'number' && isToday(timestamp)) {
-    displayDate = new Date(timestamp).toLocaleTimeString(undefined, {
+    displayDate = date.toLocaleTimeString(undefined, {
       hour: '2-digit',
       minute: '2-digit',
+      second: '2-digit',
+      hour12: true,
     })
   }
+
   return displayDate
 }
