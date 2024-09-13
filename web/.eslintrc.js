50d2a89e6cce88a46b8fa54141f0d2ca184a96a8
Louis <louis@jan.ai>
2024-09-13 14:34:22 +0700

    chore: deprecate preserve thread settings

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -71,6 +71,7 @@ module.exports = {
     '@next/next/no-img-element': 'off',
     '@next/next/no-html-link-for-pages': 'off',
     'react/display-name': 'off',
+    'react/no-is-mounted': 'off',
     'react-hooks/rules-of-hooks': 'error',
     '@typescript-eslint/no-unused-vars': ['warn'],
     'import/order': [
