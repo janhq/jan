89f7599e8711e4e507cc288b0d6eb93bf1fab137
Faisal Amir <urmauur@gmail.com>
2024-09-16 10:10:43 +0700

    chore: temporary disabeld starter screen

--- web/screens/Thread/index.tsx
+++ web/screens/Thread/index.tsx
@@ -1,80 +1,81 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
-import { useAtomValue } from 'jotai'
+// import { useAtomValue } from 'jotai'
 
 import ThreadLeftPanel from '@/screens/Thread/ThreadLeftPanel'
 
-import { localEngines } from '@/utils/modelEngine'
+// import { localEngines } from '@/utils/modelEngine'
 
 import ThreadCenterPanel from './ThreadCenterPanel'
-import OnDeviceStarterScreen from './ThreadCenterPanel/ChatBody/OnDeviceStarterScreen'
+// import OnDeviceStarterScreen from './ThreadCenterPanel/ChatBody/OnDeviceStarterScreen'
 import ModalCleanThread from './ThreadLeftPanel/ModalCleanThread'
 import ModalDeleteThread from './ThreadLeftPanel/ModalDeleteThread'
 import ModalEditTitleThread from './ThreadLeftPanel/ModalEditTitleThread'
 import ThreadRightPanel from './ThreadRightPanel'
 
-import { extensionManager } from '@/extension'
-import { downloadedModelsAtom } from '@/helpers/atoms/Model.atom'
-import { threadsAtom } from '@/helpers/atoms/Thread.atom'
+// import { extensionManager } from '@/extension'
+// import { downloadedModelsAtom } from '@/helpers/atoms/Model.atom'
+// import { threadsAtom } from '@/helpers/atoms/Thread.atom'
 
 const ThreadScreen = () => {
-  const downloadedModels = useAtomValue(downloadedModelsAtom)
-  const threads = useAtomValue(threadsAtom)
+  // const downloadedModels = useAtomValue(downloadedModelsAtom)
+  // const threads = useAtomValue(threadsAtom)
 
-  const isDownloadALocalModel = downloadedModels.some((x) =>
-    localEngines.includes(x.engine)
-  )
+  // const isDownloadALocalModel = downloadedModels.some((x) =>
+  //   localEngines.includes(x.engine)
+  // )
 
-  const [extensionHasSettings, setExtensionHasSettings] = useState<
-    { name?: string; setting: string; apiKey: string; provider: string }[]
-  >([])
+  // const [extensionHasSettings, setExtensionHasSettings] = useState<
+  //   { name?: string; setting: string; apiKey: string; provider: string }[]
+  // >([])
 
-  useEffect(() => {
-    const getAllSettings = async () => {
-      const extensionsMenu: {
-        name?: string
-        setting: string
-        apiKey: string
-        provider: string
-      }[] = []
-      const extensions = extensionManager.getAll()
+  // useEffect(() => {
+  //   const getAllSettings = async () => {
+  //     const extensionsMenu: {
+  //       name?: string
+  //       setting: string
+  //       apiKey: string
+  //       provider: string
+  //     }[] = []
+  //     const extensions = extensionManager.getAll()
 
-      for (const extension of extensions) {
-        if (typeof extension.getSettings === 'function') {
-          const settings = await extension.getSettings()
+  //     for (const extension of extensions) {
+  //       if (typeof extension.getSettings === 'function') {
+  //         const settings = await extension.getSettings()
 
-          if (
-            (settings && settings.length > 0) ||
-            (await extension.installationState()) !== 'NotRequired'
-          ) {
-            extensionsMenu.push({
-              name: extension.productName,
-              setting: extension.name,
-              apiKey:
-                'apiKey' in extension && typeof extension.apiKey === 'string'
-                  ? extension.apiKey
-                  : '',
-              provider:
-                'provider' in extension &&
-                typeof extension.provider === 'string'
-                  ? extension.provider
-                  : '',
-            })
-          }
-        }
-      }
-      setExtensionHasSettings(extensionsMenu)
-    }
-    getAllSettings()
-  }, [])
+  //         if (
+  //           (settings && settings.length > 0) ||
+  //           (await extension.installationState()) !== 'NotRequired'
+  //         ) {
+  //           extensionsMenu.push({
+  //             name: extension.productName,
+  //             setting: extension.name,
+  //             apiKey:
+  //               'apiKey' in extension && typeof extension.apiKey === 'string'
+  //                 ? extension.apiKey
+  //                 : '',
+  //             provider:
+  //               'provider' in extension &&
+  //               typeof extension.provider === 'string'
+  //                 ? extension.provider
+  //                 : '',
+  //           })
+  //         }
+  //       }
+  //     }
+  //     setExtensionHasSettings(extensionsMenu)
+  //   }
+  //   getAllSettings()
+  // }, [])
 
-  const isAnyRemoteModelConfigured = extensionHasSettings.some(
-    (x) => x.apiKey.length > 1
-  )
+  // const isAnyRemoteModelConfigured = extensionHasSettings.some(
+  //   (x) => x.apiKey.length > 1
+  // )
 
   return (
     <div className="relative flex h-full w-full flex-1 overflow-x-hidden">
-      {!isAnyRemoteModelConfigured &&
+      {/* Temporary disable this */}
+      {/* {!isAnyRemoteModelConfigured &&
       !isDownloadALocalModel &&
       !threads.length ? (
         <>
@@ -86,7 +87,11 @@ const ThreadScreen = () => {
           <ThreadCenterPanel />
           <ThreadRightPanel />
         </>
-      )}
+      )} */}
+
+      <ThreadLeftPanel />
+      <ThreadCenterPanel />
+      <ThreadRightPanel />
 
       {/* Showing variant modal action for thread screen */}
       <ModalEditTitleThread />
