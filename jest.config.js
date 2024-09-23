87c8fdf5acf0d57b244942ab4d454e136a0f0f68
Louis <louis@jan.ai>
2024-09-23 16:13:45 +0700

    fix: the monorepo jest configs should not cover extensions - they would be moved to mini repositories soon (#3723)

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-  projects: [
-    '<rootDir>/core',
-    '<rootDir>/web',
-    '<rootDir>/joi',
-    '<rootDir>/extensions/inference-nitro-extension',
-    '<rootDir>/extensions/conversational-extension',
-    '<rootDir>/extensions/model-extension',
-  ],
+  projects: ['<rootDir>/core', '<rootDir>/web', '<rootDir>/joi'],
 }
