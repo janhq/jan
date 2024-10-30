5ddbf5fb342af8fae9f7e684328e7e5b6dfd8d65
Louis <louis@jan.ai>
2024-10-30 16:08:13 +0700

    fix: unlink the entire model folder on delete

--- extensions/model-extension/src/legacy/delete.ts
+++ extensions/model-extension/src/legacy/delete.ts
@@ -1,18 +1,11 @@
-import { fs, joinPath, Model } from "@janhq/core"
+import { fs, joinPath, Model } from '@janhq/core'
 
 export const deleteModelFiles = async (model: Model) => {
-    try {
-        const dirPath = await joinPath(['file://models', model.id])
-
-        // remove all files under dirPath except model.json
-        const files = await fs.readdirSync(dirPath)
-        const deletePromises = files.map(async (fileName: string) => {
-            if (fileName !== 'model.json') {
-                return fs.unlinkSync(await joinPath([dirPath, fileName]))
-            }
-        })
-        await Promise.allSettled(deletePromises)
-    } catch (err) {
-        console.error(err)
-    }
+  try {
+    const dirPath = await joinPath(['file://models', model.id])
+    // remove model folder directory
+    await fs.unlinkSync(dirPath)
+  } catch (err) {
+    console.error(err)
+  }
 }
