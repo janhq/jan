1ad897782cb764abd46a54720993594ea2bf6a74
hiento09 <136591877+hiento09@users.noreply.github.com>
2024-11-05 19:40:19 +0700

    feat: new nightly app channel (#3948)
    Co-authored-by: Hien To <tominhhien97@gmail.com>

--- .github/scripts/rename-uninstaller.sh
+++ .github/scripts/rename-uninstaller.sh
@@ -3,6 +3,14 @@
 # File path to be modified
 FILE_PATH="electron/scripts/uninstaller.nsh"
 
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <channel>"
+    exit 1
+fi
+
+CHANNEL="$1"
+
 # Check if the file exists
 if [ ! -f "$FILE_PATH" ]; then
     echo "File does not exist: $FILE_PATH"
@@ -10,7 +18,7 @@ if [ ! -f "$FILE_PATH" ]; then
 fi
 
 # Perform the replacements
-sed -i -e "s#jan#jan-beta#g" "$FILE_PATH"
+sed -i -e "s#jan#jan-$CHANNEL#g" "$FILE_PATH"
 
 # Notify completion
 echo "File has been updated: $FILE_PATH"
