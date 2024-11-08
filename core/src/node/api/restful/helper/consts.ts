81015bab3c042d5bef10381e5f40cd67e8d69f3d
Louis <louis@jan.ai>
2024-11-08 18:03:37 +0700

    chore: clean nitro stuffs

--- core/src/node/api/restful/helper/consts.ts
+++ core/src/node/api/restful/helper/consts.ts
@@ -1,9 +1,7 @@
-// The PORT to use for the Nitro subprocess
 export const CORTEX_DEFAULT_PORT = 39291
 
-// The HOST address to use for the Nitro subprocess
 export const LOCAL_HOST = '127.0.0.1'
 
 export const SUPPORTED_MODEL_FORMAT = '.gguf'
 
-export const CORTEX_API_URL = `http://${LOCAL_HOST}:${CORTEX_DEFAULT_PORT}/v1` // default nitro url
+export const CORTEX_API_URL = `http://${LOCAL_HOST}:${CORTEX_DEFAULT_PORT}/v1`
