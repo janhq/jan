d766d019766e0671e7b170d8995d1f08babb0dd6
Faisal Amir <urmauur@gmail.com>
2024-11-13 09:57:34 +0700

    chore: update ui code block

--- web/screens/Thread/ThreadCenterPanel/SimpleTextMessage/index.tsx
+++ web/screens/Thread/ThreadCenterPanel/SimpleTextMessage/index.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/naming-convention */
 import React, { useEffect, useState } from 'react'
 
 import Markdown from 'react-markdown'
-import SyntaxHighlighter from 'react-syntax-highlighter'
 
 import {
   ChatCompletionRole,
@@ -13,11 +15,13 @@ import {
 import { Tooltip } from '@janhq/joi'
 
 import { useAtomValue } from 'jotai'
-import { CopyIcon, FolderOpenIcon } from 'lucide-react'
-import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import { FolderOpenIcon } from 'lucide-react'
+import rehypeHighlight from 'rehype-highlight'
+import rehypeHighlightCodeLines from 'rehype-highlight-code-lines'
 import rehypeKatex from 'rehype-katex'
+import rehypeRaw from 'rehype-raw'
 import remarkMath from 'remark-math'
-
+import 'katex/dist/katex.min.css'
 import { twMerge } from 'tailwind-merge'
 
 import LogoMark from '@/containers/Brand/Logo/Mark'
@@ -56,12 +60,180 @@ const SimpleTextMessage: React.FC<ThreadMessage> = (props) => {
 
   const clipboard = useClipboard({ timeout: 1000 })
 
+  function extractCodeLines(node: { children: { children: any[] }[] }) {
+    const codeLines: any[] = []
+
+    // Helper function to extract text recursively from children
+    function getTextFromNode(node: {
+      type: string
+      value: any
+      children: any[]
+    }): string {
+      if (node.type === 'text') {
+        return node.value
+      } else if (node.children) {
+        return node.children.map(getTextFromNode).join('')
+      }
+      return ''
+    }
+
+    // Traverse each line in the <code> block
+    node.children[0].children.forEach(
+      (lineNode: {
+        type: string
+        tagName: string
+        value: any
+        children: any[]
+      }) => {
+        if (lineNode.type === 'element' && lineNode.tagName === 'span') {
+          const lineContent = getTextFromNode(lineNode)
+          codeLines.push(lineContent)
+        }
+      }
+    )
+
+    return codeLines
+  }
+
+  function wrapCodeBlocksWithoutVisit() {
+    return (tree: { children: any[] }) => {
+      tree.children = tree.children.map((node) => {
+        if (node.tagName === 'pre' && node.children[0]?.tagName === 'code') {
+          const language = node.children[0].properties.className?.[1]?.replace(
+            'language-',
+            ''
+          )
+
+          if (!language) return node
+
+          return {
+            type: 'element',
+            tagName: 'div',
+            properties: {
+              className: ['code-block-wrapper'],
+            },
+            children: [
+              {
+                type: 'element',
+                tagName: 'div',
+                properties: {
+                  className: [
+                    'code-block',
+                    'group/item',
+                    'relative',
+                    'my-4',
+                    'overflow-auto',
+                  ],
+                },
+                children: [
+                  {
+                    type: 'element',
+                    tagName: 'div',
+                    properties: {
+                      className:
+                        'code-header bg-[hsla(var(--app-code-block))] flex justify-between items-center py-2 px-3 border-b border-[hsla(var(--app-border))] rounded-t-lg',
+                    },
+                    children: [
+                      {
+                        type: 'element',
+                        tagName: 'span',
+                        properties: {
+                          className: 'text-xs font-medium',
+                        },
+                        children: [
+                          {
+                            type: 'text',
+                            value: language
+                              ? `${getLanguageFromExtension(language)}`
+                              : 'No file name',
+                          },
+                        ],
+                      },
+                      {
+                        type: 'element',
+                        tagName: 'button',
+                        properties: {
+                          className:
+                            'copy-button ml-auto flex items-center gap-1 text-xs font-medium text-gray-400 hover:text-gray-600 focus:outline-none',
+                          onClick: (event: Event) => {
+                            clipboard.copy(extractCodeLines(node))
+
+                            const button = event.currentTarget as HTMLElement
+                            button.innerHTML = `
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check pointer-events-none text-green-600"><path d="M20 6 9 17l-5-5"/></svg>
+                          <span>Copied</span>
+                        `
+
+                            setTimeout(() => {
+                              button.innerHTML = `
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-copy pointer-events-none text-gray-400"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg>
+                            <span>Copy</span>
+                          `
+                            }, 2000)
+                          },
+                        },
+                        children: [
+                          {
+                            type: 'element',
+                            tagName: 'svg',
+                            properties: {
+                              xmlns: 'http://www.w3.org/2000/svg',
+                              width: '16',
+                              height: '16',
+                              viewBox: '0 0 24 24',
+                              fill: 'none',
+                              stroke: 'currentColor',
+                              strokeWidth: '2',
+                              strokeLinecap: 'round',
+                              strokeLinejoin: 'round',
+                              className:
+                                'lucide lucide-copy pointer-events-none text-gray-400',
+                            },
+                            children: [
+                              {
+                                type: 'element',
+                                tagName: 'rect',
+                                properties: {
+                                  width: '14',
+                                  height: '14',
+                                  x: '8',
+                                  y: '8',
+                                  rx: '2',
+                                  ry: '2',
+                                },
+                                children: [],
+                              },
+                              {
+                                type: 'element',
+                                tagName: 'path',
+                                properties: {
+                                  d: 'M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2',
+                                },
+                                children: [],
+                              },
+                            ],
+                          },
+                          { type: 'text', value: 'Copy' },
+                        ],
+                      },
+                    ],
+                  },
+                  node,
+                ],
+              },
+            ],
+          }
+        }
+        return node
+      })
+    }
+  }
+
   const { onViewFile, onViewFileContainer } = usePath()
   const [tokenCount, setTokenCount] = useState(0)
   const [lastTimestamp, setLastTimestamp] = useState<number | undefined>()
   const [tokenSpeed, setTokenSpeed] = useState(0)
   const messages = useAtomValue(getCurrentChatMessagesAtom)
-  const [copiedMap, setCopiedMap] = useState(new Map())
 
   useEffect(() => {
     if (props.status !== MessageStatus.Pending) {
@@ -217,71 +389,20 @@ const SimpleTextMessage: React.FC<ThreadMessage> = (props) => {
             >
               <Markdown
                 remarkPlugins={[remarkMath]}
-                rehypePlugins={[rehypeKatex]}
-                components={{
-                  code(props) {
-                    const { children, className, ...rest } = props
-                    const match = /language-(\w+)/.exec(className || '')
-                    const uniqueKey =
-                      className + String(children).substring(0, 10)
-
-                    return match ? (
-                      <div className="code-block group/item relative my-4 overflow-auto">
-                        <div className="flex select-none items-center justify-between rounded-t-md border-b border-neutral-700 bg-[hsla(var(--app-code-block))] px-4 py-2 font-sans text-xs font-medium text-neutral-200">
-                          <p>{getLanguageFromExtension(match && match[1])}</p>
-                          <div
-                            className="flex cursor-pointer items-center gap-1 rounded-md px-2 py-1 transition-colors hover:bg-neutral-700"
-                            onClick={() => {
-                              clipboard.copy(
-                                String(children).replace(/\n$/, '')
-                              )
-                              const newCopiedMap = new Map(copiedMap).set(
-                                uniqueKey,
-                                true
-                              )
-                              setCopiedMap(newCopiedMap)
-                              setTimeout(() => {
-                                const resetMap = new Map(newCopiedMap)
-                                resetMap.set(uniqueKey, false)
-                                setCopiedMap(resetMap)
-                              }, 2000)
-                            }}
-                          >
-                            {clipboard.copied && copiedMap.get(uniqueKey) ? (
-                              <>
-                                <CopyIcon size={14} />
-                                <span>copied!</span>
-                              </>
-                            ) : (
-                              <>
-                                <CopyIcon size={14} />
-                                <span>Copy</span>
-                              </>
-                            )}
-                          </div>
-                        </div>
-                        <SyntaxHighlighter
-                          showLineNumbers={
-                            String(children).split('\n').length > 2
-                          }
-                          language={
-                            match && match[1] === 'js'
-                              ? 'javascript'
-                              : match[1] || 'javascript'
-                          }
-                          style={a11yDark}
-                          useInlineStyles={false}
-                        >
-                          {String(children).replace(/\n$/, '')}
-                        </SyntaxHighlighter>
-                      </div>
-                    ) : (
-                      <code {...rest} className={className}>
-                        {children}
-                      </code>
-                    )
-                  },
-                }}
+                rehypePlugins={[
+                  rehypeKatex,
+                  rehypeRaw,
+                  [
+                    rehypeHighlight,
+                    {
+                      detect: false,
+                      ignoreMissing: true,
+                    },
+                  ],
+                  [rehypeHighlightCodeLines, { showLineNumbers: true }],
+                  wrapCodeBlocksWithoutVisit,
+                ]}
+                skipHtml
               >
                 {text}
               </Markdown>
