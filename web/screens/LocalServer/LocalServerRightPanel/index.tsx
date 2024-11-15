07ae1e36c38fe3d78bdcc49fb8053125419120dc
Faisal Amir <urmauur@gmail.com>
2024-11-15 15:19:40 +0700

    fix: api local server max ctx len not update when switch model

--- web/screens/LocalServer/LocalServerRightPanel/index.tsx
+++ web/screens/LocalServer/LocalServerRightPanel/index.tsx
@@ -19,8 +19,10 @@ import { getConfigurationsData } from '@/utils/componentSettings'
 
 import { serverEnabledAtom } from '@/helpers/atoms/LocalServer.atom'
 import { selectedModelAtom } from '@/helpers/atoms/Model.atom'
+import { getActiveThreadModelParamsAtom } from '@/helpers/atoms/Thread.atom'
 
 const LocalServerRightPanel = () => {
+  const activeModelParams = useAtomValue(getActiveThreadModelParamsAtom)
   const loadModelError = useAtomValue(loadModelErrorAtom)
   const serverEnabled = useAtomValue(serverEnabledAtom)
   const setModalTroubleShooting = useSetAtom(modalTroubleShootingAtom)
@@ -48,8 +50,17 @@ const LocalServerRightPanel = () => {
     selectedModel
   )
 
+  const modelEngineParams = extractModelLoadParams(
+    {
+      ...selectedModel?.settings,
+      ...activeModelParams,
+    },
+    selectedModel?.settings
+  )
+
   const componentDataEngineSetting = getConfigurationsData(
-    currentModelSettingParams
+    modelEngineParams,
+    selectedModel
   )
 
   const engineSettings = useMemo(
@@ -57,8 +68,11 @@ const LocalServerRightPanel = () => {
       componentDataEngineSetting.filter(
         (x) => x.key !== 'prompt_template' && x.key !== 'embedding'
       ),
+
     [componentDataEngineSetting]
   )
+
+  console.log(engineSettings)
 
   const modelSettings = useMemo(() => {
     return componentDataRuntimeSetting.filter(
