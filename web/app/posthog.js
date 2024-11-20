786c16cc6f9c96ee9362b97f36ac895478b26833
Faisal Amir <urmauur@gmail.com>
2024-11-20 23:40:52 +0700

    chore: profilce always identified

--- web/app/posthog.js
+++ web/app/posthog.js
@@ -6,7 +6,7 @@ import { PostHogProvider } from 'posthog-js/react'
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+    person_profiles: 'always',
     persistence: 'localStorage',
   })
 }
