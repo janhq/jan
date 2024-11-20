1a59b8cecb0e748ef4c041ae33815c0ae5cd81d7
Faisal Amir <urmauur@gmail.com>
2024-11-20 22:39:33 +0700

    chore: add persistence localStorage

--- web/app/posthog.js
+++ web/app/posthog.js
@@ -7,6 +7,7 @@ if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+    persistence: 'localStorage',
   })
 }
 export function CSPostHogProvider({ children }) {
