4da37087b6a46a38870840b3e4611971b7b48e99
Louis <louis@jan.ai>
2024-12-03 16:01:43 +0700

    fix: 4162 - Graceful Openrouter error handling

--- core/src/browser/extensions/engines/OAIEngine.ts
+++ core/src/browser/extensions/engines/OAIEngine.ts
@@ -132,9 +132,6 @@ export abstract class OAIEngine extends AIEngine {
         events.emit(MessageEvent.OnMessageUpdate, message)
       },
       error: async (err: any) => {
-        console.debug('inference url: ', this.inferenceUrl)
-        console.debug('header: ', header)
-        console.error(`Inference error:`, JSON.stringify(err))
         if (this.isCancelled || message.content.length) {
           message.status = MessageStatus.Stopped
           events.emit(MessageEvent.OnMessageUpdate, message)
