4cd0e63029a73e0d54be75fde3f3c3004461c783
Louis <louis@jan.ai>
2024-12-19 11:16:17 +0700

    fix: create new thread with default assistant

--- web/helpers/atoms/Assistant.atom.ts
+++ web/helpers/atoms/Assistant.atom.ts
@@ -4,15 +4,9 @@ import { atomWithStorage } from 'jotai/utils'
 
 export const assistantsAtom = atom<Assistant[]>([])
 
-export const cachedAssistantAtom = atomWithStorage<
-  ThreadAssistantInfo | undefined
->('activeAssistant', undefined, undefined, { getOnInit: true })
 /**
  * Get the current active assistant
  */
-export const activeAssistantAtom = atom(
-  (get) => get(cachedAssistantAtom) ?? get(assistantsAtom)[0],
-  (_get, set, newAssistant: ThreadAssistantInfo) => {
-    set(cachedAssistantAtom, newAssistant)
-  }
-)
+export const activeAssistantAtom = atomWithStorage<
+  ThreadAssistantInfo | undefined
+>('activeAssistant', undefined, undefined, { getOnInit: true })
--- web/screens/Thread/ThreadCenterPanel/TextMessage/index.tsx
+++ web/screens/Thread/ThreadCenterPanel/TextMessage/index.tsx
@@ -88,9 +88,7 @@ const MessageContainer: React.FC<
         >
           {isUser
             ? props.role
-            : 'assistant_name' in activeAssistant
-              ? activeAssistant?.assistant_name
-              : props.role}
+            : (activeAssistant?.assistant_name ?? props.role)}
         </div>
         <p className="text-xs font-medium text-gray-400">
           {props.created_at && displayDate(props.created_at ?? new Date())}
