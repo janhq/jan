552fa71752dc696e35690c9dceccab123fc5e813
Louis <louis@jan.ai>
2024-12-19 16:04:17 +0700

    chore: deprecate Jan core's REST module - all API requests go to cortex.cpp - use cortex.cpp swagger OpenAPI schema

--- core/src/node/api/restful/helper/consts.test.ts
+++
@@ -1,5 +0,0 @@
-import { CORTEX_DEFAULT_PORT } from './consts'
-
-it('should test CORTEX_DEFAULT_PORT', () => {
-  expect(CORTEX_DEFAULT_PORT).toBe(39291)
-})
--- core/src/node/api/restful/helper/consts.ts
+++
@@ -1,7 +0,0 @@
-export const CORTEX_DEFAULT_PORT = 39291
-
-export const LOCAL_HOST = '127.0.0.1'
-
-export const SUPPORTED_MODEL_FORMAT = '.gguf'
-
-export const CORTEX_API_URL = `http://${LOCAL_HOST}:${CORTEX_DEFAULT_PORT}/v1`
