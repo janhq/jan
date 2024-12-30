b8bd8467444977453d9b02a608cd9693174c1a79
Louis <louis@jan.ai>
2024-12-30 22:00:03 +0700

    chore: refactor extensions build script for optimization

--- extensions/engine-management-extension/rolldown.config.mjs
+++ extensions/engine-management-extension/rolldown.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'rolldown'
-import replace from '@rollup/plugin-replace'
 import pkgJson from './package.json' with { type: 'json' }
 
 export default defineConfig([
@@ -9,14 +8,12 @@ export default defineConfig([
       format: 'esm',
       file: 'dist/index.js',
     },
-    plugins: [
-      replace({
-        NODE: JSON.stringify(`${pkgJson.name}/${pkgJson.node}`),
-        API_URL: JSON.stringify('http://127.0.0.1:39291'),
-        SOCKET_URL: JSON.stringify('ws://127.0.0.1:39291'),
-        CORTEX_ENGINE_VERSION: JSON.stringify('v0.1.42'),
-      }),
-    ],
+    define: {
+      NODE: JSON.stringify(`${pkgJson.name}/${pkgJson.node}`),
+      API_URL: JSON.stringify('http://127.0.0.1:39291'),
+      SOCKET_URL: JSON.stringify('ws://127.0.0.1:39291'),
+      CORTEX_ENGINE_VERSION: JSON.stringify('v0.1.42'),
+    },
   },
   {
     input: 'src/node/index.ts',
@@ -25,11 +22,9 @@ export default defineConfig([
       format: 'cjs',
       file: 'dist/node/index.cjs.js',
     },
-    plugins: [
-      replace({
-        CORTEX_ENGINE_VERSION: JSON.stringify('v0.1.42'),
-      }),
-    ],
+    define: {
+      CORTEX_ENGINE_VERSION: JSON.stringify('v0.1.42'),
+    },
   },
   {
     input: 'src/node/cpuInfo.ts',
