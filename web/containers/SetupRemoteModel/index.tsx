e108f49032638ff8056bf0b4a59d4ba37d2251b2
Faisal Amir <urmauur@gmail.com>
2025-01-06 22:10:25 +0700

    chore: setup button gear / plus based apikey

--- web/containers/SetupRemoteModel/index.tsx
+++ web/containers/SetupRemoteModel/index.tsx
@@ -8,7 +8,6 @@ import { SettingsIcon, PlusIcon } from 'lucide-react'
 
 import { MainViewState } from '@/constants/screens'
 
-import { extensionManager } from '@/extension'
 import { mainViewStateAtom } from '@/helpers/atoms/App.atom'
 import { selectedSettingAtom } from '@/helpers/atoms/Setting.atom'
 
@@ -21,55 +20,8 @@ const SetupRemoteModel = ({ engine, isConfigured }: Props) => {
   const setSelectedSetting = useSetAtom(selectedSettingAtom)
   const setMainViewState = useSetAtom(mainViewStateAtom)
 
-  const [extensionHasSettings, setExtensionHasSettings] = useState<
-    { name?: string; setting: string; apiKey: string; provider: string }[]
-  >([])
-
-  useEffect(() => {
-    const getAllSettings = async () => {
-      const extensionsMenu: {
-        name?: string
-        setting: string
-        apiKey: string
-        provider: string
-      }[] = []
-      const extensions = extensionManager.getAll()
-
-      for (const extension of extensions) {
-        if (typeof extension.getSettings === 'function') {
-          const settings = await extension.getSettings()
-
-          if (
-            (settings && settings.length > 0) ||
-            (await extension.installationState()) !== 'NotRequired'
-          ) {
-            extensionsMenu.push({
-              name: extension.productName,
-              setting: extension.name,
-              apiKey:
-                'apiKey' in extension && typeof extension.apiKey === 'string'
-                  ? extension.apiKey
-                  : '',
-              provider:
-                'provider' in extension &&
-                typeof extension.provider === 'string'
-                  ? extension.provider
-                  : '',
-            })
-          }
-        }
-      }
-      setExtensionHasSettings(extensionsMenu)
-    }
-    getAllSettings()
-  }, [])
-
   const onSetupItemClick = (setting: InferenceEngine) => {
-    setSelectedSetting(
-      extensionHasSettings.filter((x) =>
-        x.provider.toLowerCase().includes(setting)
-      )[0]?.setting
-    )
+    setSelectedSetting(setting)
     setMainViewState(MainViewState.Settings)
   }
 
