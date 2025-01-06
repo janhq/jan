20a461fe5d1bb232e0796af8ddcecb621418812d
Louis <louis@jan.ai>
2025-01-06 21:28:54 +0700

    test: update test cases

--- core/src/node/helper/resource.ts
+++ core/src/node/helper/resource.ts
@@ -2,7 +2,6 @@ import { SystemResourceInfo } from '../../types'
 
 export const getSystemResourceInfo = async (): Promise<SystemResourceInfo> => {
   return {
-    numCpuPhysicalCore: 0,
     memAvailable: 0, // TODO: this should not be 0
   }
 }
