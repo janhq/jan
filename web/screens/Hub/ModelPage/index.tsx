d032d2f43bab5e60e03c77f0ccbbcc25eaaac087
Louis <louis@jan.ai>
2025-01-21 09:29:01 +0700

    chore: page transition animation

--- web/screens/Hub/ModelPage/index.tsx
+++ web/screens/Hub/ModelPage/index.tsx
@@ -1,5 +1,5 @@
 import { ModelSource } from '@janhq/core'
-import { Badge } from '@janhq/joi'
+import { Badge, ScrollArea } from '@janhq/joi'
 import { ArrowLeftIcon } from 'lucide-react'
 
 import MarkdownText from '@/containers/Markdown'
@@ -15,100 +15,102 @@ type Props = {
 
 const ModelPage = ({ model, onGoBack }: Props) => {
   return (
-    <div className="flex h-full w-full justify-center">
-      <div className="flex w-full max-w-[800px] flex-col ">
-        <div className="top-0 flex h-12 items-center bg-[hsla(var(--app-bg))] px-4">
-          <div className="flex items-center gap-2">
-            <button
-              onClick={onGoBack}
-              className="flex items-center gap-1 text-sm text-[hsla(var(--text-secondary))] hover:text-[hsla(var(--text-primary))]"
-            >
-              <ArrowLeftIcon size={16} />
-              <span>Back</span>
-            </button>
-          </div>
-        </div>
-        <div className="p-4">
-          {/* Header */}
-          <div className="flex items-center justify-between py-2">
-            <span className="line-clamp-1 text-base font-medium capitalize group-hover:text-blue-500 group-hover:underline">
-              {extractModelName(model.metadata.modelId)}
-            </span>
-            <div className="inline-flex items-center space-x-2">
-              <ModelDownloadButton id={model.models?.[0].id} />
+    <ScrollArea data-testid="hub-container-test-id" className="h-full w-full">
+      <div className="flex h-full w-full justify-center">
+        <div className="flex w-full max-w-[800px] flex-col ">
+          <div className="sticky top-0 flex h-12 items-center bg-[hsla(var(--app-bg))] px-4">
+            <div className="flex items-center gap-2">
+              <button
+                onClick={onGoBack}
+                className="flex items-center gap-1 text-sm text-[hsla(var(--text-secondary))] hover:text-[hsla(var(--text-primary))]"
+              >
+                <ArrowLeftIcon size={16} />
+                <span>Back</span>
+              </button>
             </div>
           </div>
-          <div className="flex flex-col gap-y-4 sm:flex-row sm:gap-x-10 sm:gap-y-0">
-            <p
-              className="font-regular mt-2 line-clamp-1 text-[hsla(var(--text-secondary))]"
-              title={model.metadata?.author}
-            >
-              {model.metadata?.author}
-            </p>
-          </div>
-          {/* Table of versions */}
-          <div className="mt-8 flex w-full flex-col items-start justify-between sm:flex-row">
-            <div className="w-full flex-shrink-0 rounded-lg border border-[hsla(var(--app-border))] text-[hsla(var(--text-secondary))]">
-              <table className="w-full p-4">
-                <thead className="bg-[hsla(var(--tertiary-bg))]">
-                  <tr>
-                    <th className="flex-1 px-6 py-3 text-left text-sm font-semibold">
-                      Version
-                    </th>
-                    <th className="w-32 px-6 py-3 text-left text-sm font-semibold">
-                      Format
-                    </th>
-                    <th className="w-32 px-6 py-3 text-left text-sm font-semibold">
-                      Size
-                    </th>
-                    <th className="w-[120px]"></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {model.models?.map((item, i) => {
-                    return (
-                      <tr
-                        key={item.id}
-                        className="border-t border-[hsla(var(--app-border))] font-medium text-[hsla(var(--text-secondary))]"
-                      >
-                        <td className="flex items-center space-x-4 px-6 py-4 text-black">
-                          <span className="line-clamp-1 max-w-[200px]">
-                            {item.id}
-                          </span>
-                          {i === 0 && (
-                            <Badge
-                              theme="secondary"
-                              className="inline-flex w-[60px] items-center font-medium"
-                            >
-                              <span>Default</span>
-                            </Badge>
-                          )}
-                        </td>
-                        <td className="px-6 py-4">GGUF</td>
-                        <td className="px-6 py-4 text-[hsla(var(--text-secondary))]">
-                          {toGigabytes(item.size)}
-                        </td>
-                        <td className="pr-4 text-right text-black">
-                          <ModelDownloadButton
-                            id={item.id}
-                            theme={i === 0 ? 'primary' : 'ghost'}
-                            variant={i === 0 ? 'solid' : 'outline'}
-                          />
-                        </td>
-                      </tr>
-                    )
-                  })}
-                </tbody>
-              </table>
+          <div className="p-4">
+            {/* Header */}
+            <div className="flex items-center justify-between py-2">
+              <span className="line-clamp-1 text-base font-medium capitalize group-hover:text-blue-500 group-hover:underline">
+                {extractModelName(model.metadata.modelId)}
+              </span>
+              <div className="inline-flex items-center space-x-2">
+                <ModelDownloadButton id={model.models?.[0].id} />
+              </div>
             </div>
-          </div>
-          {/* README */}
-          <div className="mt-8 flex w-full flex-col items-start justify-between sm:flex-row">
-            <MarkdownText text={model.metadata?.description ?? ''} />
+            <div className="flex flex-col gap-y-4 sm:flex-row sm:gap-x-10 sm:gap-y-0">
+              <p
+                className="font-regular mt-2 line-clamp-1 text-[hsla(var(--text-secondary))]"
+                title={model.metadata?.author}
+              >
+                {model.metadata?.author}
+              </p>
+            </div>
+            {/* Table of versions */}
+            <div className="mt-8 flex w-full flex-col items-start justify-between sm:flex-row">
+              <div className="w-full flex-shrink-0 rounded-lg border border-[hsla(var(--app-border))] text-[hsla(var(--text-secondary))]">
+                <table className="w-full p-4">
+                  <thead className="bg-[hsla(var(--tertiary-bg))]">
+                    <tr>
+                      <th className="flex-1 px-6 py-3 text-left text-sm font-semibold">
+                        Version
+                      </th>
+                      <th className="w-32 px-6 py-3 text-left text-sm font-semibold">
+                        Format
+                      </th>
+                      <th className="w-32 px-6 py-3 text-left text-sm font-semibold">
+                        Size
+                      </th>
+                      <th className="w-[120px]"></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {model.models?.map((item, i) => {
+                      return (
+                        <tr
+                          key={item.id}
+                          className="border-t border-[hsla(var(--app-border))] font-medium text-[hsla(var(--text-secondary))]"
+                        >
+                          <td className="flex items-center space-x-4 px-6 py-4 text-black">
+                            <span className="line-clamp-1 max-w-[200px]">
+                              {item.id}
+                            </span>
+                            {i === 0 && (
+                              <Badge
+                                theme="secondary"
+                                className="inline-flex w-[60px] items-center font-medium"
+                              >
+                                <span>Default</span>
+                              </Badge>
+                            )}
+                          </td>
+                          <td className="px-6 py-4">GGUF</td>
+                          <td className="px-6 py-4 text-[hsla(var(--text-secondary))]">
+                            {toGigabytes(item.size)}
+                          </td>
+                          <td className="pr-4 text-right text-black">
+                            <ModelDownloadButton
+                              id={item.id}
+                              theme={i === 0 ? 'primary' : 'ghost'}
+                              variant={i === 0 ? 'solid' : 'outline'}
+                            />
+                          </td>
+                        </tr>
+                      )
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            {/* README */}
+            <div className="mt-8 flex w-full flex-col items-start justify-between sm:flex-row">
+              <MarkdownText text={model.metadata?.description ?? ''} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </ScrollArea>
   )
 }
 
--- web/screens/Hub/index.tsx
+++ web/screens/Hub/index.tsx
@@ -2,7 +2,7 @@ import { useCallback, useState } from 'react'
 
 import Image from 'next/image'
 
-import { Model } from '@janhq/core'
+import { ModelSource } from '@janhq/core'
 
 import { ScrollArea, Button, Select } from '@janhq/joi'
 
@@ -20,6 +20,8 @@ import { setImportModelStageAtom } from '@/hooks/useImportModel'
 import ModelList from '@/screens/Hub/ModelList'
 
 import ModelPage from './ModelPage'
+
+import { motion as m } from 'framer-motion'
 
 import {
   configuredModelsAtom,
@@ -57,7 +59,7 @@ const HubScreen = () => {
   const [searchValue, setsearchValue] = useState('')
   const [sortSelected, setSortSelected] = useState('newest')
   const [filterOption, setFilterOption] = useState('all')
-  const [selectedModel, setSelectedModel] = useState<Model | undefined>(
+  const [selectedModel, setSelectedModel] = useState<ModelSource | undefined>(
     undefined
   )
 
@@ -89,82 +91,98 @@ const HubScreen = () => {
 
   return (
     <CenterPanelContainer>
-      <ScrollArea data-testid="hub-container-test-id" className="h-full w-full">
+      <m.div
+        key={selectedModel?.id}
+        initial={{ opacity: 0, y: -8 }}
+        className="h-full"
+        animate={{
+          opacity: 1,
+          y: 0,
+          transition: {
+            duration: 0.25,
+          },
+        }}
+      >
         {!selectedModel && (
-          <>
-            <div className="relative h-40 p-4 sm:h-auto">
-              <Image
-                src="./images/banner.jpg"
-                alt="Hub Banner"
-                width={800}
-                height={800}
-                className="h-full w-full rounded-lg object-cover"
-              />
-              <div className="absolute left-1/2 top-1/2 mx-auto w-4/5 -translate-x-1/2 -translate-y-1/2 rounded-xl sm:w-2/6">
-                <div className="flex flex-col items-center justify-between gap-2 sm:flex-row">
-                  <div className="w-full">
-                    <ModelSearch onSearchLocal={onSearchUpdate} />
+          <ScrollArea
+            data-testid="hub-container-test-id"
+            className="h-full w-full"
+          >
+            <>
+              <div className="relative h-40 p-4 sm:h-auto">
+                <Image
+                  src="./images/banner.jpg"
+                  alt="Hub Banner"
+                  width={800}
+                  height={800}
+                  className="h-full w-full rounded-lg object-cover"
+                />
+                <div className="absolute left-1/2 top-1/2 mx-auto w-4/5 -translate-x-1/2 -translate-y-1/2 rounded-xl sm:w-2/6">
+                  <div className="flex flex-col items-center justify-between gap-2 sm:flex-row">
+                    <div className="w-full">
+                      <ModelSearch onSearchLocal={onSearchUpdate} />
+                    </div>
                   </div>
                 </div>
+                <div className="absolute right-8 top-8 flex-shrink-0 rounded-md bg-[hsla(var(--app-bg))]">
+                  <Button
+                    onClick={onImportModelClick}
+                    variant="solid"
+                    theme="ghost"
+                  >
+                    <UploadIcon size={16} className="mr-2" />
+                    <span>Import</span>
+                  </Button>
+                </div>
               </div>
-              <div className="absolute right-8 top-8 flex-shrink-0 rounded-md bg-[hsla(var(--app-bg))]">
-                <Button
-                  onClick={onImportModelClick}
-                  variant="solid"
-                  theme="ghost"
-                >
-                  <UploadIcon size={16} className="mr-2" />
-                  <span>Import</span>
-                </Button>
-              </div>
-            </div>
-            <div className="mt-8 p-4 py-0 sm:px-16">
-              {!filteredModels.length ? (
-                <BlankState title="No search results found" />
-              ) : (
-                <>
-                  <div className="flex flex-row">
-                    <div className="flex w-full flex-col items-start justify-between gap-4 py-4 first:pt-0 sm:flex-row">
-                      <div className="flex items-center gap-x-2">
-                        {filterOptions.map((e) => (
-                          <div
-                            key={e.value}
-                            className={twMerge(
-                              'rounded-md border duration-200 hover:border-gray-200 hover:bg-gray-200',
-                              e.value === filterOption
-                                ? 'border-gray-200 bg-gray-200'
-                                : 'border-[hsla(var(--app-border))] bg-[hsla(var(--app-bg))]'
-                            )}
-                          >
-                            <Button
-                              theme={'ghost'}
-                              variant={'soft'}
-                              onClick={() => setFilterOption(e.value)}
+              <div className="mt-8 p-4 py-0 sm:px-16">
+                {!filteredModels.length ? (
+                  <BlankState title="No search results found" />
+                ) : (
+                  <>
+                    <div className="flex flex-row">
+                      <div className="flex w-full flex-col items-start justify-between gap-4 py-4 first:pt-0 sm:flex-row">
+                        <div className="flex items-center gap-x-2">
+                          {filterOptions.map((e) => (
+                            <div
+                              key={e.value}
+                              className={twMerge(
+                                'rounded-md border duration-200 hover:border-gray-200 hover:bg-gray-200',
+                                e.value === filterOption
+                                  ? 'border-gray-200 bg-gray-200'
+                                  : 'border-[hsla(var(--app-border))] bg-[hsla(var(--app-bg))]'
+                              )}
                             >
-                              {e.name}
-                            </Button>
-                          </div>
-                        ))}
+                              <Button
+                                theme={'ghost'}
+                                variant={'soft'}
+                                onClick={() => setFilterOption(e.value)}
+                              >
+                                {e.name}
+                              </Button>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                      <div className="mb-4 flex w-full justify-end">
+                        <Select
+                          value={sortSelected}
+                          onValueChange={(value) => {
+                            setSortSelected(value)
+                          }}
+                          options={sortMenus}
+                        />
                       </div>
                     </div>
-                    <div className="mb-4 flex w-full justify-end">
-                      <Select
-                        value={sortSelected}
-                        onValueChange={(value) => {
-                          setSortSelected(value)
-                        }}
-                        options={sortMenus}
-                      />
-                    </div>
-                  </div>
-                  <ModelList
-                    models={filteredModels}
-                    onSelectedModel={(model) => setSelectedModel(model)}
-                  />
-                </>
-              )}
-            </div>
-          </>
+                    <ModelList
+                      models={filteredModels}
+                      onSelectedModel={(model) => setSelectedModel(model)}
+                    />
+                  </>
+                )}
+              </div>
+            </>
+          </ScrollArea>
         )}
         {selectedModel && (
           <ModelPage
@@ -172,7 +190,7 @@ const HubScreen = () => {
             onGoBack={() => setSelectedModel(undefined)}
           />
         )}
-      </ScrollArea>
+      </m.div>
     </CenterPanelContainer>
   )
 }
