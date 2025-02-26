36a14600ee6244abc1c75597c3ba4f3893fcbadf
Faisal Amir <urmauur@gmail.com>
2025-02-26 19:06:46 +0700

    fix: hub ui no result search found (#4739)

--- web/screens/Hub/ModelList/index.tsx
+++ web/screens/Hub/ModelList/index.tsx
@@ -5,18 +5,29 @@ import ModelItem from '@/screens/Hub/ModelList/ModelItem'
 type Props = {
   models: ModelSource[]
   onSelectedModel: (model: ModelSource) => void
+  filterOption?: string
 }
 
-const ModelList = ({ models, onSelectedModel }: Props) => {
+const ModelList = ({ models, onSelectedModel, filterOption }: Props) => {
   return (
     <div className="w-full">
-      {models.map((model) => (
-        <ModelItem
-          key={model.id}
-          model={model}
-          onSelectedModel={() => onSelectedModel(model)}
-        />
-      ))}
+      {models.length === 0 && filterOption === 'on-device' ? (
+        <div className="my-4 p-2 text-center">
+          <span className="text-[hsla(var(--text-tertiary))]">
+            No results found
+          </span>
+        </div>
+      ) : (
+        <>
+          {models.map((model) => (
+            <ModelItem
+              key={model.id}
+              model={model}
+              onSelectedModel={() => onSelectedModel(model)}
+            />
+          ))}
+        </>
+      )}
     </div>
   )
 }
