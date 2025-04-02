8b1709c14f66ba538ad4edee8b172b82cd66e621
Louis <louis@jan.ai>
2025-04-02 10:32:42 +0700

    refactor: clean up assistant extension to remove node modules

--- extensions/assistant-extension/rolldown.config.mjs
+++ extensions/assistant-extension/rolldown.config.mjs
@@ -13,22 +13,5 @@ export default defineConfig([
       NODE: JSON.stringify(`${pkgJson.name}/${pkgJson.node}`),
       VERSION: JSON.stringify(pkgJson.version),
     },
-  },
-  {
-    input: 'src/node/index.ts',
-    external: ['@janhq/core/node', 'path', 'hnswlib-node'],
-    output: {
-      format: 'cjs',
-      file: 'dist/node/index.js',
-      sourcemap: false,
-      inlineDynamicImports: true,
-    },
-    resolve: {
-      extensions: ['.js', '.ts'],
-    },
-    define: {
-      CORTEX_API_URL: JSON.stringify(`http://127.0.0.1:${process.env.CORTEX_API_PORT ?? "39291"}`),
-    },
-    platform: 'node',
-  },
+  }
 ])
