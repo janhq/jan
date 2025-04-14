d60967b4f7b94b8aac616dc055c4d08546633756
Faisal Amir <urmauur@gmail.com>
2025-04-14 15:26:13 +0700

    chore: initial commit mcp setting

---
+++ web/screens/Settings/MCP/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MCP = () => {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, cumque
+      deleniti dolorem ducimus nisi rerum cum et sunt maxime, dicta sequi
+      assumenda sit illum? Minima beatae repudiandae praesentium sed incidunt!
+    </p>
+  )
+}
+
+export default MCP
