defd6d57e794587a8f067bcf1547f8682133138b
Thien Tran <gau.nernst@yahoo.com.sg>
2025-04-21 22:59:55 +0800

    add list HF files

--- src-tauri/src/core/download.rs
+++ src-tauri/src/core/download.rs
@@ -3,22 +3,13 @@ use std::io::Write;
 use std::path::Path;
 use std::time::Duration;
 
-pub async fn download_hf_file(
-    repo_id: &str,
-    file_path: &str,
-    save_path: &Path,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let url = format!(
-        "https://huggingface.co/{}/resolve/main/{}",
-        repo_id, file_path
-    );
-
+pub async fn download_file(url: &str, save_path: &Path) -> Result<(), Box<dyn std::error::Error>> {
     let client = reqwest::Client::builder()
         .http2_keep_alive_timeout(Duration::from_secs(15))
         .build()?;
 
     let mut resp = client
-        .get(&url)
+        .get(url)
         .header("User-Agent", "rust-reqwest/huggingface-downloader")
         .send()
         .await?;
@@ -47,13 +38,69 @@ pub async fn download_hf_file(
     Ok(())
 }
 
+pub async fn download_hf_file(
+    repo_id: &str,
+    branch: &str,
+    file_path: &str,
+    save_path: &Path,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let url = format!(
+        "https://huggingface.co/{}/resolve/{}/{}",
+        repo_id, branch, file_path
+    );
+    download_file(&url, save_path).await
+}
+
+pub async fn list_hf_files(
+    repo_id: &str,
+    branch: &str,
+) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let mut files = vec![];
+    let client = reqwest::Client::new();
+
+    // do recursion with a stack
+    let mut stack = vec!["".to_string()];
+    while let Some(directory) = stack.pop() {
+        let url = format!(
+            "https://huggingface.co/api/models/{}/tree/{}/{}",
+            repo_id, branch, directory
+        );
+        let resp = client.get(&url).send().await?;
+
+        if !resp.status().is_success() {
+            return Err(format!(
+                "Failed to list files: HTTP status {}, {}",
+                resp.status(),
+                resp.text().await?
+            )
+            .into());
+        }
+
+        #[derive(serde::Deserialize)]
+        struct Item {
+            r#type: String,
+            path: String,
+        }
+        for item in resp.json::<Vec<Item>>().await?.into_iter() {
+            match item.r#type.as_str() {
+                "file" => files.push(item.path),
+                "directory" => stack.push(item.path),
+                _ => {}
+            }
+        }
+    }
+
+    Ok(files)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[tokio::test]
     async fn test_download_hf_file() {
-        let repo_id = "gpt2";
+        let repo_id = "openai-community/gpt2";
+        let branch = "main";
         let file_path = "config.json";
         let save_path = std::path::PathBuf::from("subdir/test_config.json");
 
@@ -63,7 +110,7 @@ mod tests {
             }
         }
 
-        let result = download_hf_file(repo_id, file_path, &save_path).await;
+        let result = download_hf_file(repo_id, branch, file_path, &save_path).await;
         assert!(result.is_ok(), "{}", result.unwrap_err());
         assert!(save_path.exists());
 
@@ -87,5 +134,19 @@ mod tests {
         // NOTE: this will not run if there are errors
         // TODO: use tempfile crate instead
         std::fs::remove_dir_all(save_path.parent().unwrap()).unwrap();
+    }
+
+    #[tokio::test]
+    async fn test_list_hf_files() {
+        let repo_id = "openai-community/gpt2";
+        let branch = "main";
+
+        let result = list_hf_files(repo_id, branch).await;
+        assert!(result.is_ok(), "{}", result.unwrap_err());
+        let files = result.unwrap();
+        assert!(
+            files.iter().any(|f| f == "config.json"),
+            "config.json should be in the list"
+        );
     }
 }
