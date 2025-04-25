8f48dec2e3a506b20b30d85b80af9d87269ea798
Thien Tran <gau.nernst@yahoo.com.sg>
2025-04-25 16:00:36 +0800

    use Ollama endpoint

--- extensions/ollama-extension/src/index.ts
+++ extensions/ollama-extension/src/index.ts
@@ -1,22 +1,31 @@
 import {
   InferenceEngine,
-  LocalOAIEngine,
+  RemoteOAIEngine,
+  PayloadType,
 } from '@janhq/core'
-import ollama from 'ollama/browser'
+import { Ollama } from 'ollama/browser'
 
-export default class OllamaExtension extends LocalOAIEngine {
+export default class OllamaExtension extends RemoteOAIEngine {
   nodeModule: string = 'node'
   inferenceUrl: string = ''
   provider: string = 'ollama'
+
+  // ollama client
+  // NOTE: do we need Ollama client? we can just call the endpoints ourselves
+  ollama: Ollama
 
   async onLoad() {
     super.onLoad()
     this.registerSettings(SETTINGS)
 
+    let port = await this.getSetting<number>('port', DEFAULT_PORT)
+    this.updatePort(port)
+
     // TODO: should we have a prefix for model name, in case different
     // providers provide the same model? but when sending the request,
     // we should strip the prefix
-    let models_resp = await ollama.list()
+    // TODO: handle when server is not available
+    let models_resp = await this.ollama.list()
     this.registerModels(models_resp.models.map(obj => {
       return {
         sources: [],
@@ -38,9 +47,6 @@ export default class OllamaExtension extends LocalOAIEngine {
         engine: InferenceEngine.ollama,
       }
     }))
-
-    let port = await this.getSetting<number>('port', DEFAULT_PORT)
-    this.updatePort(port)
   }
 
   onSettingUpdate<T>(key: string, value: T): void {
@@ -49,6 +55,23 @@ export default class OllamaExtension extends LocalOAIEngine {
   }
 
   private updatePort(port: number) {
-    this.inferenceUrl = `http://127.0.0.1:${port}/v1/chat/completions`
+    // should we just use ollama client for making requests?
+    // this.inferenceUrl = `http://127.0.0.1:${port}/v1/chat/completions`
+    this.inferenceUrl = `http://127.0.0.1:${port}/api/chat`
+    this.ollama = new Ollama({ host: `http://127.0.0.1:${port}` })
+  }
+
+  // // transform OpenAI payload to Ollama payload
+  // transformPayload = (payload: PayloadType): PayloadType => {
+  //   // Jan sets messages, model, stream, tools, which are supported by Ollama API
+  //   // TODO: add Ollama-specific params e.g. num_ctx, keep_alive
+  //   return payload
+  // }
+
+  // extract message from Ollama response
+  // TODO: do we need to handle non-streaming response?
+  transformResponse = (response: string): string => {
+    const data = JSON.parse(response)
+    return data.message.content
   }
 }
