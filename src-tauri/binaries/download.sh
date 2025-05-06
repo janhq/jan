006bd43112840f6b6c370bbb86b2e7f0016bbfca
vansangpfiev <vansangpfiev@gmail.com>
2025-05-06 17:35:01 +0700

    chore: bump cortex to 1.0.13-rc6

--- src-tauri/binaries/download.sh
+++ src-tauri/binaries/download.sh
@@ -14,7 +14,7 @@ download() {
 }
 
 # Read CORTEX_VERSION
-CORTEX_VERSION=1.0.13-rc5
+CORTEX_VERSION=1.0.13-rc6
 ENGINE_VERSION=b5215
 CORTEX_RELEASE_URL="https://github.com/menloresearch/cortex.cpp/releases/download"
 ENGINE_DOWNLOAD_URL=https://github.com/menloresearch/llama.cpp/releases/download/${ENGINE_VERSION}/llama-${ENGINE_VERSION}-bin
