44f9ff4ce9ba6941440d90e1bb162ecb678e0150
Louis <louis@jan.ai>
2025-05-09 14:31:12 +0700

    chore: clean up threads

--- web-app/src/helpers/threads.ts
+++ web-app/src/helpers/threads.ts
@@ -1,3 +1,5 @@
+import { models, StreamCompletionResponse, TokenJS } from 'token.js'
+
 /**
  * @fileoverview Helper functions for creating thread content.
  * These functions are used to create thread content objects
@@ -13,7 +15,6 @@ export const newUserThreadContent = (content: string): ThreadContent => ({
     value: content,
     annotations: [],
   },
-  
 })
 /**
  * @fileoverview Helper functions for creating thread content.
@@ -31,3 +32,41 @@ export const newAssistantThreadContent = (content: string): ThreadContent => ({
     annotations: [],
   },
 })
+
+/**
+ * @fileoverview Helper function to send a completion request to the model provider.
+ * @param thread
+ * @param provider
+ * @param prompt
+ * @returns
+ */
+export const sendCompletion = async (
+  thread: Thread,
+  provider: ModelProvider,
+  prompt: string
+): Promise<StreamCompletionResponse | undefined> => {
+  if (!thread?.model?.id || !provider || !provider.api_key) return undefined
+
+  let providerName = provider.provider as unknown as keyof typeof models
+
+  if (!Object.keys(models).some((key) => key === providerName))
+    providerName = 'openai-compatible'
+
+  const tokenJS = new TokenJS({
+    apiKey: provider.api_key,
+    baseURL: provider.base_url,
+  })
+
+  const completion = await tokenJS.chat.completions.create({
+    stream: true,
+    provider: providerName,
+    model: thread.model?.id,
+    messages: [
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ],
+  })
+  return completion
+}
