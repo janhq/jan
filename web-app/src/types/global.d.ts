9326c099bc9b8fb3766fd3bcbd0e7220b727d3c8
Louis <louis@jan.ai>
2025-05-11 16:17:39 +0700

    chore: add platform utils

--- web-app/src/types/global.d.ts
+++ web-app/src/types/global.d.ts
@@ -11,4 +11,12 @@ declare global {
   interface Window {
     core: AppCore | undefined
   }
+
+  let IS_TAURI: boolean
+  let IS_MACOS: boolean
+  let IS_WINDOWS: boolean
+  let IS_LINUX: boolean
+  let IS_IOS: boolean
+  let IS_ANDROID: boolean
+  let PLATFORM: string
 }
