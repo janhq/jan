4d69d920c7aa35d9c1b75847b120e67e43a913ca
vansangpfiev <vansangpfiev@gmail.com>
2025-05-16 14:45:01 +0700

    chore: add some ts-ignore to make build works

--- web-app/src/hooks/useGeneralSetting.ts
+++ web-app/src/hooks/useGeneralSetting.ts
@@ -3,9 +3,11 @@ import { persist, createJSONStorage } from 'zustand/middleware'
 import { localStoregeKey } from '@/constants/localStorage'
 
 type LeftPanelStoreState = {
+  // @ts-ignore
   currentLanguage: Language
   spellCheckChatInput: boolean
   setSpellCheckChatInput: (value: boolean) => void
+  // @ts-ignore
   setCurrentLanguage: (value: Language) => void
 }
 
