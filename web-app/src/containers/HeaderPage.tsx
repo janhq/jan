d8ca1afda4fb4054d6454bcf97ae6346ed58659e
Faisal Amir <urmauur@gmail.com>
2025-05-28 13:22:11 +0700

    chore: add skip button joyride

--- web-app/src/containers/HeaderPage.tsx
+++ web-app/src/containers/HeaderPage.tsx
@@ -2,20 +2,19 @@ import { useLeftPanel } from '@/hooks/useLeftPanel'
 import { cn } from '@/lib/utils'
 import { IconLayoutSidebar } from '@tabler/icons-react'
 import { ReactNode } from '@tanstack/react-router'
-import { platform } from '@tauri-apps/plugin-os'
 
 type HeaderPageProps = {
-  children: ReactNode
+  children?: ReactNode
 }
 const HeaderPage = ({ children }: HeaderPageProps) => {
-  const platformName = platform()
   const { open, setLeftPanel } = useLeftPanel()
 
   return (
     <div
       className={cn(
-        'h-10 border-b border-main-view-fg/5 pl-18 text-main-view-fg flex items-center shrink-0',
-        platformName === 'macos' && !open ? 'pl-18' : 'pl-4'
+        'h-10 pl-18 text-main-view-fg flex items-center shrink-0 border-b border-main-view-fg/5',
+        IS_MACOS && !open ? 'pl-18' : 'pl-4',
+        children === undefined && 'border-none'
       )}
     >
       <div className="flex items-center w-full gap-2">
