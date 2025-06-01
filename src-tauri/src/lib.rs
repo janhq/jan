b1443e035e4cfecff18dc69db228fd0acf1bd861
Faisal Amir <urmauur@gmail.com>
2025-06-02 00:44:24 +0700

    enhancement: ux change data folder with confirmation and reveal in finder logs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -17,6 +17,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_updater::Builder::new().build())
