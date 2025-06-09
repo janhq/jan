40ad29bbefca5545babbc9c873edbec1130e4d20
Faisal Amir <urmauur@gmail.com>
2025-06-09 13:53:33 +0700

    chore: fix default color window

--- web-app/src/hooks/useAppearance.ts
+++ web-app/src/hooks/useAppearance.ts
@@ -41,9 +41,9 @@ export const fontSizeOptions = [
 // Default appearance settings
 const defaultFontSize: FontSize = '15px'
 const defaultAppBgColor: RgbaColor = {
-  r: 0,
-  g: 0,
-  b: 0,
+  r: 25,
+  g: 25,
+  b: 25,
   a: IS_WINDOWS || IS_LINUX || !IS_TAURI ? 1 : 0.4,
 }
 const defaultLightAppBgColor: RgbaColor = {
@@ -54,9 +54,9 @@ const defaultLightAppBgColor: RgbaColor = {
 }
 const defaultAppMainViewBgColor: RgbaColor = { r: 25, g: 25, b: 25, a: 1 }
 const defaultLightAppMainViewBgColor: RgbaColor = {
-  r: 250,
-  g: 250,
-  b: 250,
+  r: 255,
+  g: 255,
+  b: 255,
   a: 1,
 }
 
