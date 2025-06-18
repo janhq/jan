0e220ef319e9025e403aa4c5e1d4d73fe133c51d
Faisal Amir <urmauur@gmail.com>
2025-06-18 21:16:53 +0700

    ✨enhancement: reset heading class

--- web-app/src/routes/hub.tsx
+++ web-app/src/routes/hub.tsx
@@ -495,7 +495,7 @@ function Hub() {
                         <div className="line-clamp-2 mt-3 text-main-view-fg/60">
                           <RenderMarkdown
                             enableRawHtml={true}
-                            className="select-none"
+                            className="select-none reset-heading"
                             components={{
                               a: ({ ...props }) => (
                                 <a
