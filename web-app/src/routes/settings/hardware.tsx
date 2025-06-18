8a4d12ab916f22938f5e74d6a5aad143bf1316fd
Faisal Amir <urmauur@gmail.com>
2025-06-18 11:26:01 +0700

    ✨enhancement: update copy

--- web-app/src/routes/settings/hardware.tsx
+++ web-app/src/routes/settings/hardware.tsx
@@ -352,8 +352,7 @@ function Hardware() {
               <Card title="Vulkan">
                 <CardItem
                   title="Enable Vulkan"
-                  align="start"
-                  description="Use Vulkan API for GPU acceleration to support a wider range of GPUs (AMD, Intel Arc, etc.). Vulkan may have performance degradation compared to native CUDA support. Do not enable Vulkan if you have an NVIDIA GPU as it may cause compatibility issues."
+                  description="Use Vulkan API for GPU acceleration. Do not enable Vulkan if you have an NVIDIA GPU as it may cause compatibility issues."
                   actions={
                     <div className="flex items-center gap-4">
                       <Switch
