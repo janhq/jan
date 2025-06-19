edaddca2590d8ac63f31067fc7d3ae336822d4cc
Shubham Keshari <shubhamkeshari@static-100-0-247-135.bstnma.fios.verizon.net>
2025-06-19 19:19:09 +0530

    added exception handling in hash_function

--- .github/scripts/electron-checksum.py
+++ .github/scripts/electron-checksum.py
@@ -7,12 +7,23 @@ def hash_file(file_path):
     sha512 = hashlib.sha512()
 
     # Read and update the hash object with the content of the file
-    with open(file_path, 'rb') as f:
-        while True:
-            data = f.read(1024 * 1024)  # Read in 1 MB chunks
-            if not data:
-                break
-            sha512.update(data)
+   try:
+        # Read and update the hash object with the content of the file
+        with open(file_path, 'rb') as f:
+            while True:
+                data = f.read(1024 * 1024)  # Read in 1 MB chunks
+                if not data:
+                    break
+                sha512.update(data)
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+        sys.exit(1)
+    except PermissionError:
+        print(f"Error: Permission denied when accessing '{file_path}'.")
+        sys.exit(1)
+    except IOError as e:
+        print(f"IO error while reading '{file_path}': {e}")
+        sys.exit(1)
 
     # Obtain the hash result and encode it in base64
     hash_base64 = base64.b64encode(sha512.digest()).decode('utf-8')
