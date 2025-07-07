5ce9bbb304a30838d3daadfe8dee339a124cf6f0
Louis <louis@jan.ai>
2025-07-07 18:56:04 +0700

    fix: linux build without cortex binaries

--- .devcontainer/buildAppImage.sh
+++ .devcontainer/buildAppImage.sh
@@ -14,10 +14,7 @@ mv /tmp/tauri.conf.json ./src-tauri/tauri.conf.json
 make build-tauri
 
 cp ./src-tauri/resources/bin/bun ./src-tauri/target/release/bundle/appimage/Jan.AppDir/usr/bin/bun
-mkdir -p ./src-tauri/target/release/bundle/appimage/Jan.AppDir/usr/lib/Jan/binaries/engines
-cp -f ./src-tauri/binaries/deps/*.so* ./src-tauri/target/release/bundle/appimage/Jan.AppDir/usr/lib/Jan/binaries/
 cp -f ./src-tauri/binaries/*.so* ./src-tauri/target/release/bundle/appimage/Jan.AppDir/usr/lib/Jan/binaries/
-cp -rf ./src-tauri/binaries/engines ./src-tauri/target/release/bundle/appimage/Jan.AppDir/usr/lib/Jan/binaries/
 APP_IMAGE=./src-tauri/target/release/bundle/appimage/$(ls ./src-tauri/target/release/bundle/appimage/ | grep AppImage | head -1)
 echo $APP_IMAGE
 rm -f $APP_IMAGE
