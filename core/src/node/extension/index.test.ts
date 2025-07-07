3287e8b3000e457b1288243af2f23bb533fade67
hiento09 <136591877+hiento09@users.noreply.github.com>
2025-07-07 11:24:13 +0700

    chore: enable test coverage (#5710)
    * chore: enable test coverage

--- core/src/node/extension/index.test.ts
+++ core/src/node/extension/index.test.ts
@@ -1,7 +1,7 @@
+import { useExtensions } from './index'
 
-
-    import { useExtensions } from './index'
-    
-    test('testUseExtensionsMissingPath', () => {
-      expect(() => useExtensions(undefined as any)).toThrowError('A path to the extensions folder is required to use extensions')
-    })
+test('testUseExtensionsMissingPath', () => {
+  expect(() => useExtensions(undefined as any)).toThrow(
+    'A path to the extensions folder is required to use extensions'
+  )
+})
