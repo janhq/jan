28a51052c42fe026d923d81e1e1efe8eb8387c0d
Akarshan <akarshan@menlo.ai>
2025-07-22 11:37:33 +0530

    Add debug info about lib_path

--- src-tauri/src/core/utils/extensions/inference_llamacpp_extension/server.rs
+++ src-tauri/src/core/utils/extensions/inference_llamacpp_extension/server.rs
@@ -133,7 +133,8 @@ pub async fn load_llama_model(
                 Ok(path) => format!("{};{}", path, lib_path),
                 Err(_) => lib_path.to_string(),
             };
-            command.env("PATH", &new_path);
+            command.env("PATH", new_path);
+            log::info!("Library path:\n{}", &lib_path);
             command.current_dir(lib_path);
         } else {
             log::warn!("Library path setting is not supported on this OS");
