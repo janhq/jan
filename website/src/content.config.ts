aa117cf9170643d4701234e06118dea04f4b6e57
Ramon Perez <ramonpzg@protonmail.com>
2025-07-29 01:24:17 +1000

    added products section, and reorganized and improved the docs

--- website/src/content.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineCollection } from 'astro:content';
-import { docsLoader } from '@astrojs/starlight/loaders';
-import { docsSchema } from '@astrojs/starlight/schema';
-import { videosSchema } from 'starlight-videos/schemas';
-
-export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema({ extend: videosSchema }) }),
-};
