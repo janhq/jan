7ed01eeebeb59f9ad79e216398e37d5bec8229b3
Faisal Amir <urmauur@gmail.com>
2025-12-15 23:34:40 +0700

    chore: dialog modal setting responsive

--- app/src/components/themes/theme-toggle.tsx
+++ app/src/components/themes/theme-toggle.tsx
@@ -15,7 +15,7 @@ export function ThemeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon-sm" className="hidden md:block">
+        <Button variant="outline" size="icon-sm" className="hidden md:flex">
           <Sun className="size-4 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute size-4 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
           <span className="sr-only">Toggle theme</span>
