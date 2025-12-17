d4628592945fc990947cb2a89c74f6204b3539f9
Faisal Amir <urmauur@gmail.com>
2025-12-17 15:41:26 +0700

    chore: update space user message

--- app/src/components/ai-elements/message.tsx
+++ app/src/components/ai-elements/message.tsx
@@ -27,8 +27,8 @@ export type MessageProps = HTMLAttributes<HTMLDivElement> & {
 export const Message = ({ className, from, ...props }: MessageProps) => (
   <div
     className={cn(
-      'group flex w-full max-w-[95%] flex-col gap-2 mb-8',
-      from === 'user' ? 'is-user ml-auto justify-end' : 'is-assistant',
+      'group flex w-full max-w-[95%] flex-col gap-2',
+      from === 'user' ? 'is-user ml-auto justify-end mb-8' : 'is-assistant',
       className
     )}
     {...props}
