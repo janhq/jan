c6d2143de045b6a76c1fe07fa8cf793431c0d875
Faisal Amir <urmauur@gmail.com>
2025-12-18 22:40:54 +0700

    fix: searchbox auto zoom on mobile

--- app/src/components/ui/command.tsx
+++ app/src/components/ui/command.tsx
@@ -73,7 +73,7 @@ function CommandInput({
       <CommandPrimitive.Input
         data-slot="command-input"
         className={cn(
-          'placeholder:text-muted-foreground flex h-10 w-full rounded-md bg-transparent py-3 text-sm! outline-hidden disabled:cursor-not-allowed disabled:opacity-50',
+          'placeholder:text-muted-foreground flex h-10 w-full rounded-md bg-transparent py-3 text-base outline-hidden disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
           className
         )}
         {...props}
