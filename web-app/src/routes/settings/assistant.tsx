2d47122d7af4601f835f3bc1f1b98c5a08655852
Faisal Amir <urmauur@gmail.com>
2026-03-13 22:02:18 +0700

    chore: scroll setting menu assistant tsx

--- web-app/src/routes/settings/assistant.tsx
+++ web-app/src/routes/settings/assistant.tsx
@@ -84,7 +84,7 @@ function AssistantContent() {
         </div>
       </HeaderPage>
       <div className="flex h-[calc(100%-60px)]">
-        <div className="flex h-svh w-full">
+        <div className="flex w-full overflow-y-auto">
           <SettingsMenu />
           <div className="flex flex-col gap-4 p-4 pt-4 w-full overflow-y-auto">
             {/* Default Assistant */}
