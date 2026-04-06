f49d5166eef04809ef248ee400308159667d89f1
Clayton <claytonlin1110@gmail.com>
2026-04-06 08:48:31 -0500

    fix: update
    Revert "fix: update"
    
    This reverts commit a4f6c39d4f9da06a9b0c4a2a860be3499e546eb3.
    
    Revert "fix: update"
    
    This reverts commit 6fed42467c1497b33ac6f55978dd2db76ea59f43.
    
    Revert "fix: update"
    
    This reverts commit d9f7e17ea87da137adafa7c2a11165ff19179274.
    
    fix: update
    
    Revert "fix: update"
    
    This reverts commit 9df9f8259d9f462dc4d6b080ad3310715c991a5e.
    
    Reapply "fix: update"
    
    This reverts commit e4e392a414e593dba9b9d886ecb33e2e45a95f5a.
    
    Reapply "fix: update"
    
    This reverts commit cfcd398fb9df48ee5749b48d9119cb174c77cd89.
    
    Reapply "fix: update"
    
    This reverts commit a0f1f1bd400aeab8731bc3bd8f9e58e73c44ce75.

--- src-tauri/patches/libspa/build.rs
+++ src-tauri/patches/libspa/build.rs
@@ -1,6 +1,4 @@
 fn main() {
-    // Upstream libspa also compiles tests/pod.c here for integration tests only.
-    // This fork drops those tests; keep pkg-config probe for libspa headers/libs.
     system_deps::Config::new()
         .probe()
         .expect("Cannot find libspa");
